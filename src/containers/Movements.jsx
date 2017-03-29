b561046f393484c955124598e639340962a41c52
CPatchane <code@patchane.com>
2017-03-29 16:06:32 +0200

    feat: add FigureBlock component + related styles

--- src/containers/Movements.jsx
+++ src/containers/Movements.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import { translate } from '../lib/I18n'
 
 import Select from '../components/Select'
+import FigureBlock from '../components/FigureBlock'
 
 const DATE_OPTIONS = ['Du 01 février au 24 février 2017']
 
@@ -17,6 +18,12 @@ export class Movements extends Component {
         </h2>
         <div className={styles['bnk-mov-form']}>
           <Select name='locale' options={DATE_OPTIONS} onChange={() => {}} />
+        </div>
+        <div className={styles['bnk-mov-figures']}>
+          <FigureBlock label='Total' total={1097.97} currency='€' colored signed />
+          <FigureBlock label='Débit' total={-1292.03} currency='€' signed />
+          <FigureBlock label='Crédit' total={2390} currency='€' signed />
+          <FigureBlock label='Opérations' total={32} />
         </div>
         <h3>
           Opérations
