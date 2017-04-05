9c53d28f05018ac3a3da19fd3da7340eb470869e
CPatchane <code@patchane.com>
2017-04-05 14:55:27 +0200

    feat: use the new categories via the categories map

--- src/actions/movements.js
+++ src/actions/movements.js
@@ -7,21 +7,21 @@ export const FETCH_BANK_MOVEMENTS_SUCCESS = 'FETCH_BANK_MOVEMENTS_SUCCESS'
 const MOVEMENTS_DATA = [
   {
     name: 'Restaurant Les frères Sushi',
-    type: 'restaurant',
+    type: 'restaurants',
     date: new Date(2017, 1, 22),
     amount: -32.1,
     currency: '€'
   },
   {
     name: 'Monoprix',
-    type: 'cart',
+    type: 'regular_shopping',
     date: new Date(2017, 1, 22),
     amount: -12.83,
     currency: '€'
   },
   {
     name: 'Facture SFR',
-    type: 'phone',
+    type: 'telecom',
     date: new Date(2017, 1, 22),
     amount: -10,
     currency: '€',
@@ -32,95 +32,95 @@ const MOVEMENTS_DATA = [
   },
   {
     name: 'Docteur Martin',
-    type: 'health',
+    type: 'health_costs',
     date: new Date(2017, 1, 21),
     amount: -450,
     currency: '€',
     action: {
-      type: 'reimbursement',
+      type: 'refund',
       url: ''
     }
   },
   {
     name: 'Salaire de février',
-    type: 'transfer',
+    type: 'salary',
     date: new Date(2017, 1, 21),
     amount: 2390,
     currency: '€'
   },
   {
     name: 'Shopping le vetement c\'est la vie',
-    type: 'shopping',
+    type: 'clothing',
     date: new Date(2017, 1, 19),
     amount: -79,
     currency: '€'
   },
   {
     name: 'Mac King',
-    type: 'fastfood',
+    type: 'meals',
     date: new Date(2017, 1, 19),
     amount: -7.9,
     currency: '€'
   },
   {
     name: 'SNCF Paris 13',
-    type: 'travel',
+    type: 'trips_transportation',
     date: new Date(2017, 1, 19),
     amount: -25,
     currency: '€'
   },
   {
     name: 'Monoprix',
-    type: 'cart',
+    type: 'regular_shopping',
     date: new Date(2017, 1, 17),
     amount: -12.36,
     currency: '€'
   },
   {
     name: 'Changement frein voiture',
-    type: 'car',
+    type: 'vehicle_maintenance',
     date: new Date(2017, 1, 15),
     amount: -90.86,
     currency: '€'
   },
   {
     name: 'Virement épargne',
-    type: 'transfer',
+    type: 'saving_transfer',
     date: new Date(2017, 1, 15),
     amount: -200,
     currency: '€'
   },
   {
     name: 'Mac King',
-    type: 'fastfood',
+    type: 'meals',
     date: new Date(2017, 1, 12),
     amount: -9.50,
     currency: '€'
   },
   {
     name: 'Eurostar London',
-    type: 'travel',
+    type: 'trips_transportation',
     date: new Date(2017, 1, 12),
     amount: -110.52,
     currency: '€'
   },
   {
     name: 'Monoprix',
-    type: 'cart',
+    type: 'regular_shopping',
     date: new Date(2017, 1, 10),
     amount: -43.89,
     currency: '€'
   },
   {
     name: 'DAB',
-    type: 'toBeDefined',
+    type: 'DAB',
     date: new Date(2017, 1, 8),
     amount: -50,
     currency: '€'
   },
   {
     name: 'Loyer',
-    type: 'house',
+    type: 'rent',
     date: new Date(2017, 1, 5),
     amount: -758.32,
     currency: '€'
