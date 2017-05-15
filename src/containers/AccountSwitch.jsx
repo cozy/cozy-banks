9e5200f216ebf7cb477ea0a21aafcc145c8a0ef7
Yannick Lohse <contact@yannick-lohse.fr>
2017-05-15 11:32:20 +0200

    style: linting

--- src/containers/AccountSwitch.jsx
+++ src/containers/AccountSwitch.jsx
@@ -24,24 +24,23 @@ class AccountSwitch extends Component {
     props.fetchAccounts()
       .then(() => {
         this.setState({
-          isFetching: false,
+          isFetching: false
         })
       })
   }
   switchAccount (account) {
-    (this.props.selectedAccount && this.props.selectedAccount._id === account._id) ? this.props.selectAccounts([]) : this.props.selectAccounts([account._id]);
+    (this.props.selectedAccount && this.props.selectedAccount._id === account._id) ? this.props.selectAccounts([]) : this.props.selectAccounts([account._id])
   }
   toggle (state) {
     // You would think that opening and closing the menu is easy, but it's fucking not. It needs to be closed when the user does pretty much anything except clicking inside the menu.
     // The react-nclickoutside lib doesn't work with preact. Listening for events at the document level doesn't really work either because it's impossible to predict in what order the handlers are going to be called.
     // So here we are with a method that is called on focus / on blur. The problem is, even a click *inside* the menu is going to trigger the blur event, and close it -- and somehow the click event gets lost in translation. So we have to introduce a small delay before actually removing the menu from the DOM.
-    let delay = state ? 0 : 100
+    let delay = state ? 0 : 150
     setTimeout(() => {
       this.setState({open: state})
     }, delay)
   }
   render () {
-
     const { accounts, selectedAccount } = this.props
     const { isFetching, open } = this.state
 
@@ -51,8 +50,7 @@ class AccountSwitch extends Component {
       <div className={styles['account-switch']}>
         <button className={classNames(styles['account-switch-button'], {[styles['active']]: open})} onFocus={this.toggle.bind(this, true)} onBlur={this.toggle.bind(this, false)}>
           { selectedAccount !== null
-            ?
-            <div>
+            ? <div>
               <div className={styles['account-name']}>
                 { selectedAccount.account + ' ' + selectedAccount.bank }
               </div>
@@ -60,8 +58,7 @@ class AccountSwitch extends Component {
                 n° { selectedAccount.number }
               </div>
             </div>
-            :
-            <div>
+            : <div>
               <div className={styles['account-name']}>
                 Tous les comptes
               </div>
@@ -75,9 +72,9 @@ class AccountSwitch extends Component {
             </h4>
             <ul>
               { accounts.map(account => (
-              <li onClick={() => {this.switchAccount(account)}} className={classNames({[styles['active']]: selectedAccount && account._id === selectedAccount._id})}>
-                { account.account + ' ' + account.bank }
-              </li>
+                <li onClick={() => { this.switchAccount(account) }} className={classNames({[styles['active']]: selectedAccount && account._id === selectedAccount._id})}>
+                  { account.account + ' ' + account.bank }
+                </li>
               )) }
             </ul>
           </div>
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -7,7 +7,7 @@ const getSelectedAccounts = (state) => state.selectedAccounts
 export const getFilteredOperations = createSelector(
   [ getOperations, getAccounts, getSelectedAccounts ],
   (operations, accounts, selectedAccountIds) => {
-    if (selectedAccountIds.length === 0) return operations;// If there is no active filter, return everything
+    if (selectedAccountIds.length === 0) return operations// If there is no active filter, return everything
 
     const selectedAccountNames = accounts.filter(account => (selectedAccountIds.indexOf(account._id) >= 0)).map(account => (account.bank))
 
