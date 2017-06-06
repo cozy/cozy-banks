256c3c517a498e3c907a9bb73f335dd7d1cf17ef
Patrick Browne <pt.browne@gmail.com>
2017-06-06 18:19:41 +0200

    feat: add AccountSharingStatus component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import React from 'react'
 import classNames from 'classnames'
 
 import Sidebar from 'components/Sidebar'
+import ReactHint from 'react-hint'
+import 'react-hint/css/index.css'
 
 export const App = ({ children }) => (
   <div className={classNames(styles['bnk-wrapper'], styles['bnk-sticky'])}>
@@ -12,6 +14,9 @@ export const App = ({ children }) => (
     <main className={styles['bnk-content']}>
       { children }
     </main>
+
+    {/* Outside every other component to bypass overflow:hidden */}
+    <ReactHint />
   </div>
 )
 
