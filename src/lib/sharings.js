40da5e6689636ff196139894efc04ed5f3301bb6
Grégory Le Garec <gregory.legarec@gmail.com>
2017-06-08 14:05:51 +0200

    fix: remove debugging call :fire:

--- src/lib/sharings.js
+++ src/lib/sharings.js
@@ -1,16 +1,24 @@
 const RECIPIENT_DOCTYPE = 'io.cozy.mocks.recipients'
 
-export function findRecipients (cozy, doctype, id) {
+export function findSharing (cozy, doctype, id) {
   if (!id) throw new Error('Missing mandatory parameter `id`')
 
+  const sharing = {
+    sharing_type: 'master-slave'
+  }
+
   return cozy.data.find(doctype, id)
-    .then(doc => {
-      const { recipients } = doc
+    .then(account => {
+      sharing.owner = account.owner
+      const { recipients } = account
       const hasRecipients = recipients && recipients.length
       return hasRecipients
         ? Promise.all(
             recipients.map(recipient => cozy.data.find(RECIPIENT_DOCTYPE, recipient.recipient.id))
           )
          : []
+    }).then(recipients => {
+      sharing.recipients = recipients
+      return sharing
     })
 }
