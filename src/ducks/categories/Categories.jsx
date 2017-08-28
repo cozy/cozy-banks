879d8d8d893702880fcba1fcd726e165acba409c
Simon Constans <simonc@linux.com>
2017-08-28 17:40:12 +0200

    refactor: categories :hammer:

--- src/ducks/categories/Categories.jsx
+++ src/ducks/categories/Categories.jsx
@@ -2,159 +2,85 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { translate } from 'cozy-ui/react/I18n'
 import Toggle from 'cozy-ui/react/Toggle'
-import categoriesMap from 'lib/categoriesMap'
+import { Table } from 'components/Table'
 import { FigureBlock } from 'components/Figure'
 import Loading from 'components/Loading'
 import PieChart from 'components/PieChart'
 import { Topbar } from 'ducks/commons'
 import { SelectDates, getFilteredOperations } from 'ducks/filters'
 import { fetchOperations, indexOperationsByDate } from 'actions'
-import CategoriesBoard from './CategoriesBoard'
+import { operationsByCategory, computeCategorieData } from './helpers'
 import styles from './Categories.styl'
-
-const INCOME_CATEGORY = 'income'
-
-// This function builds a map of categories and sub-categories, each containing a list of related operations, a name and a color
-const operationsByCategory = operations => {
-  let categories = {}
-
-  operations.forEach(operation => {
-    // Creates a map of categories, where each entry contains a list of related operations and a breakdown by sub-category
-    let category = categoriesMap.get(operation.category) || categoriesMap.get('uncategorized_others')
-
-    // create a new parent category if necessary
-    if (!categories.hasOwnProperty(category.name)) {
-      categories[category.name] = {
-        name: category.name,
-        color: category.color,
-        operations: [],
-        subcategories: {}
-      }
-    }
-
-    // create the subcategory if necessary
-    if (!categories[category.name].subcategories.hasOwnProperty(operation.category)) {
-      categories[category.name].subcategories[operation.category] = {
-        name: operation.category,
-        operations: []
-      }
-    }
-
-    categories[category.name].operations.push(operation)
-    categories[category.name].subcategories[operation.category].operations.push(operation)
-  })
-
-  return categories
-}
-
-// Very specific to this component: takes the operations by category as returned by the `operationsByCategory` function, and turns it into a flat array, while computing derived data such as totals and curencies.
-// The result is used pretty much as is down the chain by other components, so changing property names here should be done with care.
-const computeCategorieData = operationsByCategory => {
-  return Object.values(operationsByCategory).map(category => {
-    let subcategories = Object.values(category.subcategories).map(subcategory => {
-      const debit = subcategory.operations.reduce((total, op) => (op.amount < 0 ? total + op.amount : total), 0)
-      const credit = subcategory.operations.reduce((total, op) => (op.amount > 0 ? total + op.amount : total), 0)
-
-      return {
-        name: subcategory.name,
-        amount: credit + debit,
-        debit: debit,
-        credit: credit,
-        percentage: 0,
-        currency: subcategory.operations[0].currency,
-        operationsNumber: subcategory.operations.length
-      }
-    })
-
-    const debit = category.operations.reduce((total, op) => (op.amount < 0 ? total + op.amount : total), 0)
-    const credit = category.operations.reduce((total, op) => (op.amount > 0 ? total + op.amount : total), 0)
-
-    return {
-      name: category.name,
-      color: category.color,
-      amount: credit + debit,
-      debit: debit,
-      credit: credit,
-      percentage: 0,
-      currency: category.operations[0].currency,
-      operationsNumber: category.operations.length,
-      subcategories: subcategories
-    }
-  })
-}
+import Category from './Category'
 
 class Categories extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      isFetching: true,
-      withIncome: true
-    }
+  state = {
+    isFetching: true,
+    withIncome: true,
+    selectedCategory: undefined
+  }
 
-    this.props.fetchOperations().then(
-      this.setState({isFetching: false})
-    )
+  selectCategory = category => {
+    this.setState({selectedCategory: category})
   }
 
   applyFilter = e => {
     this.setState({withIncome: e.target.checked})
   }
 
-  render ({t, categories}, {isFetching, withIncome}) {
-    if (isFetching) {
-      return <Loading loadingType='categories' />
-    }
+  async componentDidMount () {
+    await this.props.fetchOperations()
+    this.setState({isFetching: false})
+  }
 
-    if (categories.length === 0) {
+  render ({t, categories}, {isFetching, withIncome, selectedCategory}) {
+    if (isFetching) {
       return (
         <div>
           <Topbar>
-            <h2>{t('Categories.title.empty')}</h2>
+            <h2>{t(`Categories.title.${categories ? 'general' : 'empty'}`)}</h2>
           </Topbar>
-          <div className={styles['bnk-cat-form']}>
-            <SelectDates />
-          </div>
-          <p>{t('Categories.title.empty_text')}</p>
+          <Loading loadingType='categories' />
         </div>
       )
     }
 
+    const pieDataObject = {labels: [], data: [], colors: []}
+    let operationsTotal = 0
+    const globalCurrency = categories.length > 0 ? categories[0].currency : '€'
+
     // compute the filter to use
-    let filteredCategories = categories
     if (!withIncome) {
-      filteredCategories = filteredCategories.filter(category => (category.name !== INCOME_CATEGORY))
+      categories = categories.filter(category => (category.name !== 'income'))
     }
 
-    // compute some global data
-    const absoluteOperationsTotal = filteredCategories.reduce((total, category) => (total + Math.abs(category.amount)), 0)
-    const globalCurrency = filteredCategories[0].currency
-    let operationsTotal = 0
+    if (categories.length !== 0) {
+      // compute some global data
+      const absoluteOperationsTotal = categories.reduce((total, category) => (total + Math.abs(category.amount)), 0)
+      for (let category of categories) {
+        category.percentage = Math.round(Math.abs(category.amount) / absoluteOperationsTotal * 100)
+        for (let subcategory of category.subcategories) {
+          subcategory.percentage = Math.round(Math.abs(subcategory.amount) / absoluteOperationsTotal * 100)
+        }
+        operationsTotal += category.amount
+      }
 
-    filteredCategories.forEach(category => {
-      category.percentage = Math.round(Math.abs(category.amount) / absoluteOperationsTotal * 100)
+      // sort the categories for display
+      categories = categories.sort((a, b) => (b.percentage - a.percentage))
 
-      category.subcategories.forEach(subcategory => {
-        subcategory.percentage = Math.round(Math.abs(subcategory.amount) / absoluteOperationsTotal * 100)
-      })
+      // configure the pie chart
+      for (const category of categories) {
+        pieDataObject.labels.push(t(`Data.categories.${category.name}`))
+        pieDataObject.data.push(Math.abs(category.amount).toFixed(2)) // use positive values
+        pieDataObject.colors.push(category.color)
+      }
+    }
 
-      operationsTotal += category.amount
-    })
-
-    // sort the categories for display
-    filteredCategories = filteredCategories.sort((a, b) => (b.percentage - a.percentage))
-
-    // configure the pie chart
-    const pieDataObject = {labels: [], data: [], colors: []}
-    filteredCategories.forEach((category) => {
-      pieDataObject.labels.push(t(`Data.categories.${category.name}`))
-      pieDataObject.data.push(Math.abs(category.amount).toFixed(2)) // use positive values
-      pieDataObject.colors.push(category.color)
-    })
     return (
       <div>
 
         <Topbar>
-          <h2>{t('Categories.title.general')}</h2>
+          <h2>{t(`Categories.title.${categories.length > 0 ? 'general' : 'empty'}`)}</h2>
         </Topbar>
 
         <div className={styles['bnk-cat-top']}>
@@ -166,27 +92,41 @@ class Categories extends Component {
                 Inclure les revenus
               </label>
             </div>
-            <FigureBlock
+            {categories.length > 0 && <FigureBlock
               label={withIncome ? t('Categories.title.total') : t('Categories.title.debit')}
               total={operationsTotal}
               currency={globalCurrency}
-              coloredPositive coloredNegative signed />
+              coloredPositive coloredNegative signed />}
           </div>
 
-          <div className={styles['bnk-cat-figure']}>
+          {categories.length > 0 && <div className={styles['bnk-cat-figure']}>
             <PieChart
               labels={pieDataObject.labels}
               data={pieDataObject.data}
               colors={pieDataObject.colors}
             />
-          </div>
+          </div>}
         </div>
 
-        <div className={styles['bnk-cat-table']}>
-          <CategoriesBoard
-            categories={filteredCategories}
-          />
-        </div>
+        {categories.length > 0
+          ? <Table className={styles['bnk-table-category']}>
+            <thead>
+              <tr>
+                <td className={styles['bnk-table-category-category']}>{t('Categories.headers.categories')}</td>
+                <td className={styles['bnk-table-percentage']}>%</td>
+                <td className={styles['bnk-table-operation']}>{t('Categories.headers.operations')}</td>
+                <td className={styles['bnk-table-total']}>{t('Categories.headers.total')}</td>
+                <td className={styles['bnk-table-amount']}>{t('Categories.headers.credit')}</td>
+                <td className={styles['bnk-table-amount']}>{t('Categories.headers.debit')}</td>
+                <td className={styles['bnk-table-chevron']} />
+              </tr>
+            </thead>
+            {categories.map(category =>
+              <Category selectedCategory={selectedCategory} selectCategory={this.selectCategory} category={category} />
+            )}
+          </Table>
+          : <p>{t('Categories.title.empty_text')}</p>
+        }
       </div>
     )
   }
