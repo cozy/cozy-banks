6d9b14a75cc0a1eb9856d8fee750561e1dc2bb98
Simon Constans <simonc@linux.com>
2017-08-31 18:49:37 +0200

    feat: Save montant boolean :cocktail:

--- src/ducks/settings/Notifications.jsx
+++ src/ducks/settings/Notifications.jsx
@@ -1,98 +1,52 @@
 import React, { Component } from 'react'
 import classNames from 'classnames'
 import Toggle from 'cozy-ui/react/Toggle'
-import Modal from 'cozy-ui/react/Modal'
-import {translate} from 'cozy-ui/react/I18n'
-import {objectT} from 'utils/i18n'
+import { translate } from 'cozy-ui/react/I18n'
+import { cozyConnect } from 'redux-cozy-client'
+import Loading from 'components/Loading'
+
+import { getSettings, fetchSettingsCollection, createSettings, updateSettings } from '.'
 import styles from './Notifications.styl'
 
-const NotificationDescription = ({children}) => (
+const NotificationDescription = ({ children }) => (
   <p className={styles['notification-description']}>
     {children}
   </p>
 )
 
 class Notifications extends Component {
-  state = {
-    solde: false,
-    montant: false,
-    salaire: false,
-    retard: false,
-    hebdo: false,
-    mensuel: false,
-    comingSoonModal: true
-  }
-
   onToggle = (setting, checked) => {
-    this.setState({
-      // keep everything unchecked while it's not working
-      [setting]: false,
-      comingSoonModal: true
-    })
+    const { settingsCollection, dispatch } = this.props
+    let settings = getSettings(settingsCollection)
+    const updateOrCreate = settings._id ? updateSettings : createSettings
+    settings[setting] = checked
+    dispatch(updateOrCreate(settings))
   }
 
-  dismissComingSoon = () => {
-    this.setState({ comingSoonModal: false })
-  }
+  render ({ t, settingsCollection }) {
+    if (settingsCollection.fetchStatus === 'loading' || settingsCollection.fetchStatus === 'pending') {
+      return <Loading />
+    }
+    const settings = getSettings(settingsCollection)
 
-  render (props, state) {
-    const {t} = props
-    let notifications = [{
-      'title': t('Notifications.if_balance_lower.title'),
-      'name': 'solde',
-      'description': <NotificationDescription>
-        {t('Notifications.if_balance_lower.description')} <input type='text' value='400' className={classNames(styles['notification-input'], styles['suffixed'])} /><span>€</span>
-      </NotificationDescription>
-    }, {
-      'title': t('Notifications.if_movement_greater.title'),
-      'name': 'montant',
-      'description': <NotificationDescription>
-        {t('Notifications.if_movement_greater.description')} <input type='text' value='30' className={classNames(styles['notification-input'], styles['suffixed'])} /><span>€</span>
-      </NotificationDescription>
-    }, {
-      'title': t('Notifications.when_month_revenue.title'),
-      'name': 'salaire',
-      'description': <NotificationDescription>
-        {t('Notifications.when_month_revenue.description')}
-      </NotificationDescription>
-    }, {
-      'title': t('Notifications.no_revenue.title'),
-      'name': 'retard',
-      'description': <NotificationDescription>
-        {objectT(t, 'Notifications.no_revenue.description', {
-          input: <input
-            type='text'
-            value='7'
-            className={styles['notification-input']} />
-        })}
-      </NotificationDescription>
-    }, {
-      'title': t('Notifications.weekly_summary.title'),
-      'name': 'hebdo',
-      'description': <NotificationDescription>
-        {t('Notifications.weekly_summary.description')}
-      </NotificationDescription>
-    }, {
-      'title': t('Notifications.monthly_summary.title'),
-      'name': 'mensuel',
-      'description': <NotificationDescription>
-        {t('Notifications.monthly_summary.description')}
-      </NotificationDescription>
-    }]
+    let notifications = [
+      {
+        'title': t('Notifications.if_movement_greater.title'),
+        'name': 'montant',
+        'description':
+          <NotificationDescription>
+            {t('Notifications.if_movement_greater.description')}
+            <input type='text' value='30' className={classNames(styles['notification-input'], styles['suffixed'])} />
+            <span>€</span>
+          </NotificationDescription>
+      }
+    ]
 
     return (
       <div>
         <h4>
           {t('Notifications.title')}
         </h4>
-
-        {state.comingSoonModal &&
-          <Modal
-            title={t('ComingSoon.title')}
-            description={t('ComingSoon.description')}
-            secondaryAction={this.dismissComingSoon}
-          />
-        }
 
         {notifications.map(notification => (
           <div>
@@ -103,15 +57,20 @@ class Notifications extends Component {
               {notification.description}
 
               <div className={styles['notification-toggle']}>
-                <Toggle id={notification.name} checked={state[notification.name]} onToggle={checked => this.onToggle(notification.name, checked)} />
+                <Toggle id={notification.name} checked={settings[notification.name]}
+                  onToggle={checked => this.onToggle(notification.name, checked)}
+                />
               </div>
             </div>
           </div>
         ))}
-
       </div>
     )
   }
 }
 
-export default translate()(Notifications)
+const mapDocumentsToProps = () => ({
+  settingsCollection: fetchSettingsCollection()
+})
+
+export default cozyConnect(mapDocumentsToProps)(translate()(Notifications))
--- src/ducks/settings/index.js
+++ src/ducks/settings/index.js
@@ -1,4 +1,26 @@
+import { fetchCollection, createDocument, updateDocument } from 'redux-cozy-client'
 import Settings from './Settings'
+
+// constants
+const DOCTYPE = 'io.cozy.bank.settings'
+const COLLECTION_NAME = 'settings'
+const DEFAULTS_SETTINGS = {
+  montant: false
+}
 
 // components
 export { Settings }
+
+// actions
+export const fetchSettingsCollection = () => fetchCollection(COLLECTION_NAME, DOCTYPE)
+export const createSettings = settings => createDocument({ type: DOCTYPE, ...settings }, { updateCollections: [COLLECTION_NAME] })
+export const updateSettings = settings => updateDocument(settings)
+
+// utils
+export const getSettings = settingsCollection => {
+  if (settingsCollection && settingsCollection.data && settingsCollection.data.length > 0) {
+    return settingsCollection.data[0]
+  } else {
+    return DEFAULTS_SETTINGS
+  }
+}
