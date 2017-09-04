2d3f079450f9c6dcb016acdedbc4104ae153bd56
Simon Constans <simonc@linux.com>
2017-09-04 18:07:49 +0200

    feat: get app configuration :mag:

--- src/targets/services/notifications.js
+++ src/targets/services/notifications.js
@@ -1,1 +1,19 @@
-console.log('notifications')
+import { cozyClient } from 'cozy-konnector-libs'
+
+const getConfiguration = async () => {
+  const configurations = await cozyClient.data.findAll('io.cozy.bank.settings')
+  if (configurations.length > 0 && configurations[0].notifications) {
+    return configurations[0].notifications
+  } else {
+    return undefined
+  }
+}
+
+const notificationsService = async () => {
+  const config = await getConfiguration()
+  if (config === undefined) return // stop notifications service
+
+  console.log(config)
+}
+
+notificationsService()
