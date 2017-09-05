1235f3286026fb5a1e7184462a93586a4cb2268f
Patrick Browne <patrick@cozycloud.cc>
2017-09-05 15:21:41 +0200

    refactor: use Icons for Menu. easy change of color !

--- src/ducks/commons/Nav.jsx
+++ src/ducks/commons/Nav.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import classNames from 'classnames'
-import { translate } from 'cozy-ui/react/I18n'
+import { translate, Icon } from 'cozy-ui/react'
 import { Link, withRouter } from 'react-router'
 import styles from './Nav.styl'
+import dashboard from 'assets/icons/icon-dashboard.svg'
+import arrows from 'assets/icons/icon-arrow-left-right.svg'
+import graph from 'assets/icons/icon-graph.svg'
+import people from 'assets/icons/icon-people.svg'
 
 const ActiveLink = withRouter(function ActiveLink ({ to, className, children, router }) {
   return (
@@ -22,35 +26,23 @@ const Nav = ({ t }) => (
   <nav>
     <ul className={styles['coz-nav']}>
       <li className={styles['coz-nav-item']}>
-        <ActiveLink
-          to='currentBalance'
-          className={styles['bnk-cat-balance']}
-        >
-          {t('Nav.balance')}
+        <ActiveLink to='currentBalance'>
+          <Icon icon={dashboard} /> {t('Nav.balance')}
         </ActiveLink>
       </li>
       <li className={styles['coz-nav-item']}>
-        <ActiveLink
-          to='movements'
-          className={styles['bnk-cat-movements']}
-        >
-          {t('Nav.movements')}
+        <ActiveLink to='movements'>
+          <Icon icon={arrows} /> {t('Nav.movements')}
         </ActiveLink>
       </li>
       <li className={styles['coz-nav-item']}>
-        <ActiveLink
-          to='categories'
-          className={styles['bnk-cat-categories']}
-        >
-          {t('Nav.categorisation')}
+        <ActiveLink to='categories'>
+          <Icon icon={graph} /> {t('Nav.categorisation')}
         </ActiveLink>
       </li>
       <li className={styles['coz-nav-item']}>
-        <ActiveLink
-          to='settings'
-          className={styles['bnk-cat-settings']}
-        >
-          {t('Nav.settings')}
+        <ActiveLink to='settings'>
+          <Icon icon={people} /> {t('Nav.settings')}
         </ActiveLink>
       </li>
     </ul>
