6356933c024934fec1b95f938f83afe3585a2f1a
Patrick Browne <patrick@cozycloud.cc>
2017-09-07 13:58:04 +0200

    chore: do not use shortcut

--- src/ducks/onboarding/Hero.jsx
+++ src/ducks/onboarding/Hero.jsx
@@ -6,14 +6,14 @@ export const Icon = ({ color, icon }) => (
   <BaseIcon width='auto' height='5rem' style={{ color }} icon={icon} />
 )
 
-const mkC = (Tag, extra = {}) => ({children, ...props}) => (
+const mkComponent = (Tag, extra = {}) => ({children, ...props}) => (
   <Tag {...extra} {...props}>{children}</Tag>
 )
 
-export const Hero = mkC('div', {className: styles.Hero})
-export const Title = mkC('h2', {className: styles.Hero__title})
-export const Subtitle = mkC('h3', {className: styles.Hero__subtitle})
-export const Section = mkC('div', {className: styles.Hero__section})
-export const Sections = mkC('div', {className: styles.Hero__sections})
-export const Paragraph = mkC('p', {})
-export const CTA = mkC('p', {className: styles.Hero__cta})
+export const Hero = mkComponent('div', {className: styles.Hero})
+export const Title = mkComponent('h2', {className: styles.Hero__title})
+export const Subtitle = mkComponent('h3', {className: styles.Hero__subtitle})
+export const Section = mkComponent('div', {className: styles.Hero__section})
+export const Sections = mkComponent('div', {className: styles.Hero__sections})
+export const Paragraph = mkComponent('p', {})
+export const CTA = mkComponent('p', {className: styles.Hero__cta})
