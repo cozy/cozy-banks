7b6d88b78675afec49af95a433cfd1bfcfc1cbd0
Patrick Browne <patrick@cozycloud.cc>
2017-09-07 14:38:05 +0200

    refactor: use mkComponent to simplify code

--- src/ducks/commons/Nav.jsx
+++ src/ducks/commons/Nav.jsx
@@ -1,46 +1,49 @@
 import React from 'react'
 import { translate, Icon } from 'cozy-ui/react'
-import { Link, withRouter } from 'react-router'
+import { Link } from 'react-router'
 import styles from './Nav.styl'
-
+import mkComponent from 'utils/mkComponent'
 import dashboard from 'assets/icons/icon-dashboard.svg'
 import arrows from 'assets/icons/icon-arrow-left-right.svg'
 import graph from 'assets/icons/icon-graph.svg'
 import people from 'assets/icons/icon-people.svg'
 
-const ActiveLink = withRouter(({ to, className, children, router }) => (
+const ActiveLink = ({ to, children }) => (
   <Link
     to={to}
     activeClassName={styles['active']}
     className={styles['coz-nav-link']}>
     {children}
   </Link>
-))
+)
+
+const NavList = mkComponent('ul', { className: styles['coz-nav'] })
+const NavItem = mkComponent('li', { className: styles['coz-nav-item'] })
 
 const Nav = ({ t }) => (
   <nav>
-    <ul className={styles['coz-nav']}>
-      <li className={styles['coz-nav-item']}>
+    <NavList>
+      <NavItem>
         <ActiveLink to='currentBalance'>
           <Icon icon={dashboard} /> {t('Nav.balance')}
         </ActiveLink>
-      </li>
-      <li className={styles['coz-nav-item']}>
+      </NavItem>
+      <NavItem>
         <ActiveLink to='movements'>
           <Icon icon={arrows} /> {t('Nav.movements')}
         </ActiveLink>
-      </li>
-      <li className={styles['coz-nav-item']}>
+      </NavItem>
+      <NavItem>
         <ActiveLink to='categories'>
           <Icon icon={graph} /> {t('Nav.categorisation')}
         </ActiveLink>
-      </li>
-      <li className={styles['coz-nav-item']}>
+      </NavItem>
+      <NavItem>
         <ActiveLink to='settings'>
           <Icon icon={people} /> {t('Nav.settings')}
         </ActiveLink>
-      </li>
-    </ul>
+      </NavItem>
+    </NavList>
   </nav>
 )
 
