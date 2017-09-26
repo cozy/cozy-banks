813a5d94f73427fc57bb3e7401d00bcaf8af8520
Patrick Browne <patrick@cozycloud.cc>
2017-09-26 11:44:26 +0200

    🔩 chore: fixtures for demo

---
+++ test/fixtures/demo/helpers.js
@@ -0,0 +1,17 @@
+const categoryNameToId = inverseObject(require('./linxo-categories'))
+
+module.exports = {
+  helpers: {
+    categoryId: function (catName) {
+      return categoryNameToId[catName]
+    }
+  }
+}
+
+function inverseObject (obj) {
+  const out = {}
+  Object.keys(obj).forEach(function (k) {
+    out[obj[k]] = k
+  })
+  return out
+}
