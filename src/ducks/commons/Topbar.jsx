be4193c55bcf93d91724f03b1445af5d14827624
Patrick Browne <patrick@cozycloud.cc>
2017-10-31 09:36:13 +0100

    ✨ feat: use BarCenter/BarLeft for TopBar

--- src/ducks/commons/Topbar.jsx
+++ src/ducks/commons/Topbar.jsx
@@ -1,17 +1,28 @@
+/* global cozy */
+
 import React from 'react'
 import classNames from 'classnames'
 import styles from './Topbar.styl'
+import withBreakpoints from 'utils/breakpointsAware'
 
-export const TopbarTitle = ({ children }) => (
-  <div className={styles['Topbar-title']}>
+const { BarCenter, BarLeft } = cozy.bar
+
+export const TopbarTitle = withBreakpoints()(({ children, breakpoints }) => {
+  return breakpoints.isMobile ? <BarCenter>
+    <div className={styles['Topbar-title']}>
+      {children}
+    </div>
+  </BarCenter> : <div className={styles['Topbar-title']}>
     {children}
   </div>
-)
+})
 
 export const TopbarLeft = ({className, children}) => (
-  <div className={classNames(styles['Topbar-left'], className)}>
-    {children}
-  </div>
+  <BarLeft>
+    <div className={classNames(styles['Topbar-left'], className)}>
+      {children}
+    </div>
+  </BarLeft>
 )
 
 export default TopbarTitle
