ab496adc931e779649cf8118611141d87b35512f
Patrick Browne <patrick@cozycloud.cc>
2017-12-14 16:11:56 +0100

    🔨 refactor: use Handlebars

--- src/ducks/notifications/TransactionGreater.js
+++ src/ducks/notifications/TransactionGreater.js
@@ -1,5 +1,35 @@
 import { cozyClient } from 'cozy-konnector-libs'
+import Handlebars from 'handlebars'
 const abs = number => number < 0 ? -number : number
+
+Handlebars.registerHelper({
+  positive: function (n) {
+    return n > 0
+  },
+  formatDate: function (d) {
+    return d.substr(0, 10)
+  }
+})
+
+const textTemplate = Handlebars.compile(`
+  {{# if onlyOne }}
+    {{# with transactions.[0] }}
+      {{# if positive amount }}
+{{ formatDate date }} {{{ t "debit" label }}}
+      {{else}}
+{{ formatDate date }} {{{ t "credit" label }}}
+      {{/if}}
+    {{/with}}
+  {{ else }}
+    {{#each transactions}}
+      {{#if (positive amount) }}
+{{ formatDate date }} {{{ t "others.creditContent" this }}}
+      {{else}}
+{{ formatDate date }} {{{ t "others.debitContent" this }}}
+      {{/if}}
+    {{/each}}
+  {{/if}}
+`)
 
 class TransactionGreater {
   constructor (config) {
@@ -9,9 +39,7 @@ class TransactionGreater {
   }
 
   filter (op) {
-    return Math.random() < 0.05
     const maxAmount = abs(this.maxAmount)
-    // TODO: Find why op is undefined?
     return abs(op.amount) > maxAmount
   }
 
@@ -23,26 +51,33 @@ class TransactionGreater {
     }
 
     const notification = { reference: 'transaction_greater' }
-    let translateKey = 'Notifications.if_transaction_greater.notification.'
-    if (transactionsFiltered.length === 1) {
-      const transaction = transactionsFiltered[0]
-      translateKey += transaction.amount > 0 ? 'credit' : 'debit'
-      notification.title = this.t(`${translateKey}.title`, { amount: transaction.amount, currency: transaction.currency })
-      notification.content = this.t(`${translateKey}.content`, { label: transaction.label })
-    } else {
-      translateKey += 'others'
-      notification.title = this.t(`${translateKey}.title`, {
-        transactionsLength: transactionsFiltered.length,
-        maxAmount: this.maxAmount
-      })
-      notification.content = ''
-      for (const transactionFiltered of transactionsFiltered) {
-        const key = transactionFiltered.amount > 0 ? 'credit' : 'debit'
-        notification.content += this.t(`${translateKey}.${key}Content`, {
-          amount: transactionFiltered.amount, currency: transactionFiltered.currency, label: transactionFiltered.label
-        }) + '\n'
-      }
+    let translateKey = 'Notifications.if_transaction_greater.notification'
+
+    // Custom t bound to its part
+    const t = (key, data) => this.t(translateKey + '.' + key, data)
+    Handlebars.registerHelper({ t })
+
+    const onlyOne = transactionsFiltered.length === 1
+    const templateData = {
+      transactions: transactionsFiltered,
+      onlyOne: transactionsFiltered.length === 1,
     }
+    const firstTransaction = transactions[0]
+    const titleData = onlyOne ? {
+      amount: firstTransaction.amount,
+      currency: firstTransaction.currency
+    } : {
+      transactionsLength: transactionsFiltered.length,
+      maxAmount: this.maxAmount
+    }
+
+    let titleKey = onlyOne ? (
+      firstTransaction.amount > 0 ?
+        `${translateKey}.credit.title` :
+        `${translateKey}.debit.title`
+    ) : `${translateKey}.others.title`
+    notification.title = this.t(titleKey, titleData)
+    notification.content = textTemplate(templateData)
 
     return notification
   }
