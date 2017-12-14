76899c5fe60e2a101b779c567dbb4e894aa7fd46
Patrick Browne <patrick@cozycloud.cc>
2017-12-14 18:01:35 +0100

    🔨 refactor: use Handlebars

--- src/ducks/notifications/BalanceLower.js
+++ src/ducks/notifications/BalanceLower.js
@@ -1,4 +1,17 @@
 import { cozyClient } from 'cozy-konnector-libs'
+import Handlebars from 'handlebars'
+import _textTemplate from './balance-lower-text.hbs'
+
+Handlebars.registerHelper({
+  positive: amount => amount > 0,
+  formatDate: function (d) {
+    return d.substr(0, 10)
+  }
+})
+
+const addCurrency = o => ({...o, currency: '€'})
+
+const textTemplate = Handlebars.compile(_textTemplate)
 
 class BalanceLower {
   constructor (config) {
@@ -15,48 +28,42 @@ class BalanceLower {
   }
 
   buildNotification ({accounts, transactions}) {
-    const accountsFiltered = accounts.filter(acc => this.filter(acc))
+    const accountsFiltered = accounts.filter(acc => this.filter(acc)).map(addCurrency)
     if (accountsFiltered.length === 0) {
       console.log('BalanceLower: no matched transactions')
       return
     }
 
     const notification = { reference: 'balance_lower' }
-    let translateKey = 'Notifications.if_balance_lower.notification.'
-    if (accountsFiltered.length === 1) {
-      translateKey += 'one'
-      const account = accountsFiltered[0]
-      const transactionsFiltered = transactions.filter(op => op.account === account._id)
-      notification.title = this.t(`${translateKey}.title`, {
-        balance: account.balance,
-        currency: '€',
-        label: account.shortLabel || account.label
-      })
-      notification.content = ''
-      for (const transaction of transactionsFiltered) {
-        const key = transaction.amount > 0 ? 'credit' : 'debit'
-        notification.content += this.t(`${translateKey}.${key}Content`, {
-          amount: transaction.amount,
-          currency: transaction.currency,
-          label: transaction.label
-        }) + '\n'
-      }
-    } else {
-      translateKey += 'several'
-      notification.title = this.t(`${translateKey}.title`, {
-        accountsLength: accountsFiltered.length,
-        lowerBalance: this.lowerBalance,
-        currency: '€'
-      })
-      notification.content = ''
-      for (const accountFiltered of accountsFiltered) {
-        notification.content += this.t(`${translateKey}.content`, {
-          balance: accountFiltered.balance,
-          currency: '€',
-          label: accountFiltered.label
-        }) + '\n'
-      }
+    const translateKey = 'Notifications.if_balance_lower.notification'
+
+    const t = (key, data) => this.t(translateKey + '.' + key, data)
+    Handlebars.registerHelper({ t })
+
+    const onlyOne = accountsFiltered.length === 1
+    const firstAccount = accountsFiltered[0]
+
+    const templateData = onlyOne ? {
+      onlyOne,
+      transactions: transactions.filter(op => op.account === firstAccount._id)
+    } : {
+      onlyOne,
+      accounts: accountsFiltered
     }
+
+    const titleData = onlyOne ? {
+      balance: firstAccount.balance,
+      currency: '€',
+      label: firstAccount.shortLabel || firstAccount.label
+    } : {
+      accountsLength: accountsFiltered.length,
+      lowerBalance: this.lowerBalance,
+      currency: '€'
+    }
+
+    const titleKey = `${translateKey}.${onlyOne ? 'one' : 'several'}.title`
+    notification.title = this.t(titleKey, titleData)
+    notification.content = textTemplate(templateData)
 
     return notification
   }
