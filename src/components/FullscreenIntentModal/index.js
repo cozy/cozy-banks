1dfbf321a9e91e84bd1c0b1bdc284942eeace59b
Patrick Browne <patrick@cozycloud.cc>
2017-12-14 13:30:56 +0100

    🐝 fix: use new properties from Modal

--- src/components/FullscreenIntentModal/index.js
+++ src/components/FullscreenIntentModal/index.js
@@ -17,7 +17,7 @@ class FullscreenIntentModal extends Component {
   render ({ style }) {
     return (
       <div style={style} className={styles['modal--fullscreen']} >
-        <Modal overflowHidden withCross secondaryAction={this.props.secondaryAction}>
+        <Modal overflowHidden dismissAction={this.props.dismissAction}>
           <div className={styles.content} ref={this.saveRef} />
         </Modal>
       </div>
