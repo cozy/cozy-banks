ba73723be185a5007a2b66791708803ce56397e1
Patrick Browne <patrick@cozycloud.cc>
2017-12-15 12:20:29 +0100

    🔧 chore: have html generation working in webpack

--- config/webpack.target.services.js
+++ config/webpack.target.services.js
@@ -6,14 +6,17 @@ const merge = require('webpack-merge')
 const base = require('./webpack.config.base')
 const ui = require('./webpack.config.cozy-ui')
 
+const SRC_DIR = path.resolve(__dirname, '../src')
+
+const mimerPath = require.resolve(path.join(SRC_DIR, 'ducks/notifications/vendor/mimer.min'))
+
 module.exports = merge.strategy({
   plugins: 'replace',
   output: 'replace',
   entry: 'replace'
 })(base, ui, {
-  entry: path.resolve(__dirname, '../src/targets/services/notifications'),
+  entry: path.resolve(SRC_DIR, './targets/services/notifications'),
   target: 'node',
-  devtool: false,
   output: {
     path: path.resolve(__dirname, '../build'),
     filename: 'notifications.js'
@@ -22,13 +25,30 @@ module.exports = merge.strategy({
     rules: [
       {
         test: /\.hbs$/,
+        include: SRC_DIR,
         loader: 'raw-loader'
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve('./node_modules'),
+        loader: 'shebang-loader'
       }
     ]
   },
   plugins: [
+    new webpack.NormalModuleReplacementPlugin(
+      /mimer/,
+      mimerPath
+    ),
+
     new webpack.DefinePlugin({
       __TARGET__: JSON.stringify('services')
-    })
+    }),
+
+    /* Does not work in a bundle, we do not use it */
+    new webpack.NormalModuleReplacementPlugin(
+      /image-size/,
+      require.resolve(path.join(SRC_DIR, 'ducks/notifications/noop'))
+    ),
   ]
 })
