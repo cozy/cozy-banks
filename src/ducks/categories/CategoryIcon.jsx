5159ca848782927f5dc13a94ca086a0702bae605
Cyrille Perois <cperois@outlook.com>
2018-01-16 15:16:47 +0100

    fix: add cursor pointer on category icon on a transaction line

--- src/ducks/categories/CategoryIcon.jsx
+++ src/ducks/categories/CategoryIcon.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Icon } from 'cozy-ui/react'
 
-const CategoryIcon = ({category}) => {
+const CategoryIcon = ({category, className}) => {
   let icon
   try {
     if (category === undefined) category = 'uncategorized'
@@ -9,7 +9,7 @@ const CategoryIcon = ({category}) => {
   } catch (err) {
     icon = require(`assets/icons/categories/icon-cat-uncategorized.svg`)
   }
-  return <Icon icon={icon.default} width='2em' height='2em' />
+  return <Icon icon={icon.default} width='2em' height='2em' className={className} />
 }
 
 export default CategoryIcon
