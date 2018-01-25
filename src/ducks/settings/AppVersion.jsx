50c87a34032b49ae06f50d4c5bbdf1d84dd15dc7
Cyrille Perois <cperois@outlook.com>
2018-01-25 18:05:41 +0100

    feat: show version in settings on mobile app

---
+++ src/ducks/settings/AppVersion.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './AppVersion.styl'
+
+const AppVersion = ({ version }) => (
+  <div className={styles['app-version']}>
+    Version {version}
+  </div>
+)
+
+AppVersion.propTypes = {
+  version: PropTypes.string.isRequired
+}
+
+export default AppVersion
