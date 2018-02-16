98bf0334fa09d66f8bd22e74fe6bfc92125db018
Patrick Browne <patrick@cozycloud.cc>
2018-02-16 17:46:10 +0100

    ✨ feat: add flag switcher

---
+++ src/components/FlagSwitcher.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import flag from 'utils/flag'
+
+const FlagInput = ({ name }) => {
+  return (<input
+    type='checkbox'
+    checked={flag(name)}
+    onChange={ev => flag(name, JSON.parse(ev.target.checked))} />)
+}
+
+const style = {
+  borderBottomLeftRadius: '.5rem',
+  padding: '.5rem',
+  position: 'absolute',
+  zIndex: 1000,
+  color: 'white',
+  background: 'rgba(74,68,90,0.85)',
+  top: 0,
+  right: 0
+}
+
+const human = name => {
+  return name.replace(/[a-z][A-Z]/g, str => str[0] + ' ' + str[1].toLowerCase())
+}
+
+export default class extends React.Component {
+  render () {
+    return (flag('switcher') && <div className='flag-switcher' style={style}>
+      <button onClick={() => this.setState({date: Date.now()})}>refresh</button>
+      { flag.list().map(name => {
+        return (<div>
+          { human(name) } : <FlagInput name={name} />
+        </div>)
+      }) }
+    </div>)
+  }
+}
