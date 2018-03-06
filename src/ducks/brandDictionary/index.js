b99d29f279c47777fa9641e46c61c20a82b36f79
Simon Constans <simonc@linux.com>
2018-03-06 17:20:26 +0100

    feat: Add brands duck 🦆

---
+++ src/ducks/brandDictionary/index.js
@@ -0,0 +1,18 @@
+import { some, find } from 'lodash'
+import brands from './brands'
+
+const getRegexp = brand => {
+  return new RegExp(brand.regexp, 'i')
+}
+
+export const getBrands = filterFct => filterFct ? brands.filter(filterFct) : brands
+
+export const findMatchingBrand = (brands, label) => {
+  return find(brands, brand => getRegexp(brand).test(label))
+}
+
+export const matchBrands = (brands, label) => {
+  return some(brands, brand => getRegexp(brand).test(label))
+}
+
+export default findMatchingBrand
---
+++ src/ducks/brandDictionary/index.spec.js
@@ -0,0 +1,36 @@
+import { getBrands, matchBrands, findMatchingBrand } from '.'
+import brands from './brands'
+
+const getFilteredBrands = () => getBrands(brand => brand.name === 'Filtered')
+
+describe('brandDictionary', () => {
+  describe('getBrands', () => {
+    it('Should return brands', () => {
+      expect(getBrands()).toBe(brands)
+    })
+    it('Should return brands filtered', () => {
+      expect(getFilteredBrands()).toEqual([])
+    })
+  })
+
+  describe('matchBrands', () => {
+    it('Should return true if label match with regex', () => {
+      expect(matchBrands(getBrands(), 'Cpam Des Yvelines')).toBeTruthy()
+      expect(matchBrands(getBrands(), 'Gandi Paris')).toBeFalsy()
+    })
+
+    it('Should return false if brand is filtered', () => {
+      expect(matchBrands(getFilteredBrands(), 'Cpam Des Yvelines')).toBeFalsy()
+    })
+  })
+
+  describe('findMatchingBrand', () => {
+    it('Should return brand information if label match with regex', () => {
+      expect(findMatchingBrand(getBrands(), 'Gandi Paris')).toBeFalsy()
+      expect(findMatchingBrand(getBrands(), 'Cpam Des Yvelines').konnectorSlug).toBe('ameli')
+      expect(findMatchingBrand(getBrands(), 'Free Telecom').konnectorSlug).toBe('free')
+      expect(findMatchingBrand(getBrands(), 'Free HautDebit').konnectorSlug).toBe('free')
+      expect(findMatchingBrand(getBrands(), 'Free Mobile').konnectorSlug).toBe('freemobile')
+    })
+  })
+})
