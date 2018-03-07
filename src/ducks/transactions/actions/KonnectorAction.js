cb0045d9d31fabc180d6141c3df50152c895873a
Simon Constans <simonc@linux.com>
2018-03-07 13:06:41 +0100

    feat: Add konnector CTA on mobile 📞

--- src/ducks/transactions/actions/KonnectorAction.js
+++ src/ducks/transactions/actions/KonnectorAction.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { flowRight as compose } from 'lodash'
 import { matchBrands, findMatchingBrand } from 'ducks/brandDictionary'
-import { IntentOpener, translate } from 'cozy-ui/react'
+import { IntentOpener, translate, withBreakpoints } from 'cozy-ui/react'
 import styles from '../TransactionActions.styl'
 import palette from 'cozy-ui/stylus/settings/palette.json'
 const PRIMARY_ACTION_COLOR = palette['dodgerBlue']
@@ -11,22 +12,37 @@ export const match = ({ transaction, brands }) => {
   return matchBrands(brands, transaction.label)
 }
 
-const Action = ({t, transaction, brands}) => {
+const Action = ({t, transaction, brands, urls, breakpoints: { isDesktop }}) => {
   const brand = findMatchingBrand(brands, transaction.label)
   if (!brand) return
 
+  if (isDesktop) {
+    return (
+      <IntentOpener
+        action='CREATE'
+        doctype='io.cozy.accounts'
+        options={{slug: brand.konnectorSlug}}
+      >
+        <a
+          style={{ color: PRIMARY_ACTION_COLOR }}
+          className={styles.TransactionAction}
+        >{t('Transactions.actions.konnector', {vendor: brand.name})}</a>
+      </IntentOpener>
+    )
+  }
+
+  const collectUrl = `${urls['COLLECT']}providers/all/${brand.konnectorSlug}`
+
   return (
-    <IntentOpener
-      action='CREATE'
-      doctype='io.cozy.accounts'
-      options={{slug: brand.konnectorSlug}}
-    >
-      <a
-        style={{ color: PRIMARY_ACTION_COLOR }}
-        className={styles.TransactionAction}
-      >{t('Transactions.actions.konnector', {vendor: brand.name})}</a>
-    </IntentOpener>
+    <a
+      href={collectUrl}
+      style={{ color: PRIMARY_ACTION_COLOR }}
+      className={styles.TransactionAction}
+    >{t('Transactions.actions.konnector', {vendor: brand.name})}</a>
   )
 }
 
-export default translate()(Action)
+export default compose(
+  withBreakpoints(),
+  translate()
+)(Action)
