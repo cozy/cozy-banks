c4e86d806b33188ccfc4fc2753b5f6bdd962ad57
Cyrille Perois <cperois@outlook.com>
2018-04-13 17:59:26 +0200

    feat: use new action button in HealthExpenseStatusAction

--- src/ducks/transactions/TransactionActions.jsx
+++ src/ducks/transactions/TransactionActions.jsx
@@ -9,19 +9,12 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Icon, MenuItem } from 'cozy-ui/react'
+import { MenuItem } from 'cozy-ui/react'
 import palette from 'cozy-ui/stylus/settings/palette.json'
 import { findMatchingActions } from './actions'
 
 // TODO delete or rename this variable (see https://gitlab.cozycloud.cc/labs/cozy-bank/merge_requests/237)
 const PRIMARY_ACTION_COLOR = palette.dodgerBlue
-
-const MenuIcon = ({ action, transaction, actionProps, color }) => {
-  if (action.getIcon) {
-    return action.getIcon({ action, transaction, actionProps })
-  }
-  return <Icon icon={action.icon} color={color} />
-}
 
 const MenuAction = ({
   action,
@@ -39,14 +32,6 @@ const MenuAction = ({
       className={className}
       disabled={action.disabled}
       onClick={actionProps.onClick}
-      icon={
-        <MenuIcon
-          action={action}
-          transaction={transaction}
-          actionProps={actionProps}
-          color={color}
-        />
-      }
     >
       {displayComponent && (
         <Component
