5adf7f42c2ca2e166996f9617da4b4bcf9590887
Cyrille Perois <cperois@outlook.com>
2018-05-07 11:39:03 +0200

    fix: react-hint@3.0.0 had breaking changes (#112)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 /* global __DEVELOPMENT__ */
 import React from 'react'
-import ReactHint from 'react-hint'
+import ReactHintFactory from 'react-hint'
 import 'react-hint/css/index.css'
 import { Layout, Content } from 'components/Layout'
 import { Sidebar } from 'cozy-ui/react'
 import Nav from 'ducks/commons/Nav'
 import FlagSwitcher from 'components/FlagSwitcher'
 import styles from './App.styl'
+
+const ReactHint = ReactHintFactory(React)
 
 const App = ({ children }) => (
   <Layout>
