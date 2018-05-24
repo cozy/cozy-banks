cozy/cozy-banks

commit aeb0a471ffbceca245ba8f1be40750519b382b0c

🔧 chore: use components from cozy-ui

src/ducks/onboarding/Onboarding.jsx
@@ -11,7 +11,15 @@ import watch from 'assets/icons/icon-watch.svg'
 import cozy from 'assets/icons/icon-cozy.svg'
 import palette from 'cozy-ui/stylus/settings/palette.json'
 
-import { Hero, Sections, Section, Subtitle, Paragraph, Icon, CTA } from './Hero'
+import {
+  Hero,
+  Sections,
+  Section,
+  Subtitle,
+  Paragraph,
+  Icon,
+  CTA
+} from 'cozy-ui/react/Hero'
 
 class Onboarding extends Component {
   render() {

src/ducks/onboarding/Hero.jsx
@@ -1,16 +1,0 @@
-import React from 'react'
-import { Icon as BaseIcon } from 'cozy-ui/react'
-import styles from './Hero.styl'
-import mkComponent from 'utils/mkComponent'
-
-export const Icon = ({ color, icon }) => (
-  <BaseIcon width="auto" height="5rem" style={{ color }} icon={icon} />
-)
-
-export const Hero = mkComponent('div', { className: styles.Hero })
-export const Title = mkComponent('h2', { className: styles.Hero__title })
-export const Subtitle = mkComponent('h3', { className: styles.Hero__subtitle })
-export const Section = mkComponent('div', { className: styles.Hero__section })
-export const Sections = mkComponent('div', { className: styles.Hero__sections })
-export const Paragraph = mkComponent('p', {})
-export const CTA = mkComponent('p', { className: styles.Hero__cta })

src/ducks/onboarding/Hero.styl
@@ -1,36 +1,0 @@
-@require 'settings/breakpoints'
-
-.Hero
-    margin-left auto
-    margin-right auto
-
-    &__title
-        text-align center
-        margin-bottom 3rem
-
-    &__subtitle
-        margin-bottom 0
-
-    p
-        line-height 1.25
-
-    &__sections
-        display flex
-        justify-content space-around
-
-        +small-screen()
-            flex-direction column
-            align-items center
-
-    &__section
-        max-width 18.75rem
-        text-align center
-
-        +small-screen()
-            max-width auto
-
-            &:nth-child(2), &:nth-child(3)
-                display none
-
-    &__cta
-        text-align center

commit 761d24424be407f1b95965b1c12cf605fe50a9a4

🐝 fix: only show 1 onboarding section on mobile
broken since aeb0a471ffbceca245ba8f1be40750519b382b0c

src/ducks/onboarding/Onboarding.jsx
@@ -41,16 +41,22 @@ class Onboarding extends Component {
             <Subtitle>{t('Onboarding.manage-budget.title')}</Subtitle>
             <Paragraph>{t('Onboarding.manage-budget.description')}</Paragraph>
           </Section>
-          <Section>
-            <Icon color={palette.portage} icon={watch} />
-            <Subtitle>{t('Onboarding.save-time.title')}</Subtitle>
-            <Paragraph>{t('Onboarding.save-time.description')}</Paragraph>
-          </Section>
-          <Section>
-            <Icon color={palette['dodgerBlue']} icon={cozy} />
-            <Subtitle>{t('Onboarding.cozy-assistant.title')}</Subtitle>
-            <Paragraph>{t('Onboarding.cozy-assistant.description')}</Paragraph>
-          </Section>
+          {!isMobile ? (
+            <Section>
+              <Icon color={palette.portage} icon={watch} />
+              <Subtitle>{t('Onboarding.save-time.title')}</Subtitle>
+              <Paragraph>{t('Onboarding.save-time.description')}</Paragraph>
+            </Section>
+          ) : null}
+          {!isMobile ? (
+            <Section>
+              <Icon color={palette['dodgerBlue']} icon={cozy} />
+              <Subtitle>{t('Onboarding.cozy-assistant.title')}</Subtitle>
+              <Paragraph>
+                {t('Onboarding.cozy-assistant.description')}
+              </Paragraph>
+            </Section>
+          ) : null}
         </Sections>
         <CTA>
           <CollectLink>