aa61df014b6706a3b9e0c6e70bf77baecf3a5f9e
Patrick Browne <pt.browne@gmail.com>
2018-06-06 19:32:32 +0200

    dev: add temporary box-shadow when hot-reloading to know that hot-reloading has been done

--- src/main.jsx
+++ src/main.jsx
@@ -85,6 +85,18 @@ document.addEventListener('DOMContentLoaded', () => {
   loadState().then(setupApp)
 })
 
+const makeItShine = node => {
+  node.style.boxShadow = 'inset 0px 0px 20px yellow'
+  node.style.transition = 'box-shadow 0.3s ease'
+  setTimeout(() => {
+    node.style.boxShadow = 'inset 0 0 0'
+  }, 2000)
+}
 if (module.hot) {
-  module.hot.accept('./AppContainer', () => requestAnimationFrame(initRender))
+  module.hot.accept('./AppContainer', () =>
+    requestAnimationFrame(() => {
+      makeItShine(document.body)
+      initRender()
+    })
+  )
 }
