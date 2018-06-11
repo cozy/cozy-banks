ce6f62186a0129a49e8ba868ded21df99940e001
Patrick Browne <pt.browne@gmail.com>
2018-06-11 11:48:15 +0200

    🔧 chore: add required property to AppLike

--- test/AppLike.jsx
+++ test/AppLike.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { I18n } from 'cozy-ui/react'
 import { Provider } from 'react-redux'
 import langEn from 'locales/en.json'
+import PropTypes from 'prop-types'
 
 const AppLike = ({ children, store }) => (
   <I18n lang={'en'} dictRequire={() => langEn}>
     <Provider store={store}>{children}</Provider>
   </I18n>
 )
+
+AppLike.propTypes = {
+  store: PropTypes.object.isRequired
+}
 
 export default AppLike
