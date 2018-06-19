47286d1cc68a5f37943915c1b2b2921197a2b3a3
François <francois@cozycloud.cc>
2018-06-19 17:02:54 +0200

    corrected a bug caused by missing trailing spaces around a tag

--- src/ducks/categorization/index.js
+++ src/ducks/categorization/index.js
@@ -1,6 +1,6 @@
 import bayes from 'classificator'
 
-const DATE_TAG = 'tag_date'
+const DATE_TAG = ' tag_date '
 const DATE_REGEX = /\d{1,2}\/\d{1,2}\/\d{2,4}|\d{1,2}\/\d{1,2}/g
 const UNNECESSARY_CHARS_REGEX = /[^a-zA-Z_ ]/g
 const MAX_WORD = 3
