6c57369320ce54dfb664eb74b8b6588b11212aa7
Patrick Browne <pt.browne@gmail.com>
2018-06-20 14:06:34 +0200

    🐝 fix: tweak mobile styles to increase click area

--- src/components/Select/styleUtils.spec.js
+++ src/components/Select/styleUtils.spec.js
@@ -2,19 +2,21 @@ import { mergeStyles } from './styleUtils'
 
 describe('style utils', () => {
   it('should correctly compute styles', () => {
-    const base = { border: 1, color: 'back'}
-    const styleObj = mergeStyles({
-      container: base => ({
-        ...base,
-        background: 'blue'
-      }),
-    },
-    {
-      container: base => ({
-        background: 'red',
-        color: 'yellow'
-      })
-    })
+    const base = { border: 1, color: 'back' }
+    const styleObj = mergeStyles(
+      {
+        container: base => ({
+          ...base,
+          background: 'blue'
+        })
+      },
+      {
+        container: () => ({
+          background: 'red',
+          color: 'yellow'
+        })
+      }
+    )
     expect(styleObj.container(base)).toMatchObject({
       background: 'red',
       color: 'yellow',
