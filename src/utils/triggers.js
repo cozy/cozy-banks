ccf0ef6333f879266f1e571ccf0ccb9f8ff52e2d
Patrick Browne <pt.browne@gmail.com>
2018-06-27 23:06:39 +0200

    🐝 fix: some trigger do not have any message

--- src/utils/triggers.js
+++ src/utils/triggers.js
@@ -3,7 +3,7 @@ const { logInfo } = require('lib/sentry')
 const getKonnectorFromTrigger = trigger => {
   if (trigger.message && trigger.message.konnector) {
     return trigger.message.konnector
-  } else if (trigger.message.Data) {
+  } else if (trigger.message && trigger.message.Data) {
     // Legacy triggers
     logInfo('Legacy trigger detected')
     const message = JSON.parse(atob(trigger.message.Data))
