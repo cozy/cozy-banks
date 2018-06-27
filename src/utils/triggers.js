9f9d95f83774c6f380b1e5812ef3880e68ef0d45
Patrick Browne <pt.browne@gmail.com>
2018-06-27 18:28:56 +0200

    ✨ feat: log info to sentry when detecting legacy trigger

--- src/utils/triggers.js
+++ src/utils/triggers.js
@@ -1,8 +1,11 @@
+const { logInfo } = require('lib/sentry')
+
 const getKonnectorFromTrigger = trigger => {
   if (trigger.message && trigger.message.konnector) {
     return trigger.message.konnector
   } else if (trigger.message.Data) {
     // Legacy triggers
+    logInfo('Legacy trigger detected')
     const message = JSON.parse(atob(trigger.message.Data))
     return message.konnector
   }
