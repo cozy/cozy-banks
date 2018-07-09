894e12121cdf8c579ffd3a40b35578fc12d1d2f5
Simon Constans <simonc@linux.com>
2018-07-09 15:50:56 +0200

    feat: Add Settings on offline doctype 😎

--- src/doctypes.js
+++ src/doctypes.js
@@ -2,5 +2,13 @@ export const RECIPIENT_DOCTYPE = 'io.cozy.mocks.recipients'
 export const ACCOUNT_DOCTYPE = 'io.cozy.bank.accounts'
 export const GROUP_DOCTYPE = 'io.cozy.bank.groups'
 export const TRANSACTION_DOCTYPE = 'io.cozy.bank.operations'
+export const SETTINGS_DOCTYPE = 'io.cozy.bank.settings'
 export const BILLS_DOCTYPE = 'io.cozy.bills'
 export const TRIGGER_DOCTYPE = 'io.cozy.triggers'
+
+export const offlineDoctypes = [
+  ACCOUNT_DOCTYPE,
+  GROUP_DOCTYPE,
+  TRANSACTION_DOCTYPE,
+  SETTINGS_DOCTYPE
+]
