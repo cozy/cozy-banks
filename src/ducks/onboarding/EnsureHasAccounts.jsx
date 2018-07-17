f044c2db57d508f08628091ced5432b8ec47e683
Simon Constans <simonc@linux.com>
2018-07-17 18:34:54 +0200

    feat: Load every 30s accounts on Onboarding 🎣

--- src/ducks/onboarding/EnsureHasAccounts.jsx
+++ src/ducks/onboarding/EnsureHasAccounts.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { cozyConnect, fetchCollection } from 'cozy-client'
+import React, { Component } from 'react'
+import { fetchCollection, getCollection } from 'cozy-client'
+import { connect } from 'react-redux'
 import { ACCOUNT_DOCTYPE } from 'doctypes'
 import { flowRight as compose } from 'lodash'
 import Loading from 'components/Loading'
@@ -9,6 +10,11 @@ import styles from './Onboarding.styl'
 import stylesLayout from 'styles/main.styl'
 import { Layout, Main, Content } from 'cozy-ui/react/Layout'
 import { isCollectionLoading } from 'utils/client'
+
+export const getAccounts = state => {
+  const col = getCollection(state, 'onboarding_accounts')
+  return (col && col.data) || []
+}
 
 const hasParameter = (qs, param) => {
   // result of querystring parsing is created without prototype
@@ -27,41 +33,75 @@ const LayoutContent = props => (
 /**
  * Replaces its children by Onboarding if we have no accounts
  */
-const EnsureHasAccounts = props => {
-  const { children, accounts, location } = props
-
-  if (isCollectionLoading(accounts)) {
-    return (
-      <LayoutContent>
-        <div className={styles.Onboarding__loading}>
-          <Loading />
-        </div>
-      </LayoutContent>
-    )
+class EnsureHasAccounts extends Component {
+  state = {
+    intervalId: false
   }
 
-  if (
-    (accounts.data && accounts.data.length === 0) ||
-    hasParameter(location.query, 'onboarding')
-  ) {
-    return (
-      <LayoutContent>
-        <Onboarding />
-      </LayoutContent>
-    )
+  startInterval = () => {
+    const { accounts, fetchAccounts } = this.props
+    if (accounts.length === 0 && !this.state.intervalId) {
+      const intervalId = setInterval(() => fetchAccounts(), 30000)
+      this.setState({ intervalId })
+    }
   }
 
-  return children
+  stopInterval = () => {
+    const { intervalId } = this.state
+    if (intervalId) {
+      clearInterval(intervalId)
+      this.setState({ intervalId: false })
+    }
+  }
+
+  componentDidMount() {
+    this.startInterval()
+  }
+
+  componentWillUnmount() {
+    this.stopInterval()
+  }
+
+  render() {
+    const { children, accounts, accountsCollection, location } = this.props
+
+    if (isCollectionLoading(accountsCollection)) {
+      return (
+        <LayoutContent>
+          <div className={styles.Onboarding__loading}>
+            <Loading />
+          </div>
+        </LayoutContent>
+      )
+    }
+
+    if (
+      (accounts && accounts.length === 0) ||
+      hasParameter(location.query, 'onboarding')
+    ) {
+      return (
+        <LayoutContent>
+          <Onboarding />
+        </LayoutContent>
+      )
+    }
+
+    this.stopInterval()
+
+    return children
+  }
 }
 
-const mapDocumentsToProps = () => ({
-  // Must put things in another collection when there is a parent-child
-  // relationships between components that fetch the same thing. Need
-  // to see with goldoraf how to fetch only once so that we can use
-  // the same collection "accounts" than in the settings.
-  accounts: fetchCollection('onboarding_accounts', ACCOUNT_DOCTYPE)
+const mapStateToProps = state => ({
+  accounts: getAccounts(state)
 })
 
-export default compose(withRouter, cozyConnect(mapDocumentsToProps))(
-  EnsureHasAccounts
-)
+const mapDispatchToProps = dispatch => ({
+  fetchAccounts: () =>
+    dispatch(fetchCollection('onboarding_accounts', ACCOUNT_DOCTYPE))
+})
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps, mapDispatchToProps)
+)(EnsureHasAccounts)
