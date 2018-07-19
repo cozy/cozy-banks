c14fe1832b8506a82744dc6ee9eab4bb570e4d3b
Patrick Browne <pt.browne@gmail.com>
2018-07-19 23:03:50 +0200

    ✨ feat: wrap AppContainer into a CozyProvider

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -2,6 +2,7 @@
 
 import { I18n } from 'cozy-ui/react/I18n'
 import React from 'react'
+import CozyClient, { CozyProvider } from 'cozy-client'
 
 const AppContainer = ({ store, lang, history, client }) => {
   const AppRoute = require('components/AppRoute').default
@@ -13,12 +14,18 @@ const AppContainer = ({ store, lang, history, client }) => {
     __TARGET__ === 'mobile'
       ? require('ducks/authentication/MobileRouter').default
       : require('react-router').Router
+  const newCozyClient = new CozyClient({
+    uri: client.facade.url,
+    token: client.options.token
+  })
   return (
-    <I18n lang={lang} dictRequire={lang => require(`locales/${lang}`)}>
-      <StoreProvider store={store} client={client}>
-        <Router history={history} routes={AppRoute} />
-      </StoreProvider>
-    </I18n>
+    <CozyProvider client={newCozyClient}>
+      <I18n lang={lang} dictRequire={lang => require(`locales/${lang}`)}>
+        <StoreProvider store={store} client={client}>
+          <Router history={history} routes={AppRoute} />
+        </StoreProvider>
+      </I18n>
+    </CozyProvider>
   )
 }
 
