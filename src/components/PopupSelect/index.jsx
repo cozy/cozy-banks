3f584f137c0a8745c4cf9d3f90f1067c20344922
Patrick Browne <pt.browne@gmail.com>
2018-07-30 19:23:01 +0200

    🔨 refactor: extract PopupTitle

--- src/components/PopupSelect/index.jsx
+++ src/components/PopupSelect/index.jsx
@@ -6,25 +6,37 @@ import palette from 'cozy-ui/stylus/settings/palette.json'
 
 import styles from './styles.styl'
 
-const PopupRow = ({ isSelected, icon, title, hasArrow, onClick }) =>
-  console.log('popup row', title) || (
-    <Media
-      className={cx(
-        styles.PopupSelect__row,
-        'u-ph-1 u-pv-half',
-        isSelected ? ' u-text-bold' : ''
-      )}
-      onClick={onClick}
-    >
-      {icon && <Img className="u-pr-1">{icon}</Img>}
-      <Bd className="u-ellipsis">{title}</Bd>
-      {hasArrow && (
-        <Img className="u-pl-1">
-          <Icon icon="forward" color={palette['coolGrey']} />
-        </Img>
-      )}
-    </Media>
-  )
+const PopupTitle = ({ showBack, onClickBack, title }) => (
+  <Media>
+    {showBack && (
+      <Img className={styles.PopupSelect__back} onClick={onClickBack}>
+        <Icon icon="back" color={palette['coolGrey']} />
+      </Img>
+    )}
+    <Bd>
+      <h2>{title}</h2>
+    </Bd>
+  </Media>
+)
+
+const PopupRow = ({ isSelected, icon, title, hasArrow, onClick }) => (
+  <Media
+    className={cx(
+      styles.PopupSelect__row,
+      'u-ph-1 u-pv-half',
+      isSelected ? ' u-text-bold' : ''
+    )}
+    onClick={onClick}
+  >
+    {icon && <Img className="u-pr-1">{icon}</Img>}
+    <Bd className="u-ellipsis">{title}</Bd>
+    {hasArrow && (
+      <Img className="u-pl-1">
+        <Icon icon="forward" color={palette['coolGrey']} />
+      </Img>
+    )}
+  </Media>
+)
 
 class PopupSelect extends Component {
   constructor(props) {
@@ -44,6 +56,7 @@ class PopupSelect extends Component {
   }
 
   handleSelect = item => {
+    console.log('handleSelect', handleSelect)
     if (item.children && item.children.length > 0) {
       this.setState({
         history: [item, ...this.state.history]
@@ -53,50 +66,34 @@ class PopupSelect extends Component {
     }
   }
 
-  renderList = () => {
-    const children = this.state.history[0].children
-    return (
-      <div className={styles.PopupSelect__content}>
-        {children.map(item => (
-          <PopupRow
-            key={item.title}
-            isSelected={this.props.isSelected(item)}
-            icon={item.icon}
-            title={item.title}
-            onClick={() => this.handleSelect(item)}
-            hasArrow={item.children && item.children.length > 0}
-          />
-        ))}
-      </div>
-    )
-  }
-
-  renderTitle = () => {
-    const { history } = this.state
-    const item = history[0]
-    return (
-      <Media>
-        {history &&
-          history.length > 1 && (
-            <Img className={styles.PopupSelect__back} onClick={this.handleBack}>
-              <Icon icon="back" color={palette['coolGrey']} />
-            </Img>
-          )}
-        <Bd>
-          <h2>{item.title}</h2>
-        </Bd>
-      </Media>
-    )
-  }
-
   render() {
+    const { history } = this.state
+    const current = history[0]
+    const children = current.children || []
     return (
       <Modal overflowHidden dismissAction={this.props.onCancel} into="body">
         <div className={styles.PopupSelect__title}>
-          <ModalHeader>{this.renderTitle()}</ModalHeader>
+          <ModalHeader>
+            <PopupTitle
+              title={current.title}
+              showBack={history.length > 1}
+              onClickBack={this.handleBack}
+            />
+          </ModalHeader>
         </div>
         <ModalDescription className="u-pb-0">
-          {this.renderList()}
+          <div className={styles.PopupSelect__content}>
+            {children.map(item => (
+              <PopupRow
+                key={item.title}
+                isSelected={this.props.isSelected(item)}
+                icon={item.icon}
+                title={item.title}
+                onClick={() => this.handleSelect(item)}
+                hasArrow={item.children && item.children.length > 0}
+              />
+            ))}
+          </div>
         </ModalDescription>
       </Modal>
     )
