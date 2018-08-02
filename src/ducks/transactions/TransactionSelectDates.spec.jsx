fde7476da94e606796d43f0ba03800c6ea00b49f
Patrick Browne <pt.browne@gmail.com>
2018-08-02 17:37:40 +0200

    🐝 fix(test): update snap

--- src/ducks/transactions/TransactionSelectDates.spec.jsx
+++ src/ducks/transactions/TransactionSelectDates.spec.jsx
@@ -3,6 +3,7 @@ import fixtures from 'test/fixtures'
 
 const transactions = fixtures['io.cozy.bank.operations']
 
+
 describe('options from select dates', () => {
   it('should compute correctly', () => {
     expect(getOptions(transactions)).toMatchSnapshot()
