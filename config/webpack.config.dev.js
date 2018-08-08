ea6c0561d0a1c81675cb5f0c4bf7f38b4d06f8c2
Patrick Browne <pt.browne@gmail.com>
2018-08-08 18:20:05 +0200

    lint

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -6,17 +6,19 @@ module.exports = {
   devtool: 'cheap-source-map',
   externals: ['cozy'],
   module: {
-    rules: [{
-      test: require.resolve('cozy-bar/dist/cozy-bar.js'),
-      loader: 'imports-loader?css=./cozy-bar.css'
-    }]
+    rules: [
+      {
+        test: require.resolve('cozy-bar/dist/cozy-bar.js'),
+        loader: 'imports-loader?css=./cozy-bar.css'
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
       __STACK_ASSETS__: false,
       __DEVELOPMENT__: true,
       'process.env': {
-        'NODE_ENV': JSON.stringify('development')
+        NODE_ENV: JSON.stringify('development')
       }
     }),
     new webpack.ProvidePlugin({
