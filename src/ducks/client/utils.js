7965e821cadde81f72fda38810ec6131917694af
Patrick Browne <pt.browne@gmail.com>
2018-09-16 12:19:26 +0200

    chore: Remove unused

--- src/ducks/client/utils.js
+++ src/ducks/client/utils.js
@@ -5,37 +5,6 @@ import { withMutations, Query } from 'cozy-client'
 
 export const links = {}
 
-export const withQuery = (dest, queryOpts, Original) => {
-  if (!queryOpts) {
-    throw new Error(
-      `withQuery has no options for ${dest} (wrapping ${Original.name})`
-    )
-  }
-  return Component => {
-    const Wrapped = (props, context) => {
-      if (!context.client) {
-        throw new Error(
-          'Query should be used with client in context (use CozyProvider to set context)'
-        )
-      }
-
-      queryOpts = typeof queryOpts === 'function' ? queryOpts(props) : queryOpts
-      if (queryOpts.doc) {
-        return <Component {...{ [dest]: queryOpts.doc, ...props }} />
-      }
-
-      return (
-        <Query {...queryOpts}>
-          {result => {
-            return <Component {...{ [dest]: result, ...props }} />
-          }}
-        </Query>
-      )
-    }
-    return Wrapped
-  }
-}
-
 export const queryConnect = querySpecs => Component => {
   const enhancers = Object.keys(querySpecs).map(dest =>
     withQuery(dest, querySpecs[dest], Component)
