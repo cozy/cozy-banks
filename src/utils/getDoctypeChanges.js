64c4609e1e5aa86037c80a171f27385bf7c09277
Cyrille Perois <cperois@outlook.com>
2018-09-18 12:12:43 +0200

    feat: Add getDoctypeChanges util

---
+++ src/utils/getDoctypeChanges.js
@@ -0,0 +1,28 @@
+import { cozyClient, log } from 'cozy-konnector-libs'
+
+export default async function getDoctypeChanges(doctype, since) {
+  // lastSeq = '0' // Useful for debug
+  log('info', `Get ${doctype} updates since ${since}`)
+  const result = await cozyClient.fetchJSON(
+    'GET',
+    `/data/${doctype}/_changes?include_docs=true&since=${since}`
+  )
+
+  const newLastSeq = result.last_seq
+  const documents = result.results
+    .map(x => {
+      if (!x.doc) {
+        log(
+          'warn',
+          `This response row doesn't contain a doc. Why?`,
+          JSON.stringify(x)
+        )
+      }
+      return x.doc
+    })
+    .filter(doc => doc._id.indexOf('_design') !== 0)
+    .filter(doc => !doc._deleted)
+    .filter(Boolean) // TODO find out why some documents are not returned
+
+  return { newLastSeq, documents }
+}
