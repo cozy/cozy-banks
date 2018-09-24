39571b7c2878445ba8471bae9d3bd0a98082cf6c
Patrick Browne <pt.browne@gmail.com>
2018-09-24 09:34:10 +0200

    chore: Update cozy-ui and cozy-bar

--- config/webpack.config.cozy-ui.js
+++ config/webpack.config.cozy-ui.js
@@ -6,30 +6,13 @@ const path = require('path')
 
 module.exports = {
   resolve: {
-    extensions: ['.styl']
+    extensions: ['.styl'],
+    alias: {
+      'cozy-ui/react': 'cozy-ui/transpiled/react'
+    }
   },
   module: {
-    rules: [
-      {
-        include: path.dirname(require.resolve('cozy-ui')),
-        test: /\.styl$/,
-        use: [
-         {
-            loader: MiniCssExtractPlugin.loader
-          },
-          'css-loader?importLoaders=2&modules&localIdentName=[name]_[local]_[hash:base64:5]',
-          'stylus-loader'
-        ]
-      },
-      {
-        test: /\.jsx?$/,
-        include: path.dirname(require.resolve('cozy-ui')),
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true
-        }
-      }
-    ]
+    rules: []
   },
   plugins: [
     new webpack.DefinePlugin({
