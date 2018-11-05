aa2bb1c876223174b3e5c6b30f02d90c943f3bf8
Simon Constans <simonc@linux.com>
2018-11-05 09:49:02 +0100

    feat: Use new matomo url

--- config/webpack.config.piwik.js
+++ config/webpack.config.piwik.js
@@ -6,7 +6,7 @@
        __PIWIK_SITEID__: 255, // TODO change for real value
        __PIWIK_SITEID_MOBILE__: 255,
        __PIWIK_DIMENSION_ID_APP__: 1,
-       __PIWIK_TRACKER_URL__: JSON.stringify('https://piwik.cozycloud.cc')
+       __PIWIK_TRACKER_URL__: JSON.stringify('https://matomo.cozycloud.cc')
      })
    ]
  }
