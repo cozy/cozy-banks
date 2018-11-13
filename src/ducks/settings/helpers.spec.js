aba77f656c9d6ff7bd15300e784a001ced53ff2f
Patrick Browne <pt.browne@gmail.com>
2018-11-13 15:48:51 +0100

    feat: Change default notification settings

---
+++ src/ducks/settings/helpers.spec.js
@@ -0,0 +1,23 @@
+const { fetchSettings } = require('./helpers')
+
+fdescribe('defaulted settings', () => {
+  it('should return defaulted settings', async () => {
+    const fakeClient = {
+      find: () => {},
+      query: () => {
+        return Promise.resolve({
+          data: [{
+            notifications: {
+              balanceLower: {
+                value: 600,
+                enabled: false
+              }
+            }
+          }]
+        })
+      }
+    }
+    const settings = await fetchSettings(fakeClient)
+    expect(settings).toMatchSnapshot()
+  })
+})
