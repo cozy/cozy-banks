3c750b3b896951a8c4d03d8a2af250ded367517e
Patrick Browne <pt.browne@gmail.com>
2018-11-20 13:36:31 +0100

    chore: Lint

--- src/ducks/notifications/html/testTemplates.js
+++ src/ducks/notifications/html/testTemplates.js
@@ -12,17 +12,23 @@ const readJSONSync = filename => {
 const EMAILS = {
   balanceLower: {
     template: require('./balance-lower-html').default,
-    data: readJSONSync('src/ducks/notifications/html/data/transactions-greater.json')
+    data: readJSONSync(
+      'src/ducks/notifications/html/data/transactions-greater.json'
+    )
   },
 
   healthBillLinked: {
     template: require('./health-bill-linked-html').default,
-    data: readJSONSync('src/ducks/notifications/html/data/health-bill-linked.json')
+    data: readJSONSync(
+      'src/ducks/notifications/html/data/health-bill-linked.json'
+    )
   },
 
   transactionGreater: {
     template: require('./transaction-greater-html').default,
-    data: readJSONSync('src/ducks/notifications/html/data/health-bill-linked.json')
+    data: readJSONSync(
+      'src/ducks/notifications/html/data/health-bill-linked.json'
+    )
   }
 }
 
@@ -44,22 +50,29 @@ const main = () => {
   const express = require('express')
   const app = express()
 
-  app.get('/:templateName/:lang', function (req, res) {
+  app.get('/:templateName/:lang', function(req, res) {
     const { templateName, lang } = req.params
     const nav = `
     <div>
-      ${Object.keys(EMAILS).map(name => `${name}: <a href="/${name}/fr">fr</a>, <a href="/${name}/en">en</a><br/>`).join('  ')}
+      ${Object.keys(EMAILS)
+        .map(
+          name =>
+            `${name}: <a href="/${name}/fr">fr</a>, <a href="/${name}/en">en</a><br/>`
+        )
+        .join('  ')}
     </div>
     `
     res.send(nav + '<br/><br/>' + renderTemplate(templateName, lang))
   })
 
-  app.get('/', function (req, res) {
+  app.get('/', function(req, res) {
     res.redirect(`/${Object.keys(EMAILS)[0]}/fr`)
   })
 
   const port = 8081
-  app.listen(port, () => console.log(`Rendering emails at http://localhost:${port}!`))
+  app.listen(port, () =>
+    console.log(`Rendering emails at http://localhost:${port}!`)
+  )
 }
 
 main(process.argv)
