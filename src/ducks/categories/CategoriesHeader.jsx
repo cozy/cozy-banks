5829126684f8b8ddafa861e440428414a7ac7b6c
Simon Constans <simonc@linux.com>
2018-12-11 13:53:43 +0100

    refactor: Separation of components

--- src/ducks/categories/CategoriesHeader.jsx
+++ src/ducks/categories/CategoriesHeader.jsx
@@ -1,9 +1,11 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Toggle, translate, withBreakpoints } from 'cozy-ui/react'
 import { Breadcrumb } from 'components/Breadcrumb'
 import { AccountSwitch } from 'ducks/account'
 import BackButton from 'components/BackButton'
+import Header from 'components/Header'
+import { Padded } from 'components/Spacing'
 import { ConnectedSelectDates as SelectDates } from 'components/SelectDates'
 import CategoriesChart from './CategoriesChart'
 import { getTransactionsTotal, getGlobalCurrency } from './helpers'
@@ -11,65 +13,104 @@ import { flowRight as compose } from 'lodash'
 import styles from './CategoriesHeader.styl'
 
 class CategoriesHeader extends PureComponent {
-  render() {
+  renderAccountSwitch = () => {
+    const { selectedCategory, breadcrumbItems } = this.props
+    const [previousItem] = breadcrumbItems.slice(-2, 1)
+
+    return (
+      <Fragment>
+        <AccountSwitch small={selectedCategory !== undefined} />
+        {selectedCategory && (
+          <BackButton onClick={previousItem && previousItem.onClick} />
+        )}
+      </Fragment>
+    )
+  }
+
+  renderIncomeToggle = () => {
     const {
-      breadcrumbItems,
       selectedCategory,
       withIncome,
       onWithIncomeToggle,
+      categories,
+      t
+    } = this.props
+    const hasData =
+      categories.length > 0 && categories[0].transactionsNumber > 0
+    const showIncomeToggle = hasData && selectedCategory === undefined
+
+    if (!showIncomeToggle) {
+      return null
+    }
+
+    return (
+      <div className={styles.CategoriesHeader__Toggle}>
+        <Toggle
+          id="withIncome"
+          checked={withIncome}
+          onToggle={onWithIncomeToggle}
+        />
+        <label htmlFor="withIncome">
+          {t('Categories.filter.includeIncome')}
+        </label>
+      </div>
+    )
+  }
+
+  renderChart = () => {
+    const {
+      selectedCategory,
       chartSize = 182,
       categories,
       t,
-      breakpoints: { isMobile },
       isFetching
     } = this.props
-
     const globalCurrency = getGlobalCurrency(categories)
     const transactionsTotal = getTransactionsTotal(categories)
-    const [previousItem] = breadcrumbItems.slice(-2, 1)
-    const hasData = categories.length > 0 && categories[0].transactionsNumber > 0
-    const showIncomeToggle = hasData && selectedCategory === undefined
+
+    if (isFetching) {
+      return null
+    }
+
+    return (
+      <CategoriesChart
+        width={chartSize}
+        height={chartSize}
+        categories={
+          selectedCategory ? selectedCategory.subcategories : categories
+        }
+        selectedCategory={selectedCategory}
+        total={selectedCategory ? selectedCategory.amount : transactionsTotal}
+        currency={globalCurrency}
+        label={t('Categories.title.total')}
+      />
+    )
+  }
+
+  render() {
+    const {
+      breadcrumbItems,
+      breakpoints: { isMobile }
+    } = this.props
+
+    const accountSwitch = this.renderAccountSwitch()
+    const incomeToggle = this.renderIncomeToggle()
+    const chart = this.renderChart()
 
     return (
       <div className={styles.CategoriesHeader}>
         <div>
-          <AccountSwitch small={selectedCategory !== undefined} />
-          {selectedCategory && (
-            <BackButton onClick={previousItem && previousItem.onClick} />
-          )}
+          {accountSwitch}
           <SelectDates showFullYear />
-          {!isMobile && breadcrumbItems.length > 1 ? (
+          {!isMobile && breadcrumbItems.length > 1 && (
             <Breadcrumb
               items={breadcrumbItems}
               className={styles.CategoriesHeader__Breadcrumb}
             />
-          ) : null}
-          {showIncomeToggle && (
-            <div className={styles.CategoriesHeader__Toggle}>
-              <Toggle
-                id="withIncome"
-                checked={withIncome}
-                onToggle={checked => onWithIncomeToggle(checked)}
-              />
-              <label htmlFor="withIncome">
-                {t('Categories.filter.includeIncome')}
-              </label>
-            </div>
           )}
+          {incomeToggle}
         </div>
-        {!isFetching && (
-          <CategoriesChart
-            width={chartSize}
-            height={chartSize}
-            categories={
-              selectedCategory ? selectedCategory.subcategories : categories
-            }
-            selectedCategory={selectedCategory}
-            total={selectedCategory ? selectedCategory.amount : transactionsTotal}
-            currency={globalCurrency}
-            label={t('Categories.title.total')}
-          />
-        )}
+        {chart}
       </div>
     )
   }
