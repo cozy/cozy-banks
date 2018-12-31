55f6bdba183de5baf0786c74b76367a96ee7549c
Cyrille Perois <cperois@outlook.com>
2018-12-31 17:13:37 +0100

    feat: Add styles for new accounts list

--- src/components/ExpansionPanel/ExpansionPanelDetails.js
+++ src/components/ExpansionPanel/ExpansionPanelDetails.js
@@ -1,3 +1,8 @@
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import { withStyles } from '@material-ui/core/styles'
 
-export default ExpansionPanelDetails
+export default withStyles(() => ({
+  root: {
+    padding: '0'
+  }
+}))(ExpansionPanelDetails)
--- src/ducks/balance/NewAccountsList.jsx
+++ src/ducks/balance/NewAccountsList.jsx
@@ -10,6 +10,33 @@ import ExpansionPanelDetails from 'components/ExpansionPanel/ExpansionPanelDetai
 
 import styles from './styles.styl'
 
+class AccountsList extends React.PureComponent {
+  static propTypes = {
+    accounts: PropTypes.arrayOf(PropTypes.object).isRequired
+  }
+
+  render() {
+    const { accounts } = this.props
+
+    return (
+      <ol className={styles.AccountsList}>
+        {sortBy(accounts, a => a.balance).map(a => (
+          <li key={a._id} className={styles.AccountsList__item}>
+            <span>{a.label}</span>
+            <Figure
+              currency="€"
+              total={a.balance}
+              className={styles.AccountsList__itemFigure}
+              totalClassName={styles.AccountsList__itemFigure}
+              currencyClassName={styles.AccountsList__itemFigure}
+            />
+          </li>
+        ))}
+      </ol>
+    )
+  }
+}
+
 class GroupPanel extends React.PureComponent {
   static propTypes = {
     group: PropTypes.object.isRequired
@@ -31,7 +58,9 @@ class GroupPanel extends React.PureComponent {
             currencyClassName={styles.GroupPannelSummary__figureCurrency}
           />
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails>Content</ExpansionPanelDetails>
+        <ExpansionPanelDetails>
+          <AccountsList accounts={group.accounts.data} />
+        </ExpansionPanelDetails>
       </ExpansionPanel>
     )
   }
