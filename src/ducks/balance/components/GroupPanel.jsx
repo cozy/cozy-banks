d28e36a545108bd0ea3ef123f525853f3363e4c3
Cyrille Perois <cperois@outlook.com>
2019-01-03 15:56:49 +0100

    feat(GroupPanel): Filter group on summary content click

--- src/ducks/balance/components/GroupPanel.jsx
+++ src/ducks/balance/components/GroupPanel.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Icon } from 'cozy-ui/react'
+import { flowRight as compose } from 'lodash'
+import { withRouter } from 'react-router'
 import { Figure } from 'components/Figure'
 import ExpansionPanel from 'components/ExpansionPanel/ExpansionPanel'
 import ExpansionPanelSummary from 'components/ExpansionPanel/ExpansionPanelSummary'
 import ExpansionPanelDetails from 'components/ExpansionPanel/ExpansionPanelDetails'
+import withFilteringDoc from 'components/withFilteringDoc'
 import AccountsList from './AccountsList'
 import { getGroupBalance } from '../helpers'
 import styles from './GroupPanel.styl'
 
 class GroupPanel extends React.PureComponent {
   static propTypes = {
-    group: PropTypes.object.isRequired
+    group: PropTypes.object.isRequired,
+    filterByDoc: PropTypes.func.isRequired,
+    router: PropTypes.object.isRequired
+  }
+
+  goToTransactionsFilteredByDoc = () => {
+    this.props.filterByDoc(this.props.group)
+    this.props.router.push('/transactions')
+  }
+
+  handleSummaryContentClick = e => {
+    e.stopPropagation()
+    this.goToTransactionsFilteredByDoc()
   }
 
   render() {
@@ -22,7 +37,10 @@ class GroupPanel extends React.PureComponent {
         <ExpansionPanelSummary
           expandIcon={<Icon icon="bottom" color="black" width={12} />}
         >
-          <div className={styles.GroupPanelSummary__content}>
+          <div
+            onClick={this.handleSummaryContentClick}
+            className={styles.GroupPanelSummary__content}
+          >
             {group.label}
             <Figure
               currency="€"
@@ -39,4 +57,7 @@ class GroupPanel extends React.PureComponent {
   }
 }
 
-export default GroupPanel
+export default compose(
+  withFilteringDoc,
+  withRouter
+)(GroupPanel)
