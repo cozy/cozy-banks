205c7ba8195b3ba0748655e4a6b0fa8b1f26ed24
Simon Constans <simonc@linux.com>
2019-01-16 15:50:47 +0100

    fix: Margin on settings

--- src/ducks/settings/Settings.jsx
+++ src/ducks/settings/Settings.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import {
   translate,
+  withBreakpoints,
   Tabs,
   TabPanels,
   TabPanel,
@@ -14,10 +15,11 @@ import { flowRight as compose } from 'lodash'
 import AppVersion from './AppVersion'
 import { PageTitle } from 'components/Title'
 import { Padded } from 'components/Spacing'
+import cx from 'classnames'
 
 const tabNames = ['configuration', 'accounts', 'groups']
 
-const Settings = ({ t, children, router }) => {
+const Settings = ({ t, children, router, breakpoints: { isMobile } }) => {
   let defaultTab = router.location.pathname.replace('/settings/', '')
   if (tabNames.indexOf(defaultTab) === -1) defaultTab = 'configuration'
 
@@ -32,22 +34,29 @@ const Settings = ({ t, children, router }) => {
   ))
 
   return (
-    <Padded>
-      <PageTitle className={styles.settings__title}>
-        {t('Settings.title')}
-      </PageTitle>
+    <React.Fragment>
+      <Padded className={cx({['u-p-0']: isMobile})}>
+        <PageTitle>
+          {t('Settings.title')}
+        </PageTitle>
+      </Padded>
       <Tabs className={styles['bnk-tabs']} initialActiveTab={defaultTab}>
-        <TabList className={styles['bnk-coz-tab-list']}>{tabs}</TabList>
+        <TabList className={styles['bnk-coz-tab-list']}>
+          {tabs}
+        </TabList>
         <TabPanels className={styles.TabPanels}>
-          <TabPanel active>{children}</TabPanel>
+          <Padded>
+            <TabPanel active>{children}</TabPanel>
+          </Padded>
         </TabPanels>
       </Tabs>
       {__TARGET__ === 'mobile' && <AppVersion version={__APP_VERSION__} />}
-    </Padded>
+    </React.Fragment>
   )
 }
 
 export default compose(
   withRouter,
-  translate()
+  translate(),
+  withBreakpoints()
 )(Settings)
