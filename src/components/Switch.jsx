7d2c6fcb3440677678f5b7c9c7d15919a5308076
Cyrille Perois <cperois@outlook.com>
2019-01-22 09:39:35 +0100

    feat: Internalize MUI Switch customization
    For now there are discussions about adding this customization to cozy-ui
    or not. So we add it only in Banks.

---
+++ src/components/Switch.jsx
@@ -0,0 +1,45 @@
+import Switch from '@material-ui/core/Switch'
+import { withStyles } from '@material-ui/core/styles'
+
+export default withStyles(() => ({
+  root: {
+    width: 40,
+    height: 23
+  },
+  switchBase: {
+    width: 25,
+    height: 23
+  },
+  checked: {
+    '& + $bar': {
+      opacity: 1
+    }
+  },
+  icon: {
+    width: 16,
+    height: 16
+  },
+  bar: {
+    width: 25,
+    height: 12,
+    marginTop: -6,
+    marginLeft: -12,
+    backgroundColor: 'var(--silver)',
+    opacity: 1
+  },
+  colorPrimary: {
+    '&$checked': {
+      color: 'white'
+    }
+  },
+  colorSecondary: {
+    '&$checked': {
+      color: 'white'
+    }
+  },
+  disabled: {
+    '&$switchBase': {
+      color: 'white'
+    }
+  }
+}))(Switch)
