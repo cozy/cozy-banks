ffe0856c63c20e16e8383f519d6728ec5defe823
Simon Constans <simonc@linux.com>
2019-01-24 11:29:32 +0100

    fix: Typo

--- src/components/KonnectorUpdateInfo/index.js
+++ src/components/KonnectorUpdateInfo/index.js
@@ -32,7 +32,7 @@ class KonnectorUpdateInfo extends React.PureComponent {
       this.setState({ url })
     } catch (err) {
       // eslint-disable-next-line no-console
-      console.warning('Error while retrieving redirection URL', err)
+      console.warn('Error while retrieving redirection URL', err)
     }
   }
 
