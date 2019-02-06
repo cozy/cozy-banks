c1998b5c60e1530de28a4f96be791fb65fffcb51
Simon Constans <simonc@linux.com>
2019-02-06 11:51:36 +0100

    refactor: Use lodash memoize

--- src/components/Chart/LineChart.jsx
+++ src/components/Chart/LineChart.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import * as d3 from 'd3'
-import { max, minBy, keyBy } from 'lodash'
-import memoize from 'memoize-one'
+import { max, minBy, keyBy, memoize } from 'lodash'
 import styles from './LineChart.styl'
 import Tooltip from './Tooltip'
 
