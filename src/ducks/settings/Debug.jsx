3f47ccba2ae3f1403096b1116036b721b5452b48
Patrick Browne <pt.browne@gmail.com>
2019-02-18 12:42:44 +0100

    feat: Expire token button in settings

--- src/ducks/settings/Debug.jsx
+++ src/ducks/settings/Debug.jsx
@@ -5,13 +5,28 @@ import { connect } from 'react-redux'
 class DebugSettings extends React.PureComponent {
   constructor(props) {
     super(props)
+    this.handleMakeTokenExpired = this.handleMakeTokenExpired.bind(this)
+  }
+
+  handleMakeTokenExpired() {
+    this.props.makeTokenExpired()
   }
 
   render() {
+    return (
+      <div>
+        <button onClick={this.handleMakeTokenExpired}>
+          Make token expired
+        </button>
+      </div>
+    )
   }
 }
 
 const mapDispatchToProps = dispatch => {
+  return {
+    makeTokenExpired: () => dispatch(expireToken())
+  }
 }
 
 export default connect(
