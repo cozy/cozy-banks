62aa451909b0799086cfab4f353c5caf46770a18
Cyrille Perois <cperois@outlook.com>
2019-02-19 09:41:24 +0100

    feat(Debug): Add an alert on token expiration button click

--- src/ducks/settings/Debug.jsx
+++ src/ducks/settings/Debug.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { expireToken } from 'ducks/mobile'
 import { connect } from 'react-redux'
+import { Alerter } from 'cozy-ui/react'
 
 class DebugSettings extends React.PureComponent {
   constructor(props) {
@@ -10,6 +11,7 @@ class DebugSettings extends React.PureComponent {
 
   handleMakeTokenExpired() {
     this.props.makeTokenExpired()
+    Alerter.info('Token expired')
   }
 
   render() {
