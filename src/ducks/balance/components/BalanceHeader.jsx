cd3fb9454c771bc43e0031b18ed4e25343c637bf
Simon Constans <simonc@linux.com>
2019-02-26 10:42:00 +0100

    refactor: Create BalanceHeader components

---
+++ src/ducks/balance/components/BalanceHeader.jsx
@@ -0,0 +1,54 @@
+import React, { memo } from 'react'
+import { flowRight as compose } from 'lodash'
+import { translate, withBreakpoints } from 'cozy-ui/react'
+
+import { Padded } from 'components/Spacing'
+import Header from 'components/Header'
+import { Figure } from 'components/Figure'
+import { PageTitle } from 'components/Title'
+import KonnectorUpdateInfo from 'components/KonnectorUpdateInfo'
+
+import styles from './BalanceHeader.styl'
+import History from '../History'
+
+const BalanceHeader = ({
+  t,
+  breakpoints: { isMobile },
+  accountsBalance,
+  accounts,
+  subtitleParams
+}) => {
+  const titlePaddedClass = isMobile ? 'u-p-0' : 'u-pb-0'
+  const titleColor = isMobile ? 'primary' : 'default'
+
+  return (
+    <Header className={styles.BalanceHeader} color="primary">
+      {isMobile && (
+        <Padded className={titlePaddedClass}>
+          <PageTitle color={titleColor}>{t('Balance.title')}</PageTitle>
+        </Padded>
+      )}
+      {accountsBalance !== undefined && (
+        <Figure
+          className={styles.BalanceHeader__currentBalance}
+          currencyClassName={styles.BalanceHeader__currentBalanceCurrency}
+          total={accountsBalance}
+          symbol="€"
+        />
+      )}
+      <div className={styles.BalanceHeader__subtitle}>
+        {subtitleParams
+          ? t('BalanceHistory.checked_accounts', subtitleParams)
+          : t('BalanceHistory.all_accounts')}
+      </div>
+      {accounts && <History accounts={accounts} />}
+      <KonnectorUpdateInfo />
+    </Header>
+  )
+}
+
+export default compose(
+  withBreakpoints(),
+  translate(),
+  memo
+)(BalanceHeader)
