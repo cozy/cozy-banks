7f42e1c01a1f26e802833d7b702db5bb51dc5ee4
Simon Constans <simonc@linux.com>
2019-03-11 15:51:53 +0100

    feat: Add desktop style

--- src/ducks/balance/components/NoAccount.jsx
+++ src/ducks/balance/components/NoAccount.jsx
@@ -1,19 +1,25 @@
 import React, { memo } from 'react'
+import { flowRight as compose } from 'lodash'
+import cx from 'classnames'
+import { translate, withBreakpoints } from 'cozy-ui/react'
 import Bouton from 'cozy-ui/react/Button'
 import HeaderTitle from 'ducks/balance/components/HeaderTitle'
-import noAccountImg from 'ducks/balance/components/NoAccount.svg'
 import AddAccountLink from 'ducks/settings/AddAccountLink'
 
 import styles from './NoAccount.styl'
 
-const NoAccount = () => {
+const NoAccount = ({ lang, t, breakpoints: { isMobile } }) => {
+  const timelineImg = require(`./timeline_${isMobile ? 'mobile' : 'desktop'}_${
+    lang === 'fr' ? 'fr' : 'en'
+  }.svg`)
   return (
-    <div className={styles.NoAccount}>
-      <div className={styles.NoAccount_center}>
-        <HeaderTitle balance={0} subtitle="Aucun compte" />
+    <div className={cx(styles.NoAccount, styles.VerticalContainer)}>
+      <div className={styles.NoAccount_title}>
+        <HeaderTitle balance={0} subtitle={t('Aucun compte')} />
       </div>
-      <div className={styles.NoAccount_chart}>
-        <img src={noAccountImg} alt="" />
+      <div className={styles.NoAccount_bottom}>
+        <div className={styles.NoAccount_chart} />
+        <img src={timelineImg} alt="" className={styles.NoAccount_timeline} />
       </div>
       <AddAccountLink>
         <Bouton
@@ -21,11 +27,15 @@ const NoAccount = () => {
           icon="plus"
           size="large"
           className={styles.NoAccount_addButton}
-          label="Ajouter une banque"
+          label={t('Ajouter une banque')}
         />
       </AddAccountLink>
     </div>
   )
 }
 
-export default memo(NoAccount)
+export default compose(
+  translate(),
+  withBreakpoints(),
+  memo
+)(NoAccount)
