e633845f74a8644a17acefb1087f0bcc460bed78
Simon Constans <simonc@linux.com>
2019-03-13 19:19:58 +0100

    feat(AccountRowLoading): Add component for row loading

--- src/ducks/balance/components/AccountsList.jsx
+++ src/ducks/balance/components/AccountsList.jsx
@@ -6,6 +6,7 @@ import withFilteringDoc from 'components/withFilteringDoc'
 import AccountRow from './AccountRow'
 import styles from './AccountsList.styl'
 import { getAccountBalance, getAccountType } from 'ducks/account/helpers'
+import AccountRowLoading from 'ducks/balance/components/AccountRowLoading'
 
 class AccountsList extends React.PureComponent {
   static propTypes = {
@@ -34,19 +35,23 @@ class AccountsList extends React.PureComponent {
 
     return (
       <ol className={styles.AccountsList}>
-        {sortBy(accounts.filter(Boolean), getAccountBalance).map(a => (
-          <AccountRow
-            key={a._id}
-            account={a}
-            group={group}
-            onClick={this.goToTransactionsFilteredByDoc(a)}
-            warningLimit={warningLimit}
-            checked={switches[a._id].checked}
-            disabled={switches[a._id].disabled}
-            id={`${group._id}.accounts.${a._id}`}
-            onSwitchChange={onSwitchChange}
-          />
-        ))}
+        {sortBy(accounts.filter(Boolean), getAccountBalance).map(a =>
+          a.loading ? (
+            <AccountRowLoading key={a._id} institutionSlug={a._id} />
+          ) : (
+            <AccountRow
+              key={a._id}
+              account={a}
+              group={group}
+              onClick={this.goToTransactionsFilteredByDoc(a)}
+              warningLimit={warningLimit}
+              checked={switches[a._id].checked}
+              disabled={switches[a._id].disabled}
+              id={`${group._id}.accounts.${a._id}`}
+              onSwitchChange={onSwitchChange}
+            />
+          )
+        )}
       </ol>
     )
   }
