2e3ed9a2bb14a49545bfb206bba86c91fc4e89c9
Patrick Browne <pt.browne@gmail.com>
2019-03-18 14:28:39 +0100

    chore: Remove unneeded view

--- src/components/AppRoute.jsx
+++ src/components/AppRoute.jsx
@@ -17,8 +17,6 @@ import {
 import { Balance } from 'ducks/balance'
 import EnsureIsFirstSynced from 'components/EnsureIsFirstSynced'
 
-export const ComingSoon = () => <p style="margin-left: 2em">Coming soon!</p>
-
 const AppRoute = (
   <Route component={EnsureIsFirstSynced}>
     <Route component={App}>
@@ -39,8 +37,6 @@ const AppRoute = (
         />
         <Route path=":categoryName" component={CategoriesPage} />
       </Route>
-      <Route path="projections" component={ComingSoon} />
-      <Route path="savings" component={ComingSoon} />
       <Route path="settings">
         <Route path="groups/new" component={NewGroupSettings} />
         <Route path="groups/:groupId" component={GroupSettings} />
