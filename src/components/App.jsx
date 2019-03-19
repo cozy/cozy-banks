4103c4eabac86f4af93df1738464cf5096064e51
Patrick Browne <pt.browne@gmail.com>
2019-03-19 18:18:03 +0100

    refactor: Extract pinGuarded to ducks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ import ErrorBoundary, { Error } from 'components/ErrorBoundary'
 import { withRouter } from 'react-router'
 import { flowRight as compose } from 'lodash'
 import { hasParameter } from 'utils/qs'
-import { PinGuard } from 'ducks/Pin'
+import { pinGuarded } from 'ducks/Pin'
 
 const ReactHint = ReactHintFactory(React)
 
@@ -51,16 +51,8 @@ const App = props => {
   )
 }
 
-const pinGuarded = Component => props => (
-  <PinGuard timeout={30 * 1000}>
-    <Component {...props} />
-  </PinGuard>
-)
-
-const noop = x => x
-
 export default compose(
-  __TARGET__ === 'mobile' ? pinGuarded : noop,
+  pinGuarded,
   queryConnect({ settingsCollection: settingsConn }),
   withRouter
 )(App)
--- src/ducks/pin/index.jsx
+++ src/ducks/pin/index.jsx
@@ -1,1 +1,2 @@
 export { default as PinGuard } from './PinGuard'
+export { default as pinGuarded } from './hoc'
