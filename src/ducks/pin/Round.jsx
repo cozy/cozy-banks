9d7d71c2d7a20ee0d0d06fded10b8548cd8ec252
Patrick Browne <pt.browne@gmail.com>
2019-03-20 10:08:24 +0100

    refactor: Destructure className

--- src/ducks/pin/Round.jsx
+++ src/ducks/pin/Round.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from 'ducks/pin/styles.styl'
 import cx from 'classnames'
 
-const Round = props => {
-  return <button {...props} className={cx(styles.Round, props.className)} />
+const Round = ({ className, ...props }) => {
+  return <button {...props} className={cx(styles.Round, className)} />
 }
 
 export default Round
