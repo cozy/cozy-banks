0f6fa36fd01b612c6b40d35d35fbc4cc91054676
Patrick Browne <pt.browne@gmail.com>
2019-03-21 18:32:00 +0100

    feat: PinKeyboard can act as controlled/uncontrolled component

--- src/ducks/pin/PinKeyboard.jsx
+++ src/ducks/pin/PinKeyboard.jsx
@@ -5,27 +5,76 @@ import range from 'lodash/range'
 import styles from 'ducks/pin/styles'
 import Round from 'ducks/pin/Round'
 
+
+/**
+ * Allows to type a value with an onScreen keyboard.
+ * If `onChange` is defined, it acts as a controlled component,
+ * otherwise, it keeps an internal state.
+ */
 class PinKeyboard extends React.PureComponent {
   constructor(props) {
     super(props)
+    this.handleConfirm = this.handleConfirm.bind(this)
+    this.handleClickNumber = this.handleClickNumber.bind(this)
+    this.handleRemoveCharacter = this.handleRemoveCharacter.bind(this)
   }
 
+  state = {
+    value: ''
+  }
 
+  handleConfirm() {
+    this.props.onConfirm(this.getValue())
+  }
 
+  getValue() {
+    return this.props.value || this.state.value
+  }
+
+  handleClickNumber(n) {
+    const value = this.getValue()
+    const newVal = (value + n).substr(0, MAX_LENGTH)
+    this.handleNewValue(newVal)
+  }
+
+  handleRemoveCharacter() {
+    const newVal = this.state.value.substr(0, this.state.value.length - 1)
+    this.handleNewValue(newVal)
+  }
+
+  handleNewValue(newVal) {
+    if (this.props.onChange) {
+      this.props.onChange(newVal)
+    } else {
+      this.setState({ value: newVal })
+    }
   }
 
   render() {
+    const value = this.getValue()
     return (
-      <div className={styles.PinKeyboard}>
-        {range(1, 10).map(n => (
-          <Round key={n}>{n}</Round>
-        ))}
-        <Round>0</Round>
-        <Round className="u-hide" />
+      <div>
+        <div className={styles.PinKeyboard}>
+          {range(1, 10).map(n => (
+            <Round
+              onClick={this.handleClickNumber.bind(null, n.toString())}
+              key={n}
+            >
+              {n}
+            </Round>
+          ))}
+          <Round onClick={this.handleRemoveCharacter}>R</Round>
+          <Round>0</Round>
+          <Round onClick={this.handleConfirm}>OK</Round>
+        </div>
       </div>
     )
   }
+}
 
+PinKeyboard.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func
 }
 
 export default PinKeyboard
