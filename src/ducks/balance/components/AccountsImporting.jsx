76a63a89bf23d751c047e8434102bb61d83c3899
Cyrille Perois <cperois@outlook.com>
2019-03-21 18:19:10 +0100

    feat(AccountsImporting): Set bar theme to primary

--- src/ducks/balance/components/AccountsImporting.jsx
+++ src/ducks/balance/components/AccountsImporting.jsx
@@ -10,6 +10,7 @@ import Header from 'components/Header'
 import { Padded } from 'components/Spacing'
 import VerticalBox from 'components/VerticalBox'
 import BalancePanels from 'ducks/balance/BalancePanels'
+import BarTheme from 'ducks/mobile/BarTheme'
 
 import headerTitleStyles from 'ducks/balance/components/HeaderTitle.styl'
 import styles from 'ducks/balance/components/AccountsImporting.styl'
@@ -64,6 +65,7 @@ const AccountsImporting = ({ t, classes, konnectorSlugs }) => {
 
   return (
     <Fragment>
+      <BarTheme theme="primary" />
       <Header className={styles.content} color="primary">
         <VerticalBox center className={styles.header}>
           <Padded>
