41fd7aa43904305f7e079cd76dfadd21cf74374a
Patrick Browne <pt.browne@gmail.com>
2019-03-22 10:49:00 +0100

    feat: Logout after too many attempts or logout btn

--- src/ducks/pin/PinAuth.jsx
+++ src/ducks/pin/PinAuth.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import compose from 'lodash/flowRight'
 
+import { connect } from 'react-redux'
 import { queryConnect } from 'cozy-client'
 import Alerter from 'cozy-ui/react/Alerter'
 import { translate } from 'cozy-ui/react/I18n'
@@ -11,6 +12,7 @@ import FingerprintButton from 'ducks/pin/FingerprintButton'
 import { pinSetting } from 'ducks/pin/queries'
 import PinButton from 'ducks/pin/PinButton'
 import { PIN_MAX_LENGTH, MAX_ATTEMPT } from 'ducks/pin/constants'
+import { onLogout } from 'ducks/mobile/utils'
 
 const AttemptCount_ = ({ t, current, max }) => {
   return <div>{t('Pin.attempt-count', { current, max })}</div>
@@ -30,6 +32,7 @@ class PinAuth extends React.Component {
     this.handleFingerprintError = this.handleFingerprintError.bind(this)
     this.handleFingerprintCancel = this.handleFingerprintCancel.bind(this)
     this.handleEnteredPin = this.handleEnteredPin.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
     this.state = {
       attempt: 0,
       pinValue: ''
@@ -49,8 +52,17 @@ class PinAuth extends React.Component {
     // this.props.onCancel()
   }
 
+  handleLogout() {
+    this.logout()
+  }
+
   onMaxAttempt() {
-    console.log('LOGOUT')
+    this.logout()
+  }
+
+  logout() {
+    const { client, store } = this.context
+    onLogout(store, client)
   }
 
   handleEnteredPin(pinValue) {
@@ -80,6 +92,7 @@ class PinAuth extends React.Component {
   }
 
   render() {
+    const { t } = this.props
     const { attempt, pinValue } = this.state
     return (
       <div>
@@ -89,7 +102,13 @@ class PinAuth extends React.Component {
           onError={this.handleFingerprintError}
           onCancel={this.handleFingerprintCancel}
         />
-        <PinKeyboard value={pinValue} onChange={this.handleEnteredPin} />
+        <PinKeyboard
+          leftButton={
+            <PinButton onClick={this.handleLogout}>{t('Pin.logout')}</PinButton>
+          }
+          value={pinValue}
+          onChange={this.handleEnteredPin}
+        />
       </div>
     )
   }
@@ -102,6 +121,7 @@ class PinAuth extends React.Component {
 }
 
 export default compose(
+  connect(),
   translate(),
   queryConnect({
     pinSetting
