3c8ace0229b844e4b3334b26b7150fd5660ee69b
Cyrille Perois <cperois@outlook.com>
2019-03-25 17:35:56 +0100

    chore: Add possibility to transform config.xml before build

---
+++ src/targets/mobile/scripts/beforePrepare.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs-extra')
+const libxslt = require('libxslt')
+
+function handleError(error) {
+  console.log('Error in beforePrepare hook:')
+  console.error(error)
+  throw error
+}
+module.exports = function (context) {
+  if (!process.env.MOBILE_CONFIG_TRANSFORM_FILE) {
+    console.log('No transformation file provided, skipping transformation step')
+    return
+  }
+
+  const transformFilePath = path.resolve(
+    process.cwd(),
+    process.env.MOBILE_CONFIG_TRANSFORM_FILE
+  )
+
+  let transformFile
+
+  try {
+    transformFile = fs.readFileSync(transformFilePath, 'utf8')
+  } catch (err) {
+    handleError(err)
+  }
+
+  const configPath = path.resolve(__dirname, '../config.xml')
+  const config = fs.readFileSync(configPath, 'utf8')
+
+  libxslt.parse(transformFile, (err, stylesheet) => {
+    if (err) {
+      handleError(err)
+    }
+
+    stylesheet.apply(config, (err, output) => {
+      if (err) {
+        handleError(err)
+      }
+
+      fs.writeFileSync(configPath, output)
+      console.log(`config.xml successfully transformed using ${transformFilePath}`)
+    })
+  })
+}
