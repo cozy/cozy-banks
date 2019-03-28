15966e22f61b6c755d619186b1aa5790a9c0501f
Patrick Browne <pt.browne@gmail.com>
2019-03-28 16:10:46 +0100

    feat: Do not lock app after 1m inactivity

--- src/ducks/pin/PinGuard.jsx
+++ src/ducks/pin/PinGuard.jsx
@@ -15,37 +15,23 @@ class PinGuard extends React.Component {
     this.state = { last: Date.now() }
     this.handleInteraction = this.handleInteraction.bind(this)
     this.handlePinSuccess = this.handlePinSuccess.bind(this)
-    this.handleResume = this.handleResume.bind(this)
-    this.handlePause = this.handlePause.bind(this)
   }
 
   componentDidMount() {
     document.addEventListener('touchstart', this.handleInteraction)
     document.addEventListener('click', this.handleInteraction)
-    document.addEventListener('resume', this.handleResume)
-    document.addEventListener('pause', this.handlePause)
     this.handleInteraction()
   }
 
   componentWillUnmount() {
     document.removeEventListener('touchstart', this.handleInteraction)
     document.removeEventListener('click', this.handleInteraction)
-    document.removeEventListener('resume', this.handleResume)
-    document.removeEventListener('pause', this.handlePause)
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.pinSetting.data !== prevProps.pinSetting.data) {
       this.resetTimeout()
     }
-  }
-
-  handlePause() {
-    this.setState({ showPin: true })
-  }
-
-  handleResume() {
-    this.setState({ showPin: true })
   }
 
   handleInteraction() {
