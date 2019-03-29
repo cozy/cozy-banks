842bd0ba044523d88d039c5628951e5c05ab8b66
Cyrille Perois <cperois@outlook.com>
2019-03-29 12:33:14 +0100

    refactor(withFilters): Remove useless intermediary function

--- src/components/withFilters.jsx
+++ src/components/withFilters.jsx
@@ -1,20 +1,18 @@
 import { connect } from 'react-redux'
 import { getFilteringDoc, filterByDoc, addFilterByPeriod } from 'ducks/filters'
 
-function withFilters(Wrapped) {
-  const mapStateToProps = state => ({
-    filteringDoc: getFilteringDoc(state)
-  })
+const mapStateToProps = state => ({
+  filteringDoc: getFilteringDoc(state)
+})
 
-  const mapDispatchToProps = dispatch => ({
-    filterByDoc: doc => dispatch(filterByDoc(doc)),
-    addFilterByPeriod: period => dispatch(addFilterByPeriod(period))
-  })
+const mapDispatchToProps = dispatch => ({
+  filterByDoc: doc => dispatch(filterByDoc(doc)),
+  addFilterByPeriod: period => dispatch(addFilterByPeriod(period))
+})
 
-  return connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Wrapped)
-}
+const withFilters = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)
 
 export default withFilters
