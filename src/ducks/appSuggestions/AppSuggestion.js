a9a482291509e9f26dd628e34469fbcdac5e8689
Patrick Browne <pt.browne@gmail.com>
2019-04-05 10:50:45 +0200

    refactor: Use helper to push in array at path

--- src/ducks/appSuggestions/AppSuggestion.js
+++ src/ducks/appSuggestions/AppSuggestion.js
@@ -1,6 +1,19 @@
 import { Document } from 'cozy-doctypes'
 import { head } from 'lodash'
 import { TRANSACTION_DOCTYPE } from 'doctypes'
+import get from 'lodash/get'
+import setWith from 'lodash/setWith'
+import clone from 'lodash/clone'
+
+const setIn = (obj, path, value) => {
+  return setWith(clone(obj), path, value, clone)
+}
+
+const pushAtPath = (obj, path, value) => {
+  const arr = get(obj, path, [])
+  arr.push(value)
+  return setIn(obj, path, arr)
+}
 
 class AppSuggestion extends Document {
   static fetchBySlug(slug) {
@@ -8,6 +21,8 @@ class AppSuggestion extends Document {
   }
 
   static linkTransaction(suggestion, transaction) {
+    const transactionRelationship = { _id: transaction._id, _type: TRANSACTION_DOCTYPE }
+    return pushAtPath(suggestion, 'relationships.transactions.data', transactionRelationship)
   }
 
   static init(slug, reasonCode) {
