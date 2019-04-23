35098c4cffc68b4acffe717fd0a408a4a0d79838
Cyrille Perois <cperois@outlook.com>
2019-04-23 10:46:10 +0200

    refactor(TransactionActionsContext): Pass props to value
    All props except children are passed as context value. This make this
    component more generic, so we don't have to override it in
    customizations

--- src/ducks/transactions/TransactionActionsContext.jsx
+++ src/ducks/transactions/TransactionActionsContext.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { flowRight as compose } from 'lodash'
+import { flowRight as compose, omit } from 'lodash'
 import withAppsUrls from 'ducks/apps/withAppsUrls'
 import withBrands from 'ducks/brandDictionary/withBrands'
 
@@ -7,9 +7,7 @@ export const TransactionActionsContext = React.createContext()
 
 class DumbTransactionActionsProvider extends React.Component {
   render() {
-    const { brands, urls } = this.props
-
-    const value = { brands, urls }
+    const value = omit(this.props, 'children')
 
     return (
       <TransactionActionsContext.Provider value={value}>
