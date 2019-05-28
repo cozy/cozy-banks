ab447e77658359f60d698379eecd7ece226b6d0c
Cyrille Perois <cperois@outlook.com>
2019-05-28 17:41:52 +0200

    feat(AppLinkAction): Use Chip if reimbursement-tag flag is enabled

--- src/ducks/transactions/actions/AppLinkAction.jsx
+++ src/ducks/transactions/actions/AppLinkAction.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { capitalize, findKey, omit } from 'lodash'
 import { translate } from 'cozy-ui/react'
 import ButtonAction from 'cozy-ui/react/ButtonAction'
+import Chip from 'cozy-ui/react/Chip'
+import Icon from 'cozy-ui/react/Icon'
+import flag from 'cozy-flags'
 import icon from 'assets/icons/actions/icon-link-out.svg'
 import styles from 'ducks/transactions/TransactionActions.styl'
 import { TransactionModalRow } from 'ducks/transactions/TransactionModal'
@@ -49,7 +52,13 @@ const Component = ({
     )
   }
 
-  return (
+  return flag('reimbursement-tag') ? (
+    <Chip size="small" variant="outlined" onClick={() => open(url)}>
+      {label}
+      <Chip.Separator />
+      <Icon icon="openwith" />
+    </Chip>
+  ) : (
     <ButtonAction
       onClick={() => open(url)}
       label={label}
