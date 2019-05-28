f3a83632e8a9f8238b9a6608113e968f12c5176c
Cyrille Perois <cperois@outlook.com>
2019-05-28 11:13:59 +0200

    feat(AttachedDocsAction): Handle render in transaction row

--- src/ducks/transactions/actions/AttachedDocsAction/index.js
+++ src/ducks/transactions/actions/AttachedDocsAction/index.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import flag from 'cozy-flags'
 import { hasBills, hasReimbursements } from 'ducks/transactions/helpers'
+import Bill from 'ducks/transactions/actions/AttachedDocsAction/Bill'
 
 class AttachedDocsAction extends React.PureComponent {
+  renderTransactionRow() {
+    const { transaction } = this.props
+
+    if (hasBills(transaction)) {
+      return this.renderTransactionRowBills()
+    } else if (hasReimbursements(transaction)) {
+      return this.renderTransactionRowReimbursements()
+    } else {
+      return null
+    }
+  }
+
+  renderTransactionRowBills() {
+    const { transaction } = this.props
+    const bills = transaction.bills.data.filter(Boolean)
+
+    return bills.map(bill => <Bill bill={bill} key={bill._id} />)
+  }
+
+  renderTransactionRowReimbursements() {
+    const { transaction } = this.props
+
+    const reimbursements = transaction.reimbursements.data.filter(
+      reimbursement => reimbursement && reimbursement.bill
+    )
+
+    return reimbursements.map(reimbursement => (
+      <Bill bill={reimbursement.bill} key={reimbursement.bill._id} />
+    ))
+  }
+
   render() {
-    return <div>AttachedDocsAction</div>
+    const { isModalItem } = this.props
+
+    if (isModalItem) {
+      return null
+    } else {
+      return this.renderTransactionRow()
+    }
   }
 }
 
