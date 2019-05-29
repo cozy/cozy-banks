d9fb1365213a412e17f07faf64f13f6177db483f
Cyrille Perois <cperois@outlook.com>
2019-05-29 11:15:41 +0200

    feat(FileIcon): Use file-outline icon

--- src/ducks/transactions/actions/AttachedDocsAction/FileIcon.jsx
+++ src/ducks/transactions/actions/AttachedDocsAction/FileIcon.jsx
@@ -5,7 +5,13 @@ import cx from 'classnames'
 const FileIcon = props => {
   const { className, ...rest } = props
 
-  return <Icon icon="file" className={cx('u-mr-half', className)} {...rest} />
+  return (
+    <Icon
+      icon="file-outline"
+      className={cx('u-mr-half', className)}
+      {...rest}
+    />
+  )
 }
 
 export default FileIcon
