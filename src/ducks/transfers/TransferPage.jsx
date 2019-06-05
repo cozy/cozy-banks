9694ac7eca2375b6e29fa066845ff28f2b54d45e
Patrick Browne <pt.browne@gmail.com>
2019-06-05 15:17:09 +0200

    feat: Add button to launch job

--- src/ducks/transfers/TransferPage.jsx
+++ src/ducks/transfers/TransferPage.jsx
@@ -4,8 +4,91 @@ import { translate, Text, Button } from 'cozy-ui/transpiled/react'
 import { withClient, queryConnect } from 'cozy-client'
 import compose from 'lodash/flowRight'
 import Loading from 'components/Loading'
+import Alerter from 'cozy-ui/react/Alerter'
 
+const transfers = {
+  /**
+   * Creates a job to transfer money
+   *
+   * @param  {CozyClient} client            - CozyClient
+   * @param  {Integer} options.amount    - Amount to send
+   * @param  {String} options.recipientId - io.cozy.bank.recipients id
+   * @param  {String} options.fromAccountId - io.cozy.bank.accounts id
+   * @return {Promise}
+   */
+  createJob: (client, { amount, recipientId, fromAccount }) => {
+    const konnector = fromAccount.cozyMetadata.createdByApp
+    return client.stackClient.jobs.create('konnector', {
+      mode: 'transfer',
+      konnector,
+      recipientId,
+      amount,
+      fromAccountId: fromAccount._id
+    })
+  }
+}
 class DumbRecipient extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.handleClickTransfer = this.handleClickTransfer.bind(this)
+    this.state = {
+      sending: false
+    }
+    this.inputRef = React.createRef()
+  }
+
+  /**
+   * Manages UI during transfer, setting state.sending and showing alerts
+   */
+  async handleClickTransfer() {
+    try {
+      this.setState({ sending: true })
+      await this.transferMoney()
+      Alerter.success('Transfer successfully sent')
+      this.clearInput()
+    } catch (e) {
+      console.error(e) // eslint-disable-line no-console
+      Alerter.error('Could not create transfer, check console')
+    } finally {
+      this.setState({ sending: false })
+    }
+  }
+
+  clearInput() {
+    if (this.inputRef.current) {
+      this.inputRef.current.value = ''
+    }
+  }
+
+  /**
+   * Creates the job to transfer money to the recipient
+   */
+  async transferMoney() {
+    const { client, recipient } = this.props
+
+    // TODO find out why the request with a selector did not work
+    // Works as is but is not the most efficient
+    const resp = await client.query(client.all('io.cozy.bank.accounts'))
+    const data = resp.data
+    const account = data.find(x => x.vendorId == recipient.vendorAccountId)
+
+    return transfers.createJob(client, {
+      amount: this.inputRef.current.value,
+      recipientId: this.props.recipient._id,
+      fromAccount: account
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.checkToFocus(prevState)
+  }
+
+  checkToFocus(prevState) {
+    if (prevState.sending !== this.state.sending && this.inputRef.current) {
+      this.inputRef.current.focus()
+    }
+  }
+
   render() {
     const { recipient } = this.props
     const { sending } = this.state
@@ -16,6 +99,13 @@ class DumbRecipient extends React.Component {
         {recipient.iban}
         <br />
         {recipient.bankName}
+        {recipient.bankName && <br />}
+        How much ? <input type="text" name="amount" ref={this.inputRef} />
+        <Button
+          busy={sending}
+          onClick={this.handleClickTransfer}
+          label="Send money"
+        />
       </Text>
     )
   }
