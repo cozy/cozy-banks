84a07fc99fe84bd24023f372d974171d3971438c
Cyrille Perois <cperois@outlook.com>
2019-06-06 15:42:37 +0200

    feat(StoreLink): Get redirection URL as soon as possible

--- src/components/StoreLink/StoreLink.jsx
+++ src/components/StoreLink/StoreLink.jsx
@@ -9,12 +9,32 @@ class DumbStoreLink extends React.Component {
     category: PropTypes.string
   }
 
-  redirect = async () => {
-    const { client: cozyClient, type, category } = this.props
-    const redirectionURL = await cozyClient.intents.getRedirectionURL(
+  async componentDidMount() {
+    await this.updateRedirectionURL()
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (
+      this.props.type !== prevProps.type ||
+      this.props.category !== prevProps.category
+    ) {
+      await this.updateRedirectionURL()
+    }
+  }
+
+  async updateRedirectionURL() {
+    const { client, type, category } = this.props
+
+    this.redirectionURL = await client.intents.getRedirectionURL(
       'io.cozy.apps',
       pickBy({ type, category }, Boolean)
     )
+  }
+
+  redirect = async () => {
+    if (!this.redirectionURL) {
+      await this.updateRedirectionURL()
+    }
 
     // We use `window.location` because on desktop we want to stay in the same tab/window
     // and on mobile we want to open the user's browser instead of an inapp browser
@@ -23,7 +43,7 @@ class DumbStoreLink extends React.Component {
     // This means login cookies are stored in the external browser.
     // To prevent asking the user to login again, we have to use an external browser
     // instead of the in app browser.
-    window.location = redirectionURL
+    window.location = this.redirectionURL
   }
 
   render() {
