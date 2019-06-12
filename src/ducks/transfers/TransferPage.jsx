b09eea897deadf851d240bc3c134d7e7f9fc1768
Patrick Browne <pt.browne@gmail.com>
2019-06-12 13:44:55 +0200

    feat: Revamp TransferPage, use react-swipeable-views
    Do not look, at the diff, prefer to look directly as the resulting file
    as the changes are too massive

--- src/ducks/transfers/TransferPage.jsx
+++ src/ducks/transfers/TransferPage.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-import Padded from 'components/Spacing/Padded'
-import { translate, Text, Button } from 'cozy-ui/transpiled/react'
-import { withClient, queryConnect } from 'cozy-client'
 import compose from 'lodash/flowRight'
+import groupBy from 'lodash/groupBy'
+import { withRouter } from 'react-router'
+import Padded from 'components/Spacing/Padded'
+import {
+  Media,
+  Bd,
+  Img,
+  translate,
+  Text,
+  Caption,
+  Bold,
+  Title as UITitle,
+  Field
+} from 'cozy-ui/transpiled/react'
+import { withClient, queryConnect } from 'cozy-client'
+
 import Loading from 'components/Loading'
+import { List, Row, Radio } from 'components/List'
+import Stepper from 'components/Stepper'
+import PageTitle from 'components/Title/PageTitle'
+import BottomButton from 'components/BottomButton'
+
+const Title = ({ children }) => {
+  return <UITitle className="u-ta-center u-mb-1">{children}</UITitle>
+}
 
 const transfers = {
   /**
@@ -26,10 +47,41 @@ const transfers = {
     })
   }
 }
+
+class TransferPage extends React.Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
+      slide: 0,
     }
+    this.handleGoBack = this.handleGoBack.bind(this)
+    this.handleConfirm = this.handleConfirm.bind(this)
+  }
+
+  async transferMoney() {
+    const { client } = this.props
+    const account = this.state.account
+    return transfers.createJob(client, {
+      amount: this.inputRef.current.value,
+      recipientId: this.props.recipient._id,
+      fromAccount: account
+    })
+  }
+
+  handleGoBack() {
+    this.goToPrevious()
+  }
+
+  goToNext() {
+    this.setState({ slide: this.state.slide + 1 })
+  }
+
+  goToPrevious() {
+    this.setState({ slide: Math.max(this.state.slide - 1, 0) })
+  }
+
+  handleConfirm() {
+    this.transferMoney()
   }
 
   render() {
@@ -41,11 +93,17 @@ const transfers = {
       )
     }
     return (
+      <>
+        <PageTitle>{t('Transfer.page-title')}</PageTitle>
+        <Stepper current={this.state.slide} onBack={this.handleGoBack} />
+      </>
     )
   }
 }
 
 const enhance = compose(
+  withClient,
+  withRouter,
   queryConnect({
     recipients: {
       query: client => client.all('io.cozy.bank.recipients'),
