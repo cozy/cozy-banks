d8c67cf3b85c2c8d407a953542db0e3ebcde0d17
Patrick Browne <pt.browne@gmail.com>
2019-06-13 09:22:23 +0200

    refactor(transfers): Extract styles

--- src/components/BottomButton.jsx
+++ src/components/BottomButton.jsx
@@ -4,22 +4,34 @@ import { Button } from 'cozy-ui/react'
 import Bottom from 'components/Bottom'
 import Padded from 'components/Spacing/Padded'
 
+const buttonStyle = {
+  transition: 'transform 0.5s ease'
+}
+
+const styles = {
+  visibleWrapper: {
+    pointerEvents: 'none'
+  },
+  visibleButton: {
+    ...buttonStyle,
+    opacity: 1,
+    transform: `translateY(0px)`
+  },
+  invisibleButton: {
+    ...buttonStyle,
+    opacity: 0,
+    transform: `translateY(100px)`
+  }
+}
+
 /** Button displayed at the bottom of the view on mobile, appears from the bottom */
 const BottomButton = ({ visible, ...buttonProps }) => (
-  <Bottom
-    style={{
-      pointerEvents: visible ? '' : 'none'
-    }}
-  >
+  <Bottom style={visible ? styles.visibleWrapper : null}>
     <Padded>
       <Button
         extension="full"
         theme="primary"
-        style={{
-          transition: 'transform 0.5s ease',
-          opacity: visible ? 1 : 0,
-          transform: `translateY(${visible ? 0 : 100}px)`
-        }}
+        style={visible ? styles.visibleButton : styles.invisibleButton}
         {...buttonProps}
       />
     </Padded>
