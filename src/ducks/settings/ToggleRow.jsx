715df957649a3bce530d0652bb26fccd328a9b0f
Cyrille Perois <cperois@outlook.com>
2019-06-14 11:24:05 +0200

    refactor(ToggleRow): Extract intermediary components

--- src/ducks/settings/ToggleRow.jsx
+++ src/ducks/settings/ToggleRow.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Toggle from 'cozy-ui/react/Toggle'
+import cx from 'classnames'
 import styles from 'ducks/settings/ToggleRow.styl'
 
 const parseNumber = val => {
   val = val.replace(/\D/gi, '') || 0
   return parseInt(val, 10)
+}
+
+export const ToggleRowWrapper = props => {
+  const { className, ...rest } = props
+
+  return <div className={cx(styles.ToggleRow__wrapper, className)} {...rest} />
+}
+
+export const ToggleRowTitle = props => {
+  return <h5 {...props} />
+}
+
+export const ToggleRowContent = props => {
+  const { className, ...rest } = props
+
+  return <div className={cx(styles.ToggleRow__body, className)} {...rest} />
+}
+
+export const ToggleRowDescription = props => {
+  const { className, ...rest } = props
+
+  return (
+    <div className={cx(styles.ToggleRow__description, className)} {...rest} />
+  )
+}
+
+export const ToggleRowInput = props => {
+  const { onChange, value, unit, className, ...rest } = props
+
+  return (
+    <span className={cx(styles.ToggleRow__input, className)} {...rest}>
+      <span className={styles.ToggleRow__inputContainer}>
+        <input type="text" onChange={onChange} value={value} />
+      </span>
+      {unit && <span>{unit}</span>}
+    </span>
+  )
+}
+
+export const ToggleRowToggle = props => {
+  const { id, checked, onToggle, className, ...rest } = props
+
+  return (
+    <div className={cx(styles.ToggleRow__toggle, className)} {...rest}>
+      <Toggle id={id} checked={checked} onToggle={onToggle} />
+    </div>
+  )
 }
 
 class ToggleRow extends Component {
@@ -24,34 +72,27 @@ class ToggleRow extends Component {
     const hasValue = value !== undefined
 
     return (
-      <div>
-        {title && <h5>{title}</h5>}
-        <div className={styles.ToggleRow__body}>
-          <p className={styles.ToggleRow__description}>
+      <ToggleRowWrapper>
+        {title && <ToggleRowTitle>{title}</ToggleRowTitle>}
+        <ToggleRowContent>
+          <ToggleRowDescription>
             <span dangerouslySetInnerHTML={{ __html: description }} />
             {hasValue && (
-              <span className={styles.ToggleRow__input}>
-                <span className={styles.ToggleRow__inputContainer}>
-                  <input
-                    type="text"
-                    onChange={e => onChangeValue(parseNumber(e.target.value))}
-                    value={value}
-                  />
-                </span>
-                {unit && <span>{unit}</span>}
-              </span>
+              <ToggleRowInput
+                value={value}
+                onChange={e => onChangeValue(parseNumber(e.target.value))}
+                unit={unit}
+              />
             )}
-          </p>
+          </ToggleRowDescription>
 
-          <div className={styles.ToggleRow__toggle}>
-            <Toggle
-              id={name}
-              checked={enabled}
-              onToggle={checked => onToggle(checked)}
-            />
-          </div>
-        </div>
-      </div>
+          <ToggleRowToggle
+            id={name}
+            checked={enabled}
+            onToggle={checked => onToggle(checked)}
+          />
+        </ToggleRowContent>
+      </ToggleRowWrapper>
     )
   }
 }
