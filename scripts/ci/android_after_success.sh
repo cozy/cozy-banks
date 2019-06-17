f51a58a4ea59f0bc8fddd007a4823005eb08846a
Simon Constans <simonc@linux.com>
2019-06-17 16:51:13 +0200

    feat(Travis): Autosign apk [apk]

--- scripts/ci/android_after_success.sh
+++ scripts/ci/android_after_success.sh
@@ -4,6 +4,6 @@
 set -e
 
 ./scripts/ci/add_ssh_key.sh
-export APK_URL=$(node scripts/ci/push_artifact.js src/targets/mobile/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk)
+export APK_URL=$(node scripts/ci/push_artifact.js src/targets/mobile/build/android/cozy-banks.apk)
 yarn run cozy-ci-github "🎁 [Click here]($APK_URL) to download the latest Android APK"
 ./scripts/ci/mattermost_comment.sh
