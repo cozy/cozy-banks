a2c37fa8acc0ad3223944764c1bfe2a338dabea7
Simon Constans <simonc@linux.com>
2019-06-19 11:55:14 +0200

    refactor(AccountsLoading): Use return on AccountRowLoading

--- src/ducks/balance/components/AccountRowLoading.jsx
+++ src/ducks/balance/components/AccountRowLoading.jsx
@@ -7,42 +7,51 @@ import KonnectorIcon from 'ducks/balance/components/KonnectorIcon'
 import styles from 'ducks/balance/components/AccountRow.styl'
 import stylesLoading from 'ducks/balance/components/AccountRowLoading.styl'
 
-const AccountRowLoading = ({ t, institutionSlug, status }) => (
-  <li className={styles.AccountRow}>
-    <div className={styles.AccountRow__column}>
-      <div className={styles.AccountRow__logo}>
-        {institutionSlug && (
-          <KonnectorIcon
-            slug={institutionSlug}
-            className={styles.KonnectorIcon}
-          />
-        )}
-      </div>
-      <div className={styles.AccountRow__labelUpdatedAtWrapper}>
-        <div className={styles.AccountRow__label}>
-          {status === 'errored'
-            ? t('Balance.importing_accounts_error')
-            : t('Balance.importing_accounts')}
-        </div>
-        <div className={styles.AccountRow__updatedAt}>
-          {status === 'errored' ? (
-            <>
-              <Icon size="12" icon="warning" />
-              <span className={stylesLoading.error}>{t('Balance.error')}</span>
-            </>
-          ) : (
-            <>
-              <Icon size="12" icon="spinner" color="var(--primaryColor)" spin />
-              <span className={stylesLoading.InProgress}>
-                {t('Balance.in_progress')}
-              </span>
-            </>
+const AccountRowLoading = ({ t, institutionSlug, status }) => {
+  return (
+    <li className={styles.AccountRow}>
+      <div className={styles.AccountRow__column}>
+        <div className={styles.AccountRow__logo}>
+          {institutionSlug && (
+            <KonnectorIcon
+              slug={institutionSlug}
+              className={styles.KonnectorIcon}
+            />
           )}
         </div>
+        <div className={styles.AccountRow__labelUpdatedAtWrapper}>
+          <div className={styles.AccountRow__label}>
+            {status === 'errored'
+              ? t('Balance.importing_accounts_error')
+              : t('Balance.importing_accounts')}
+          </div>
+          <div className={styles.AccountRow__updatedAt}>
+            {status === 'errored' ? (
+              <>
+                <Icon size="12" icon="warning" />
+                <span className={stylesLoading.error}>
+                  {t('Balance.error')}
+                </span>
+              </>
+            ) : (
+              <>
+                <Icon
+                  size="12"
+                  icon="spinner"
+                  color="var(--primaryColor)"
+                  spin
+                />
+                <span className={stylesLoading.InProgress}>
+                  {t('Balance.in_progress')}
+                </span>
+              </>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  </li>
-)
+    </li>
+  )
+}
 
 AccountRowLoading.propTypes = {
   t: PropTypes.func.isRequired,
