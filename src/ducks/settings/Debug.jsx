76677a4c427b7f73b8304f9f61707feb6b10268e
Cyrille Perois <cperois@outlook.com>
2019-06-19 16:06:53 +0200

    feat(settings): Add send notification button in debug panel

--- src/ducks/settings/Debug.jsx
+++ src/ducks/settings/Debug.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Checkbox from 'cozy-ui/react/Checkbox'
+import Button from 'cozy-ui/react/Button'
+import Alerter from 'cozy-ui/react/Alerter'
 import { Title, SubTitle } from 'cozy-ui/react/Text'
 import flag, { FlagSwitcher } from 'cozy-flags'
 import { withClient } from 'cozy-client'
@@ -21,6 +23,30 @@ class DumbDebugSettings extends React.PureComponent {
     flag('account-loading', accountLoadingValue)
     if (accountLoadingValue) {
       flag('no-account', false)
+    }
+  }
+
+  async sendNotification() {
+    const { client } = this.props
+
+    try {
+      await client.stackClient.fetchJSON('POST', '/notifications', {
+        data: {
+          type: 'io.cozy.notifications',
+          attributes: {
+            category: 'transaction-greater',
+            title: 'Test notification',
+            message: 'This is a test notification message',
+            preferred_channels: ['mail', 'mobile'],
+            content: 'This is a test notification text content',
+            content_html: 'This is a test notification HTML content'
+          }
+        }
+      })
+
+      Alerter.success('Notification sent')
+    } catch (err) {
+      Alerter.error('Failed to send notification: ' + err)
     }
   }
 
@@ -50,6 +76,9 @@ class DumbDebugSettings extends React.PureComponent {
             <p>{client.stackClient.oauthOptions.notification_device_token}</p>
           </>
         ) : null}
+        <Button onClick={() => this.sendNotification()}>
+          Send notification
+        </Button>
         <Title>Flags</Title>
         <FlagSwitcher.List />
       </div>
