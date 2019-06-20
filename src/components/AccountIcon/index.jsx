ab542c72c168fe01528dd1b299257c6a9a50cc3a
Patrick Browne <pt.browne@gmail.com>
2019-06-20 13:43:00 +0200

    chore: Lint

--- src/components/AccountIcon/index.jsx
+++ src/components/AccountIcon/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import KonnectorIcon from 'ducks/balance/components/KonnectorIcon'
 import { getAccountInstitutionSlug } from 'ducks/account/helpers'
 import styles from './styles.styl'
-import cx from 'classnames'
 
 /** Displays a konnector icon for an io.cozy.bank.accounts */
 const _AccountIcon = ({ account, className }) => {
