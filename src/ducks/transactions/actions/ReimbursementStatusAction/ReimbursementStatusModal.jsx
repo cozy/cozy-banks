1e99e7732754a06592e97d62b0f7ade8dd9a9007
Cyrille Perois <cperois@outlook.com>
2019-06-28 11:24:07 +0200

    fix(ReimbursementStatusAction): Handle overflowing content

--- src/ducks/transactions/actions/ReimbursementStatusAction/ReimbursementStatusModal.jsx
+++ src/ducks/transactions/actions/ReimbursementStatusAction/ReimbursementStatusModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { flowRight as compose } from 'lodash'
 import { translate, withBreakpoints } from 'cozy-ui/react'
-import Modal from 'cozy-ui/react/Modal'
+import Modal, { ModalContent } from 'cozy-ui/react/Modal'
 import Icon from 'cozy-ui/react/Icon'
 import { Title, Text } from 'cozy-ui/react/Text'
 import cx from 'classnames'
@@ -33,57 +33,63 @@ class _ReimbursementStatusModal extends React.PureComponent {
     return (
       <Modal
         mobileFullscreen
-        className={cx('u-flex', 'u-flex-column', 'u-pv-2', className)}
+        className={cx('u-flex', 'u-flex-column', className)}
         {...rest}
       >
-        <header className="u-ta-center">
-          <Icon icon={iconReimbursement} size={56} color="var(--slateGrey)" />
-          <Title className="u-mt-1">
-            {t('Transactions.actions.reimbursementStatus.modal.title')}
-          </Title>
-          <Text className={styles.ReimbursementStatusModal__transactionLabel}>
-            {getLabel(transaction)}
-          </Text>
-        </header>
-        <form className="u-mt-1">
-          <List border="vertical">
-            {choices.map(choice => (
-              <Row key={choice}>
-                <Radio
-                  key={choice}
-                  name="reimbursementStatus"
-                  value={choice}
-                  label={t(`Transactions.reimbursementStatus.${choice}`)}
-                  checked={status === choice}
-                  onChange={onChange}
-                  className={cx('u-mb-0', styles.Radio)}
-                />
-              </Row>
-            ))}
-          </List>
-        </form>
-        {showContacts ? (
-          <div
-            className={cx(styles.ReimbursementStatusModal__contact, 'u-pt-2', {
-              'u-mt-auto': isMobile
-            })}
-          >
-            {brands
-              .filter(
-                brand => brand.health && brand.hasTrigger && brand.contact
-              )
-              .map((brand, index) => (
-                <ContactItem
-                  brand={brand}
-                  key={brand.name}
-                  // TODO use stack layout when https://github.com/cozy/cozy-banks/pull/1312 has been merged (see https://github.com/cozy/cozy-banks/pull/1312/commits/2bc1d75a25fe2c61f219579ac56407e356997105 more particularly)
-                  className={cx({
-                    'u-mt-1-half': index !== 0
-                  })}
-                />
+        <ModalContent className="u-ph-0">
+          <header className="u-ta-center">
+            <Icon icon={iconReimbursement} size={56} color="var(--slateGrey)" />
+            <Title className="u-mt-1">
+              {t('Transactions.actions.reimbursementStatus.modal.title')}
+            </Title>
+            <Text className={styles.ReimbursementStatusModal__transactionLabel}>
+              {getLabel(transaction)}
+            </Text>
+          </header>
+          <form className="u-mt-1">
+            <List border="vertical">
+              {choices.map(choice => (
+                <Row key={choice}>
+                  <Radio
+                    key={choice}
+                    name="reimbursementStatus"
+                    value={choice}
+                    label={t(`Transactions.reimbursementStatus.${choice}`)}
+                    checked={status === choice}
+                    onChange={onChange}
+                    className={cx('u-mb-0', styles.Radio)}
+                  />
+                </Row>
               ))}
-          </div>
-        ) : null}
+            </List>
+          </form>
+          {showContacts ? (
+            <div
+              className={cx(
+                styles.ReimbursementStatusModal__contact,
+                'u-pt-2',
+                {
+                  'u-mt-auto': isMobile
+                }
+              )}
+            >
+              {brands
+                .filter(
+                  brand => brand.health && brand.hasTrigger && brand.contact
+                )
+                .map((brand, index) => (
+                  <ContactItem
+                    brand={brand}
+                    key={brand.name}
+                    // TODO use stack layout when https://github.com/cozy/cozy-banks/pull/1312 has been merged (see https://github.com/cozy/cozy-banks/pull/1312/commits/2bc1d75a25fe2c61f219579ac56407e356997105 more particularly)
+                    className={cx({
+                      'u-mt-1-half': index !== 0
+                    })}
+                  />
+                ))}
+            </div>
+          ) : null}
+        </ModalContent>
       </Modal>
     )
   }
