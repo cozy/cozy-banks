d97a0ffcacc6be7c363b915650a533ffaae0c5c6
Patrick Browne <pt.browne@gmail.com>
2019-07-04 11:48:23 +0200

    feat: Make Title responsive

--- src/ducks/transfers/steps/Title.jsx
+++ src/ducks/transfers/steps/Title.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Title as UITitle } from 'cozy-ui/transpiled/react'
+import { Title as UITitle, withBreakpoints } from 'cozy-ui/transpiled/react'
 
-const _Title = ({ children }) => {
-  return <UITitle className="u-ta-center u-mb-1">{children}</UITitle>
+const _Title = ({ children, breakpoints: { isMobile } }) => {
+  return (
+    <UITitle className={'u-mb-1 ' + (isMobile ? 'u-ta-center' : '')}>
+      {children}
+    </UITitle>
+  )
 }
 
-const Title = React.memo(_Title)
+const Title = React.memo(withBreakpoints()(_Title))
 
 export default Title
