c6deb48fa4589fa9f099db62b21b3f075a9340a8
Cyrille Perois <cperois@outlook.com>
2019-07-10 19:08:19 +0200

    refactor(stats): Add getTransactionsByCategoryName helper

--- src/targets/services/stats.js
+++ src/targets/services/stats.js
@@ -82,51 +82,44 @@ const main = async () => {
       const accountStats = statsByAccountId[accountId] || {}
       const transactionsByCategory = groupBy(transactions, getCategoryId)
 
+      const getTransactionsByCategoryName = categoryName => {
+        const categoryId = getCategoryIdFromName(categoryName)
+        return transactionsByCategory[categoryId] || []
+      }
+
       accountStats.periodStart = period.start
       accountStats.periodEnd = period.end
 
       accountStats.income = getMeanOnPeriod(
-        transactionsByCategory[getCategoryIdFromName('activityIncome')],
+        getTransactionsByCategoryName('activityIncome'),
         period
       )
 
       accountStats.additionalIncome = getMeanOnPeriod(
         [
-          ...(transactionsByCategory[
-            getCategoryIdFromName('replacementIncome')
-          ] || []),
-          ...(transactionsByCategory[getCategoryIdFromName('interests')] || []),
-          ...(transactionsByCategory[getCategoryIdFromName('dividends')] || []),
-          ...(transactionsByCategory[
-            getCategoryIdFromName('donationsReceived')
-          ] || []),
-          ...(transactionsByCategory[getCategoryIdFromName('allocations')] ||
-            []),
-          ...(transactionsByCategory[getCategoryIdFromName('rentalIncome')] ||
-            []),
-          ...(transactionsByCategory[
-            getCategoryIdFromName('additionalIncome')
-          ] || []),
-          ...(transactionsByCategory[getCategoryIdFromName('incomeCat')] || [])
+          ...getTransactionsByCategoryName('replacementIncome'),
+          ...getTransactionsByCategoryName('interests'),
+          ...getTransactionsByCategoryName('dividends'),
+          ...getTransactionsByCategoryName('donationsReceived'),
+          ...getTransactionsByCategoryName('allocations'),
+          ...getTransactionsByCategoryName('rentalIncome'),
+          ...getTransactionsByCategoryName('additionalIncome'),
+          ...getTransactionsByCategoryName('incomeCat')
         ],
         period
       )
 
       accountStats.mortgage = getMeanOnPeriod(
-        transactionsByCategory[getCategoryIdFromName('realEstateLoan')],
+        getTransactionsByCategoryName('realEstateLoan'),
         period
       )
 
       accountStats.loans = getMeanOnPeriod(
         [
-          ...(transactionsByCategory[getCategoryIdFromName('realEstateLoan')] ||
-            []),
-          ...(transactionsByCategory[getCategoryIdFromName('consumerLoan')] ||
-            []),
-          ...(transactionsByCategory[getCategoryIdFromName('studentLoan')] ||
-            []),
-          ...(transactionsByCategory[getCategoryIdFromName('vehiculeLoan')] ||
-            [])
+          ...getTransactionsByCategoryName('realEstateLoan'),
+          ...getTransactionsByCategoryName('consumerLoan'),
+          ...getTransactionsByCategoryName('studentLoan'),
+          ...getTransactionsByCategoryName('vehiculeLoan')
         ],
         period
       )
