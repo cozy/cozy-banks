ecf676f4f9b50983b6b76341c7c5c32b94a40046
Quentin Valmori <quentin.valmori@gmail.com>
2019-07-15 17:12:34 +0200

    fix: Finally unlink file

--- src/targets/mobile/scripts/customizeConfigXML.js
+++ src/targets/mobile/scripts/customizeConfigXML.js
@@ -25,47 +25,56 @@ const customizeConfigXML = function(context) {
 
   const configPath = path.resolve(__dirname, '../config.xml')
   const tmpPath = path.resolve(__dirname, '../tmp.txt')
-  fs.writeFileSync(tmpPath, '')
+  try {
+    fs.writeFileSync(tmpPath, '')
 
-  let [major, minor, patch, beta] = overrideConfig.fullVersion
-    .split('.')
-    .map(x => parseInt(x, 10))
-  const versionCode = overrideConfig.fullVersion
-    .split('.')
-    .slice(0, 3)
-    .join('.')
-  const androidVersionCode = (
-    major * 1000000 +
-    minor * 10000 +
-    patch * 100 +
-    beta
-  ).toString()
-  const transformedContent = xsltproc.transform(transformFilePath, configPath, {
-    output: tmpPath
-  })
-  transformedContent.stdout.on('data', function(data) {
-    console.log('xsltproc stdout: ' + data)
-  })
+    let [major, minor, patch, beta] = overrideConfig.fullVersion
+      .split('.')
+      .map(x => parseInt(x, 10))
+    const versionCode = overrideConfig.fullVersion
+      .split('.')
+      .slice(0, 3)
+      .join('.')
+    const androidVersionCode = (
+      major * 1000000 +
+      minor * 10000 +
+      patch * 100 +
+      beta
+    ).toString()
+    const transformedContent = xsltproc.transform(
+      transformFilePath,
+      configPath,
+      {
+        output: tmpPath
+      }
+    )
+    transformedContent.stdout.on('data', function(data) {
+      console.log('xsltproc stdout: ' + data)
+    })
 
-  transformedContent.stderr.on('data', function(data) {
-    console.log('xsltproc stderr: ' + data)
-  })
+    transformedContent.stderr.on('data', function(data) {
+      console.log('xsltproc stderr: ' + data)
+    })
 
-  transformedContent.on('exit', function(code) {
-    if (code === 0) {
-      const config = fs.readFileSync(tmpPath, 'utf8')
+    transformedContent.on('exit', function(code) {
+      if (code === 0) {
+        const config = fs.readFileSync(tmpPath, 'utf8')
 
-      let output = config
-        .toString()
-        .replace('$VERSION_CODE', versionCode)
-        .replace('$ANDROID_VERSION_CODE', androidVersionCode)
-        .replace('$IOS_VERSION_CODE', overrideConfig.fullVersion)
-        .replace('$USER_AGENT_VERSION', 'io.cozy.banks.mobile-' + versionCode)
+        let output = config
+          .toString()
+          .replace('$VERSION_CODE', versionCode)
+          .replace('$ANDROID_VERSION_CODE', androidVersionCode)
+          .replace('$IOS_VERSION_CODE', overrideConfig.fullVersion)
+          .replace('$USER_AGENT_VERSION', 'io.cozy.banks.mobile-' + versionCode)
 
-      fs.writeFileSync(configPath, output)
-    }
+        fs.writeFileSync(configPath, output)
+      }
+    })
+  } catch (error) {
+    console.log('error during the transform', error)
+  } finally {
     fs.unlinkSync(tmpPath)
-  })
+  }
 }
 
 module.exports = customizeConfigXML
