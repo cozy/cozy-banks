361e955a159de54c1cdcac5fcc8f2d62f32e559d
Patrick Browne <pt.browne@gmail.com>
2019-07-29 17:28:22 +0200

    refactor: Clearer function names

--- src/ducks/mobile/push.js
+++ src/ducks/mobile/push.js
@@ -5,14 +5,33 @@ import { fetchSettings, isNotificationEnabled } from 'ducks/settings/helpers'
 
 let push
 
-export const registerPushNotifications = async cozyClient => {
-  const clientInfos = cozyClient.stackClient.oauthOptions
+export const startPushNotificationsIfSettingEnabled = async cozyClient => {
   const settings = await fetchSettings(cozyClient)
-  return startPushNotifications(cozyClient, settings, clientInfos)
+  if (!isNotificationEnabled(settings)) {
+    return
+  }
+  return startPushNotifications(cozyClient)
 }
 
-export const startPushNotifications = (cozyClient, settings, clientInfos) => {
-  if (push || !isNotificationEnabled(settings)) {
+/**
+ * When we receive a notification while the app is in foreground, all on('notification')
+ * handlers are executed. But we don't want to redirect the user without his consent.
+ * So we redirect only when the user taps on the notification in the notification center.
+ * In this case, the app is always in background.
+ */
+const handleNotification = notification => {
+  if (
+    !notification.additionalData.foreground &&
+    notification.additionalData.route
+  ) {
+    hashHistory.push(notification.additionalData.route)
+  }
+}
+
+export const startPushNotifications = cozyClient => {
+  if (push) {
+    // eslint-disable-next-line no-console
+    console.warn('Push notifications already started')
     return
   }
 
@@ -20,21 +39,6 @@ export const startPushNotifications = (cozyClient, settings, clientInfos) => {
     // eslint-disable-next-line no-console
     console.warn('No window.PushNotification, did not start push notifications')
     return
-  }
-
-  /**
-   * When we receive a notification while the app is in foreground, all on('notification')
-   * handlers are executed. But we don't want to redirect the user without his consent.
-   * So we redirect only when the user taps on the notification in the notification center.
-   * In this case, the app is always in background.
-   */
-  const handleNotification = notification => {
-    if (
-      !notification.additionalData.foreground &&
-      notification.additionalData.route
-    ) {
-      hashHistory.push(notification.additionalData.route)
-    }
   }
 
   push = window.PushNotification.init({
@@ -53,6 +57,7 @@ export const startPushNotifications = (cozyClient, settings, clientInfos) => {
   // eslint-disable-next-line no-console
   push.on('error', err => console.log(err))
   push.on('registration', async ({ registrationId }) => {
+    const clientInfos = cozyClient.stackClient.oauthOptions
     cozyClient.stackClient.updateInformation({
       ...clientInfos,
       notificationDeviceToken: registrationId
@@ -105,7 +110,7 @@ export const stopPushNotifications = async () => {
  */
 export default client => {
   client.on('login', async () => {
-    await registerPushNotifications(client)
+    await startPushNotificationsIfSettingEnabled(client)
   })
 
   client.on('logout', async () => {
