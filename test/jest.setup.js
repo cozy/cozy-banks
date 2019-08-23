811229f69d2bb89e0d6a75cbb763d65d1dd6d735
Patrick Browne <pt.browne@gmail.com>
2019-08-23 10:45:20 +0200

    fix: Add whatwg-fetch in Jest

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -2,6 +2,9 @@ import { configure, mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import logger from 'cozy-logger'
 
+// Jest is running in node environment but some of our libs need fetch
+import 'whatwg-fetch'
+
 logger.setLevel('ERROR')
 
 configure({ adapter: new Adapter() })
@@ -13,7 +16,7 @@ global.cozy = {
   bar: {
     BarLeft: () => null,
     BarRight: () => null,
-    BarCenter:() => null,
+    BarCenter: () => null,
     setTheme: () => null
   }
 }
