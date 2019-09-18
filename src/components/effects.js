bc98c2be9599af4fe4d3dcf01ce9709ad44c1ac4
Patrick Browne <pt.browne@gmail.com>
2019-09-18 12:10:12 +0200

    refactor: Rename variable inside redirectionURL effect

--- src/components/effects.js
+++ src/components/effects.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react'
 import { Intents } from 'cozy-interapp'
 
 export const useRedirectionURL = (client, doctype, options) => {
-  const [fetched, setFetched] = useState(null)
+  const [redirectionURL, setRedirectionURL] = useState(null)
   useEffect(() => {
     const fetch = async () => {
       const intents = new Intents({ client: client })
-      const fetchedRes = await intents.getRedirectionURL(doctype, options)
-      setFetched(fetchedRes)
+      const redirectionURL = await intents.getRedirectionURL(doctype, options)
+      setRedirectionURL(redirectionURL)
     }
     fetch()
   }, [client.uri, doctype, options])
-  return fetched
+  return redirectionURL
 }
