e79848602d7faea9897136b5c8ff6e84137cba8c
Patrick Browne <pt.browne@gmail.com>
2019-09-23 10:13:01 +0200

    test: Add test for LateHealthReimbursement fetch data

--- src/ducks/notifications/LateHealthReimbursement.spec.js
+++ src/ducks/notifications/LateHealthReimbursement.spec.js
@@ -1,4 +1,11 @@
 import LateHealthReimbursement from './LateHealthReimbursement'
+import { Transaction, Bill, BankAccount } from 'src/models'
+import { Document } from 'cozy-doctypes'
+
+beforeEach(() => {
+  jest.spyOn(Document, 'queryAll').mockResolvedValue([])
+  jest.spyOn(Document, 'getAll').mockResolvedValue([])
+})
 
 describe('LateHealthReimbursement', () => {
   const setup = ({ lang }) => {
@@ -29,5 +36,44 @@ describe('LateHealthReimbursement', () => {
         notification.getPushContent({ transactions: twoTransactions })
       ).toMatchSnapshot()
     })
+  })
+
+  it('should fetch data', async () => {
+    jest.spyOn(Transaction, 'queryAll').mockResolvedValue([
+      {
+        amount: 20,
+        date: '2018-09-16T12:00',
+        label: '1',
+        account: 'accountId1'
+      },
+      {
+        amount: 10,
+        date: '2018-09-17T12:00',
+        label: '2',
+        account: 'accountId2'
+      },
+      {
+        amount: -5,
+        date: '2018-09-18T12:00',
+        label: '3',
+        manualCategoryId: '400610',
+        account: 'accountId3',
+        reimbursements: [{ billId: 'io.cozy.bills:billId12345' }]
+      }
+    ])
+    jest.spyOn(Document, 'getAll').mockImplementation(async function() {
+      if (this.doctype == 'io.cozy.bills') {
+        return [{ _id: 'billId12345' }]
+      } else if (this.doctype == 'io.cozy.bank.accounts') {
+        return [{ _id: 'accountId3' }]
+      }
+    })
+    const { notification } = setup({ lang: 'en' })
+
+    const res = await notification.fetchData()
+    expect(Bill.getAll).toHaveBeenCalledWith(['billId12345'])
+    expect(BankAccount.getAll).toHaveBeenCalledWith(['accountId3'])
+    expect(res.transactions).toHaveLength(1)
+    expect(res.accounts).toHaveLength(1)
   })
 })
