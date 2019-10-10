f6557ddc26b4f93ad46454cf93ed230cf5094219
Patrick Browne <pt.browne@gmail.com>
2019-10-10 10:47:08 +0200

    fix: Add cozy-ui stylesheet and icons

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,12 +5,14 @@ module.exports = {
   require: [
     ...glob.sync(path.resolve(__dirname, 'build/app.*.css')),
     path.resolve(__dirname, 'docs/styleguide/style.css'),
-    path.resolve(__dirname, 'docs/styleguide/setup.js')
+    path.resolve(__dirname, 'docs/styleguide/setup.js'),
+    'cozy-ui/transpiled/react/stylesheet.css'
   ],
   serverPort: 6061,
   styleguideDir: 'docs/build/styleguide',
   styleguideComponents: {
-    Wrapper: path.resolve(__dirname, 'docs/styleguide/Wrapper')
+    Wrapper: path.resolve(__dirname, 'docs/styleguide/Wrapper'),
+    SectionsRenderer: path.resolve('cozy-ui/docs/IconSpriteInjector.jsx')
   },
   sections: [
     {
