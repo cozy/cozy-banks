6851097f0dc75d1b3e771f5b740f5a8610d6d4ad
Cyrille Perois <cperois@outlook.com>
2019-11-06 16:27:00 +0100

    feat(LoanListPage): Correct alignment on desktop

--- src/ducks/loan/LoanListPage.jsx
+++ src/ducks/loan/LoanListPage.jsx
@@ -3,11 +3,25 @@ import { Section } from 'components/Section'
 import LoanProgress from 'ducks/loan/LoanProgress'
 import CompositeRow from 'cozy-ui/transpiled/react/CompositeRow'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import withBreakpoints from 'cozy-ui/transpiled/react/helpers/withBreakpoints'
 import { Bold } from 'cozy-ui/transpiled/react/Text'
 import AccountIcon from 'components/AccountIcon'
 import withFilters from 'components/withFilters'
 import { BalanceDetailsHeader } from 'ducks/balance'
-import { Wrapper } from 'components/Spacing'
+import { Wrapper, Padded } from 'components/Spacing'
+
+const PaddedOnDesktop = withBreakpoints()(props => {
+  const {
+    breakpoints: { isDesktop },
+    children
+  } = props
+
+  if (isDesktop) {
+    return <Padded>{children}</Padded>
+  }
+
+  return children
+})
 
 const DumbLoanListPage = props => {
   const { filteringDoc, filterByDoc } = props
@@ -19,20 +33,22 @@ const DumbLoanListPage = props => {
       {accounts.map(account => (
         <Section key={account._id}>
           <Wrapper>
-            <CompositeRow
-              primaryText={
-                <div className="u-flex u-flex-items-center">
-                  <AccountIcon account={account} />
-                  <Bold className="u-ml-1">{account.label}</Bold>
-                </div>
-              }
-              right={<Icon icon="right" color="var(--coolGrey)" />}
-              actions={<LoanProgress account={account} />}
-              className="u-c-pointer"
-              onClick={() => {
-                filterByDoc(account)
-              }}
-            />
+            <PaddedOnDesktop>
+              <CompositeRow
+                primaryText={
+                  <div className="u-flex u-flex-items-center">
+                    <AccountIcon account={account} />
+                    <Bold className="u-ml-1">{account.label}</Bold>
+                  </div>
+                }
+                right={<Icon icon="right" color="var(--coolGrey)" />}
+                actions={<LoanProgress account={account} />}
+                className="u-c-pointer"
+                onClick={() => {
+                  filterByDoc(account)
+                }}
+              />
+            </PaddedOnDesktop>
           </Wrapper>
         </Section>
       ))}
