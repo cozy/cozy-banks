0604a00e01eec9456150c80b880cc8647885e202
Patrick Browne <pt.browne@gmail.com>
2019-11-14 11:03:01 +0100

    test: Correctly send notifications

--- src/ducks/notifications/services.spec.js
+++ src/ducks/notifications/services.spec.js
@@ -1,11 +1,22 @@
 // Mocking handlersbars since we don't want to test HTML rendering here and it fails in the tests context
 jest.mock('handlebars')
 
+import CozyClient from 'cozy-client'
 import merge from 'lodash/merge'
-import { getEnabledNotificationClasses } from './services'
+import { getEnabledNotificationClasses, sendNotifications } from './services'
 import BalanceLower from './BalanceLower'
 import TransactionGreater from './TransactionGreater'
 import HealthBillLinked from './HealthBillLinked'
+import { BankAccount } from 'models'
+import { sendNotification } from 'cozy-notifications'
+
+jest.mock('cozy-notifications', () => {
+  const mod = jest.requireActual('cozy-notifications')
+  return {
+    ...mod,
+    sendNotification: jest.fn()
+  }
+})
 
 describe('getEnabledNotificationClasses', () => {
   it('should return the right classes', () => {
@@ -69,5 +80,53 @@ describe('getEnabledNotificationClasses', () => {
         })
       )
     ).toHaveLength(0)
+  })
+})
+
+const mockEnvForEachTest = values => {
+  let prev = {}
+
+  beforeEach(() => {
+    Object.entries(values).forEach(([name, value]) => {
+      prev[name] = process.env[name]
+      process.env[name] = value
+    })
+  })
+
+  afterEach(() => {
+    Object.entries(values).forEach(([name]) => {
+      process.env[name] = prev[name]
+    })
+  })
+}
+
+describe('service', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(BankAccount, 'getAll')
+      .mockReturnValue([
+        { _id: 'c0ffeedeadbeef', label: 'My bank account', balance: 123 }
+      ])
+  })
+
+  mockEnvForEachTest({
+    COZY_URL: 'http://localhost:8080',
+    COZY_CREDENTIALS: 'fake-token'
+  })
+
+  it('should correctly send a notification', async () => {
+    const transactions = [
+      { account: 'c0ffeedeadbeef', label: 'My transaction' }
+    ]
+    const config = {
+      notifications: {
+        healthBillLinked: { enabled: true }
+      }
+    }
+    await sendNotifications(config, transactions)
+    expect(sendNotification).toHaveBeenCalledWith(
+      expect.any(CozyClient),
+      expect.any(HealthBillLinked)
+    )
   })
 })
