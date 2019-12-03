3b9527a6e613c7a5836a4a026b6e5a039d5b287e
Patrick Browne <pt.browne@gmail.com>
2019-12-03 17:49:09 +0100

    fix: Prevent clashes between configuration setting doc and pin doc

--- src/ducks/settings/selectors.js
+++ src/ducks/settings/selectors.js
@@ -1,10 +1,3 @@
 import { queryDataSelector } from 'selectors'
 
 export const getSettings = queryDataSelector('settings')
-
-export const getNotificationFromSettings = (settings, name) => {
-  if (!settings || settings.length === 0) {
-    return null
-  }
-  return settings[0].notifications[name]
-}
