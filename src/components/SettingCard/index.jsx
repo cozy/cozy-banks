733c377593ddefdb08b2b68d193c54b66dca5355
Patrick Browne <pt.browne@gmail.com>
2019-12-09 18:09:02 +0100

    refactor: Simplify prop merging

--- src/components/SettingCard/index.jsx
+++ src/components/SettingCard/index.jsx
@@ -3,25 +3,15 @@ import { Card } from 'cozy-ui/react'
 import styles from './styles.styl'
 import cx from 'classnames'
 
-const mergeProps = (
-  { className, ...props },
-  { className: otherClassName, ...otherProps }
-) => ({
-  ...props,
-  ...otherProps,
-  className: cx(className, otherClassName)
-})
-
 const SettingCard = ({ enabled, clickable, ...props }) => {
   return (
     <Card
-      {...mergeProps(props, {
-        className: cx(
-          styles.SettingCard,
-          !enabled && styles['SettingCard--disabled'],
-          clickable && styles['SettingCard--clickable']
-        )
-      })}
+      {...props}
+      className={cx(
+        styles.SettingCard,
+        !enabled && styles['SettingCard--disabled'],
+        clickable && styles['SettingCard--clickable']
+      )}
     />
   )
 }
