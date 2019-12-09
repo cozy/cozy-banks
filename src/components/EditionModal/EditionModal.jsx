4333b96b0c80a93d6991502d2fd60e9e73b9d802
Patrick Browne <pt.browne@gmail.com>
2019-12-09 16:25:06 +0100

    refactor: Use a more generic NumberSection to implement ThresoldSection
    - Useful to select days for example
    - Parsing of user selected value is done at component level

--- src/components/EditionModal/EditionModal.jsx
+++ src/components/EditionModal/EditionModal.jsx
@@ -87,8 +87,8 @@ const InfoSlide = ({
             }
             onChange={
               fieldSpec.immediate
-                ? ev => {
-                    onChangeField(fieldName, ev.target.value)
+                ? value => {
+                    onChangeField(fieldName, value)
                   }
                 : null
             }
