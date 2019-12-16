b95681783173a384dc15545c1a40da317a6e8c94
Patrick Browne <pt.browne@gmail.com>
2019-12-16 14:18:03 +0100

    refactor: Extract useList helper

--- src/ducks/settings/CategoryAlerts/CategoryAlertSettingsPane.jsx
+++ src/ducks/settings/CategoryAlerts/CategoryAlertSettingsPane.jsx
@@ -14,6 +14,7 @@ import CategoryAlertCard from 'ducks/settings/CategoryAlerts/CategoryAlertCard'
 import CategoryAlertEditModal from 'ducks/settings/CategoryAlerts/CategoryAlertEditModal'
 
 import { getAlertId, getNextAlertId, makeNewAlert } from 'ducks/budgetAlerts'
+import useList from '../useList'
 
 export const CreateCategoryAlert = translate()(({ createAlert, t }) => {
   const [creating, setCreating] = useState(false)
@@ -50,53 +51,29 @@ export const CreateCategoryAlert = translate()(({ createAlert, t }) => {
   )
 })
 
-/**
- * Replace item in arr, finding item through idFn.
- * If previous item cannot be found, the new item is pushed at the
- * end of the array.
- */
-const replaceBy = (arr, item, idFn) => {
-  const id = idFn(item)
-  const index = arr.findIndex(x => idFn(x) === id)
-  return index !== -1
-    ? [...arr.slice(0, index), item, ...arr.slice(index + 1)]
-    : [...arr, item]
+const updateBudgetAlerts = async (client, settings, categoryBudgetAlerts) => {
+  const updatedSettings = {
+    ...settings,
+    categoryBudgetAlerts
+  }
+  await client.save(updatedSettings)
 }
 
 const CategoryAlertsPane = ({ client, settingsCollection, t }) => {
   const settings = getDefaultedSettingsFromCollection(settingsCollection)
-  const [alerts, setAlerts] = useState(settings.categoryBudgetAlerts)
-
-  const updateAlerts = async updatedAlerts => {
-    const previousAlerts = alerts
-    const updatedSettings = {
-      ...settings,
-      categoryBudgetAlerts: updatedAlerts
+  const onUpdate = updatedAlerts =>
+    updateBudgetAlerts(client, settings, updatedAlerts)
+  const onError = () =>
+    Alerter.error(t('Settings.budget-category-alerts.saving-error'))
+  const [alerts, createOrUpdateAlert, removeAlert] = useList(
+    settings.categoryBudgetAlerts,
+    {
+      onUpdate,
+      onError,
+      getId: getAlertId,
+      getNextId: getNextAlertId
     }
-    setAlerts(updatedAlerts)
-    try {
-      await client.save(updatedSettings)
-    } catch (e) {
-      Alerter.error(t('Settings.budget-category-alerts.saving-error'))
-      setAlerts(previousAlerts)
-    }
-  }
-
-  const createOrUpdateAlert = async updatedAlert => {
-    if (updatedAlert.id === undefined) {
-      updatedAlert.id = getNextAlertId(alerts)
-    }
-    const updatedAlerts = replaceBy(alerts, updatedAlert, getAlertId)
-    await updateAlerts(updatedAlerts)
-  }
-
-  const removeAlert = async alertToRemove => {
-    const idToRemove = getAlertId(alertToRemove)
-    const updatedAlerts = alerts.filter(
-      alert => getAlertId(alert) !== idToRemove
-    )
-    await updateAlerts(updatedAlerts)
-  }
+  )
 
   return (
     <ToggleRowWrapper>
