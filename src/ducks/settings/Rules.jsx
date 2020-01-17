5d78b22f8c9ea1863af1eafb627f6d9d5e238be0
Patrick Browne <pt.browne@gmail.com>
2020-01-17 12:19:15 +0100

    fix: Could not create any rules when no rule was there

--- src/ducks/settings/Rules.jsx
+++ src/ducks/settings/Rules.jsx
@@ -53,15 +53,14 @@ const Rules = ({
           {items
             ? items.map((item, i) => children(item, i, createOrUpdate, remove))
             : null}
-
-          {creating ? (
-            <ItemEditionModal
-              onDismiss={() => setCreating(false)}
-              initialDoc={makeNewItem()}
-              onEdit={handleCreateItem}
-            />
-          ) : null}
         </Stack>
+      ) : null}
+      {creating ? (
+        <ItemEditionModal
+          onDismiss={() => setCreating(false)}
+          initialDoc={makeNewItem()}
+          onEdit={handleCreateItem}
+        />
       ) : null}
       <AddRuleButton
         label={t(addButtonLabelKey)}
