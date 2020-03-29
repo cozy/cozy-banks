c19ada8e641ac6bd56b31754e4aeb26917fb0909
Patrick Browne <pt.browne@gmail.com>
2020-03-29 15:33:13 +0200

    refactor: Use common target.app for target.{browser,mobile}

--- config/webpack.target.browser.js
+++ config/webpack.target.browser.js
@@ -2,34 +2,23 @@
 
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const appConfig = require('./webpack.target.app')
 
-module.exports = {
-  entry: [
-    require.resolve('@babel/polyfill'),
-    path.resolve(__dirname, '../src/main')
-  ],
-  output: {
-    path: path.resolve(__dirname, '../build')
-  },
-  externals: {
-    'cozy-client-js': 'cozy'
-  },
-  resolve: {
-    extensions: ['.browser.js', '.browser.jsx']
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __TARGET__: JSON.stringify('browser'),
-      __POUCH__: JSON.stringify(process.env.FORCE_POUCH ? true : false)
-    }),
-    new HtmlWebpackPlugin({
-      alwaysWriteToDisk: true,
-      template: path.resolve(__dirname, '../src/index.ejs'),
-      title: 'Cozy Banks',
-      minify: {
-        collapseWhitespace: true
-      }
-    })
-  ]
-}
+module.exports = merge(
+  appConfig,
+  {
+    resolve: {
+      extensions: ['.browser.js', '.browser.jsx']
+    },
+    output: {
+      path: path.resolve(__dirname, '../build')
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __TARGET__: JSON.stringify('browser'),
+        __POUCH__: JSON.stringify(process.env.FORCE_POUCH ? true : false)
+      })
+    ]
+  }
+)
--- config/webpack.target.mobile.js
+++ config/webpack.target.mobile.js
@@ -1,10 +1,9 @@
 'use strict'
 
+const merge = require('webpack-merge')
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const { production } = require('./webpack.vars')
+const appConfig = require('./webpack.target.app')
 
 const output = {
   path: path.resolve(__dirname, '../src/targets/mobile/www')
@@ -14,37 +13,25 @@ if (process.env.PUBLIC_PATH) {
   output.publicPath = process.env.PUBLIC_PATH
 }
 
-module.exports = {
-  entry: {
-    app: [
-      require.resolve('@babel/polyfill'),
-      path.resolve(__dirname, '../src/main.jsx')
+module.exports = merge(
+  appConfig,
+  {
+    resolve: {
+      extensions: ['.mobile.js', '.mobile.jsx']
+    },
+    output: output,
+    plugins: [
+      new webpack.DefinePlugin({
+        __TARGET__: JSON.stringify('mobile'),
+        __POUCH__: JSON.stringify(true)
+      }),
+      new webpack.ProvidePlugin({
+        PouchDB: 'pouchdb',
+        pouchdbFind: 'pouchdb-find',
+        pouchdbAdapterCordovaSqlite: 'pouchdb-adapter-cordova-sqlite',
+        'cozy.client': 'cozy-client-js/dist/cozy-client.js',
+        'cozy.bar': `cozy-bar/dist/cozy-bar.mobile.js`
+      })
     ]
-  },
-  resolve: {
-    extensions: ['.mobile.js', '.mobile.jsx']
-  },
-  output: output,
-  plugins: [
-    new webpack.DefinePlugin({
-      __ALLOW_HTTP__: !production,
-      __TARGET__: JSON.stringify('mobile'),
-      __POUCH__: JSON.stringify(true)
-    }),
-    new webpack.ProvidePlugin({
-      PouchDB: 'pouchdb',
-      pouchdbFind: 'pouchdb-find',
-      pouchdbAdapterCordovaSqlite: 'pouchdb-adapter-cordova-sqlite',
-      'cozy.client': 'cozy-client-js/dist/cozy-client.js',
-      'cozy.bar': `cozy-bar/dist/cozy-bar.mobile.js`
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, `../src/index.ejs`),
-      title: `cozy-banks`,
-      chunks: ['app'],
-      minify: {
-        collapseWhitespace: false
-      }
-    })
-  ]
-}
+  }
+)
