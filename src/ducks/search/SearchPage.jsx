3e4e09ba22a163cc81efd6aa265802f6e073f380
Patrick Browne <pt.browne@gmail.com>
2020-05-19 20:42:50 +0200

    feat: Make header fixed and add TransactionTableHead

--- src/ducks/search/SearchPage.jsx
+++ src/ducks/search/SearchPage.jsx
@@ -7,6 +7,8 @@ import { Input, Stack, useI18n, Empty } from 'cozy-ui/transpiled/react'
 import { transactionsConn } from 'doctypes'
 import { TransactionList } from 'ducks/transactions/Transactions'
 import BarTheme from 'ducks/bar/BarTheme'
+import TransactionTableHead from 'ducks/transactions/header/TableHead'
+
 import Header from 'components/Header'
 import Padded from 'components/Spacing/Padded'
 import { PageTitle } from 'components/Title'
@@ -39,7 +41,7 @@ const SearchPage = ({ router }) => {
   return (
     <div>
       <BarTheme theme="primary" />
-      <Header theme="inverted">
+      <Header theme="inverted" fixed>
         <Padded>
           <Stack spacing="l">
             <PageTitle className="u-lh-tiny">{t('Search.title')}</PageTitle>
@@ -51,6 +53,7 @@ const SearchPage = ({ router }) => {
             />
           </Stack>
         </Padded>
+        <TransactionTableHead isSubcategory={false} />
       </Header>
       {!searchSufficient ? <Padded>{t('Search.type-a-search')}</Padded> : null}
       <div className={`js-scrolling-element`}>
