58715ed59b791be488d58044bb9404ab9afed873
Patrick Browne <pt.browne@gmail.com>
2020-09-23 16:11:10 +0200

    feat: Specify index fields by hand
    Couch is a bit picky concerning indexes and in the case of a sort,
    we need to have the same order between the indexed fields and the sort

--- src/ducks/recurrence/queries.js
+++ src/ducks/recurrence/queries.js
@@ -20,7 +20,11 @@ export const bundleTransactionsQueryConn = ({ bundle }) => {
     query: () => {
       const initialQDef = queryRecurrenceTransactions(bundle)
       const qDef = initialQDef
-        .sortBy([{ date: 'desc' }])
+        .sort([
+          { 'relationships.recurrence.data._id': 'desc' },
+          { date: 'desc' }
+        ])
+        .indexFields(['relationships.recurrence.data._id', 'date'])
         .where({ ...initialQDef.selector, date: { $gt: null } })
         .UNSAFE_noLimit()
         .include(['bills', 'account', 'reimbursements', 'recurrence'])
