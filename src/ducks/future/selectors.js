db73ddd1d3cfa7275ec8dc635507af7e2e335ce5
Patrick Browne <pt.browne@gmail.com>
2020-11-19 19:19:09 +0100

    fix: Filter out old recurrence transactions

--- src/ducks/future/selectors.js
+++ src/ducks/future/selectors.js
@@ -6,6 +6,7 @@ import { getFilteredAccountIds } from 'ducks/filters'
 import { nextDate, STATUS_FINISHED } from 'ducks/recurrence'
 import getClient from 'selectors/getClient'
 import { TRANSACTION_DOCTYPE } from 'doctypes'
+import isAfter from 'date-fns/is_after'
 
 export const getMaxDate = () => {
   return addDays(new Date(Date.now()), 30)
@@ -18,9 +19,11 @@ export const getMaxDate = () => {
  */
 export const getPlannedTransactions = createSelector(
   [getRecurrences, getFilteredAccountIds, getMaxDate],
-  (recurrences, filteredAccountIds, maxDate) => {
+  (recurrences, filteredAccountIds, maxDateArg) => {
     const client = getClient()
     const res = []
+    const now = new Date()
+    const maxDate = new Date(maxDateArg)
     for (let recurrence of recurrences) {
       if (recurrence.status === STATUS_FINISHED) {
         continue
@@ -38,17 +41,18 @@ export const getPlannedTransactions = createSelector(
         continue
       }
 
-      const futureDate = nextDate(recurrence).toISOString()
-      if (futureDate > maxDate) {
+      const futureDate = nextDate(recurrence)
+      if (isAfter(now, futureDate) || isAfter(futureDate, maxDate)) {
         continue
       }
 
       const transaction = client.hydrateDocument({
         _type: TRANSACTION_DOCTYPE,
         label: recurrence.automaticLabel,
-        date: futureDate,
+        date: futureDate.toISOString(),
         amount: recurrence.amounts[0],
         account: lastBankAccount,
+        manualCategoryId: recurrence.categoryIds[0],
         automaticCategoryId: recurrence.categoryIds[0]
       })
       res.push(transaction)
