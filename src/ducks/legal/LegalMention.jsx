0cd81a24fa6f0d945ddf3ead263d856fda508354
Patrick Browne <pt.browne@gmail.com>
2020-11-24 16:28:28 +0100

    feat: Add legal mention in balance header

---
+++ src/ducks/legal/LegalMention.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+
+/**
+ * This is a placeholder element that can be overrided.
+ * It is displayed in page headers to show which enterprise is
+ * responsible for retrieving the financial data of the user
+ */
+const LegalMention = () => {
+  return null
+}
+
+LegalMention.propTypes = {
+  className: PropTypes.string
+}
+
+export default LegalMention
