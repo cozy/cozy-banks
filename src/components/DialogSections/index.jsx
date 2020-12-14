a21b3fd89ac5b87c61f115ddc36e3d194097007d
Patrick Browne <pt.browne@gmail.com>
2020-12-14 10:19:55 +0100

    refactor: Clean outdated comment

--- src/components/DialogSections/index.jsx
+++ src/components/DialogSections/index.jsx
@@ -1,8 +1,3 @@
-/**
- * Should be moved inside cozy-ui, see
- * https://github.com/cozy/cozy-ui/issues/1290
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import { DialogContent } from 'cozy-ui/transpiled/react/Dialog'
