418dd016e678457b645ceccc6f17af99fbe2c020
Patrick Browne <pt.browne@gmail.com>
2021-05-12 12:09:29 +0200

    fix: When fetching configuration settings, find the right one
    Previously, we took the first setting document since we did not have
    many settings documents. Now that there trigger-states, pin and more
    to come, we cannot expect the configuration settings document to be
    the right one

--- src/ducks/settings/services.js
+++ src/ducks/settings/services.js
@@ -1,5 +1,5 @@
 import logger from 'cozy-logger'
-import { getDefaultedSettings } from './helpers'
+import { getDefaultedSettings, isConfigurationSetting } from './helpers'
 import { Document } from 'cozy-doctypes'
 import { DOCTYPE } from './constants'
 import omit from 'lodash/omit'
@@ -9,7 +9,9 @@ const log = logger.namespace('settings-doctype')
 class Settings extends Document {
   static async fetchWithDefault() {
     const settingDocuments = await this.fetchAll()
-    const settings = settingDocuments[0]
+    const settings = settingDocuments.find(
+      isConfigurationSetting
+    )
 
     if (!settings) {
       log('info', 'No settings yet, default settings are used')
