8b9b90dd09752e4f80b993043adbeb1053a5ece8
Patrick Browne <pt.browne@gmail.com>
2021-06-02 16:19:53 +0200

    feat: Add polyfill for intersection-observer
    Since we target iOS 11 and IntersectionObserver is only
    available from iOS 12.4

--- src/hooks/useIntersectionObserver.jsx
+++ src/hooks/useIntersectionObserver.jsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
+import 'intersection-observer'
 
 const useIntersectionObserver = (observerOptions, handleIntersection) => {
   const [ref, setRef] = useState(null)
