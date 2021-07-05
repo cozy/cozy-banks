5b8286ecfa9467433cb5e9342206caded36cafa0
Patrick Browne <pt.browne@gmail.com>
2021-07-05 14:36:22 +0200

    feat: Improve mattermost comment script
    - Fail on undefined variable, -u
    - Output executed commands, -x
    - Fail on bad HTTP codes, -f

--- scripts/ci/mattermost_comment.sh
+++ scripts/ci/mattermost_comment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is used to comment on mattermost
 
-set -e
+set -eux
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   FROM="pull request [#$TRAVIS_PULL_REQUEST](https://github.com/$TRAVIS_PULL_REQUEST_SLUG/pull/$TRAVIS_PULL_REQUEST)"
@@ -12,4 +12,4 @@ else
 fi
 
 export MATTERMOST_CHANNEL="gangsters"
-curl -i -X POST -H "Content-Type: application/json" -d "{\"text\": \"🎁 [Click here]($APK_URL) to download the latest Android APK from $FROM\", \"icon_url\": \"https://travis-ci.com/images/logos/TravisCI-Mascot-1.png\", \"username\": \"Travis\", \"channel\": \"$MATTERMOST_CHANNEL\"}" $MATTERMOST_HOOK_URL
+curl -f -i -X POST -H "Content-Type: application/json" -d "{\"text\": \"🎁 [Click here]($APK_URL) to download the latest Android APK from $FROM\", \"icon_url\": \"https://travis-ci.com/images/logos/TravisCI-Mascot-1.png\", \"username\": \"Travis\", \"channel\": \"$MATTERMOST_CHANNEL\"}" $MATTERMOST_HOOK_URL
