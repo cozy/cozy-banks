374f7098a944ae0e305eaf43fa2ed95aeabd5425
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-09-01 14:59:36 +0200

    refactor: Move code inside getLinks() method
    to be able to use await inside it, after asyncing it

--- src/ducks/client/links.js
+++ src/ducks/client/links.js
@@ -1,17 +1,15 @@
 /* global __POUCH__ */
-
 import fromPairs from 'lodash/fromPairs'
+
 import { StackLink, Q } from 'cozy-client'
-import { offlineDoctypes } from 'doctypes'
 import { isMobileApp, isIOSApp } from 'cozy-device-helper'
 import flag from 'cozy-flags'
-import { TRANSACTION_DOCTYPE } from 'doctypes'
 
+import { offlineDoctypes, TRANSACTION_DOCTYPE } from 'doctypes'
 import { APPLICATION_DATE } from 'ducks/transactions/constants'
 
 const activatePouch = __POUCH__ && !flag('banks.pouch.disabled')
-
-let PouchLink
+let links = null
 
 const makeWarmupQueryOptions = (doctype, indexedFields) => {
   return {
@@ -29,53 +27,52 @@ const makeWarmupQueryOptions = (doctype, indexedFields) => {
   }
 }
 
-const pouchLinkOptions = {
-  doctypes: offlineDoctypes,
-  doctypesReplicationOptions: {
-    [TRANSACTION_DOCTYPE]: {
-      warmupQueries: [
-        makeWarmupQueryOptions(TRANSACTION_DOCTYPE, ['date']),
-        makeWarmupQueryOptions(TRANSACTION_DOCTYPE, [APPLICATION_DATE]),
-        makeWarmupQueryOptions(TRANSACTION_DOCTYPE, ['account']),
-        makeWarmupQueryOptions(TRANSACTION_DOCTYPE, ['date', 'account']),
-        makeWarmupQueryOptions(TRANSACTION_DOCTYPE, [
-          APPLICATION_DATE,
-          'account'
-        ])
-      ]
-    }
-  },
-  initialSync: true
-}
-
-if (activatePouch) {
-  PouchLink = require('cozy-pouch-link').default
-
-  if (isMobileApp() && isIOSApp()) {
-    pouchLinkOptions.pouch = {
-      plugins: [require('pouchdb-adapter-cordova-sqlite')],
-      options: {
-        adapter: 'cordova-sqlite',
-        location: 'default'
-      }
-    }
-  }
-}
-
-let links = null
 export const getLinks = (options = {}) => {
   if (links) {
     return links
   }
 
   const stackLink = new StackLink()
+
   links = [stackLink]
 
   if (activatePouch) {
+    const pouchLinkOptions = {
+      doctypes: offlineDoctypes,
+      doctypesReplicationOptions: {
+        [TRANSACTION_DOCTYPE]: {
+          warmupQueries: [
+            makeWarmupQueryOptions(TRANSACTION_DOCTYPE, ['date']),
+            makeWarmupQueryOptions(TRANSACTION_DOCTYPE, [APPLICATION_DATE]),
+            makeWarmupQueryOptions(TRANSACTION_DOCTYPE, ['account']),
+            makeWarmupQueryOptions(TRANSACTION_DOCTYPE, ['date', 'account']),
+            makeWarmupQueryOptions(TRANSACTION_DOCTYPE, [
+              APPLICATION_DATE,
+              'account'
+            ])
+          ]
+        }
+      },
+      initialSync: true
+    }
+
+    if (isMobileApp() && isIOSApp()) {
+      pouchLinkOptions.pouch = {
+        plugins: [require('pouchdb-adapter-cordova-sqlite')],
+        options: {
+          adapter: 'cordova-sqlite',
+          location: 'default'
+        }
+      }
+    }
+
+    const PouchLink = require('cozy-pouch-link').default
+
     const pouchLink = new PouchLink({
       ...pouchLinkOptions,
       ...options.pouchLink
     })
+
     links = [pouchLink, ...links]
   }
 
