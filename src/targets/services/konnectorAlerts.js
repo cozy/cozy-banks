04611a7eb20b2dd3f44ee300c85a7b786813880a
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-10-18 12:11:38 +0200

    feat: Add konnectorAlerts log for at trigger creation

--- src/targets/services/konnectorAlerts.js
+++ src/targets/services/konnectorAlerts.js
@@ -70,16 +70,35 @@ const fetchRegistryInfo = memoize(
 )
 
 const createTriggerAt = async (client, date) => {
-  await client.save({
-    _type: TRIGGER_DOCTYPE,
-    type: '@at',
-    arguments: date.toISOString(),
-    worker: 'service',
-    message: {
-      name: 'konnectorAlerts',
-      slug: 'banks'
-    }
-  })
+  logger(
+    'info',
+    `Try to create new @at trigger for konnectorAlerts service at ${
+      date.toISOString().split('T')[0]
+    }...`
+  )
+  try {
+    await client.save({
+      _type: TRIGGER_DOCTYPE,
+      type: '@at',
+      arguments: date.toISOString(),
+      worker: 'service',
+      message: {
+        name: 'konnectorAlerts',
+        slug: 'banks'
+      }
+    })
+    logger(
+      'info',
+      `⭐ Created: new @at trigger for konnectorAlerts service at ${
+        date.toISOString().split('T')[0]
+      }`
+    )
+  } catch (error) {
+    logger(
+      'error',
+      `❗ Error when creating new @at trigger for konnectorAlerts service: ${error.message}`
+    )
+  }
 }
 
 export const containerForTesting = {
