4a677379b8ed26e307732afb2cd6944468d5a314
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-02-07 10:44:38 +0100

    refactor: Transform Reimbursements in func comp

--- src/ducks/reimbursements/Reimbursements.jsx
+++ src/ducks/reimbursements/Reimbursements.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { transactionsConn } from 'doctypes'
 import sumBy from 'lodash/sumBy'
-import compose from 'lodash/flowRight'
 
-import { queryConnect, isQueryLoading, hasQueryBeenLoaded } from 'cozy-client'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { isQueryLoading, hasQueryBeenLoaded, useQuery } from 'cozy-client'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Figure from 'cozy-ui/transpiled/react/Figure'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
 
@@ -13,130 +12,130 @@ import { TransactionList } from 'ducks/transactions/Transactions'
 import styles from 'ducks/reimbursements/Reimbursements.styl'
 import Loading from 'components/Loading'
 import { Section, SectionTitle } from 'components/Section'
-import withFilters from 'components/withFilters'
+import { useFilters } from 'components/withFilters'
 import { getYear } from 'date-fns'
 import TransactionActionsProvider from 'ducks/transactions/TransactionActionsProvider'
-import withBrands from 'ducks/brandDictionary/withBrands'
+import { useBrands } from 'ducks/brandDictionary/withBrands'
 import { getGroupedFilteredExpenses } from './selectors'
 import { getPeriod, parsePeriod, getFilteringDoc } from 'ducks/filters'
 import { DESKTOP_SCROLLING_ELEMENT_CLASSNAME } from 'ducks/transactions/scroll/getScrollingElement'
 import NoPendingReimbursements from 'ducks/reimbursements/NoPendingReimbursements'
 import NoReimbursedExpenses from 'ducks/reimbursements/NoReimbursedExpenses'
 
-export class DumbReimbursements extends Component {
-  componentDidMount() {
-    this.props.addFilterByPeriod(getYear(new Date()).toString())
+export const DumbReimbursements = ({
+  triggers,
+  addFilterByPeriod,
+  groupedExpenses,
+  currentPeriod,
+  filteringDoc,
+  transactions,
+  brands
+}) => {
+  const { t, f } = useI18n()
+
+  useEffect(() => {
+    addFilterByPeriod(getYear(new Date()).toString())
+  }, [addFilterByPeriod])
+
+  if (
+    (isQueryLoading(transactions) && !hasQueryBeenLoaded(transactions)) ||
+    (isQueryLoading(triggers) && !hasQueryBeenLoaded(triggers))
+  ) {
+    return <Loading loadingType />
   }
 
-  render() {
-    const {
-      groupedExpenses,
-      t,
-      f,
-      triggers,
-      transactions,
-      brands,
-      currentPeriod,
-      filteringDoc
-    } = this.props
+  const {
+    reimbursed: reimbursedExpenses,
+    pending: pendingExpenses
+  } = groupedExpenses
 
-    if (
-      (isQueryLoading(transactions) && !hasQueryBeenLoaded(transactions)) ||
-      (isQueryLoading(triggers) && !hasQueryBeenLoaded(triggers))
-    ) {
-      return <Loading loadingType />
-    }
+  const pendingAmount = sumBy(pendingExpenses, t => -t.amount)
 
-    const { reimbursed: reimbursedExpenses, pending: pendingExpenses } =
-      groupedExpenses
+  const hasHealthBrands =
+    brands.filter(brand => brand.hasTrigger && brand.health).length > 0
 
-    const pendingAmount = sumBy(pendingExpenses, t => -t.amount)
+  const formattedPeriod = f(
+    parsePeriod(currentPeriod),
+    currentPeriod.length === 4 ? 'YYYY' : 'MMMM YYYY'
+  )
 
-    const hasHealthBrands =
-      brands.filter(brand => brand.hasTrigger && brand.health).length > 0
+  const hasPendingExpenses = pendingExpenses && pendingExpenses.length > 0
+  const hasReimbursedExpenses =
+    reimbursedExpenses && reimbursedExpenses.length > 0
 
-    const formattedPeriod = f(
-      parsePeriod(currentPeriod),
-      currentPeriod.length === 4 ? 'YYYY' : 'MMMM YYYY'
-    )
-
-    const hasPendingExpenses = pendingExpenses && pendingExpenses.length > 0
-    const hasReimbursedExpenses =
-      reimbursedExpenses && reimbursedExpenses.length > 0
-
-    return (
-      <TransactionActionsProvider>
-        <div
-          className={`${styles.Reimbursements} ${DESKTOP_SCROLLING_ELEMENT_CLASSNAME}`}
-        >
-          <Section>
-            <SectionTitle>
-              {t('Reimbursements.pending')}
-              <Figure
-                symbol="€"
-                total={pendingAmount}
-                className={styles.Reimbursements__figure}
-                signed
-              />{' '}
-            </SectionTitle>
-            {hasPendingExpenses ? (
-              <TransactionList
-                transactions={pendingExpenses}
-                className={styles.Reimbursements__transactionsList}
-                showTriggerErrors={false}
+  return (
+    <TransactionActionsProvider>
+      <div
+        className={`${styles.Reimbursements} ${DESKTOP_SCROLLING_ELEMENT_CLASSNAME}`}
+      >
+        <Section>
+          <SectionTitle>
+            {t('Reimbursements.pending')}
+            <Figure
+              symbol="€"
+              total={pendingAmount}
+              className={styles.Reimbursements__figure}
+              signed
+            />{' '}
+          </SectionTitle>
+          {hasPendingExpenses ? (
+            <TransactionList
+              transactions={pendingExpenses}
+              className={styles.Reimbursements__transactionsList}
+              showTriggerErrors={false}
+            />
+          ) : (
+            <>
+              <Divider />
+              <NoPendingReimbursements
+                period={formattedPeriod}
+                doc={filteringDoc}
               />
-            ) : (
-              <>
-                <Divider />
-                <NoPendingReimbursements
-                  period={formattedPeriod}
-                  doc={filteringDoc}
-                />
-              </>
-            )}
-          </Section>
-          <Section>
-            <SectionTitle>{t('Reimbursements.alreadyReimbursed')}</SectionTitle>
-            {hasReimbursedExpenses ? (
-              <TransactionList
-                transactions={reimbursedExpenses}
-                className={styles.Reimbursements__transactionsList}
-                showTriggerErrors={false}
+            </>
+          )}
+        </Section>
+        <Section>
+          <SectionTitle>{t('Reimbursements.alreadyReimbursed')}</SectionTitle>
+          {hasReimbursedExpenses ? (
+            <TransactionList
+              transactions={reimbursedExpenses}
+              className={styles.Reimbursements__transactionsList}
+              showTriggerErrors={false}
+            />
+          ) : (
+            <>
+              <Divider />
+              <NoReimbursedExpenses
+                hasHealthBrands={hasHealthBrands}
+                doc={filteringDoc}
               />
-            ) : (
-              <>
-                <Divider />
-                <NoReimbursedExpenses
-                  hasHealthBrands={hasHealthBrands}
-                  doc={filteringDoc}
-                />
-              </>
-            )}
-          </Section>
-        </div>
-      </TransactionActionsProvider>
-    )
-  }
+            </>
+          )}
+        </Section>
+      </div>
+    </TransactionActionsProvider>
+  )
 }
 
-function mapStateToProps(state) {
-  return {
-    groupedExpenses: getGroupedFilteredExpenses(state),
-    currentPeriod: getPeriod(state),
-    filteringDoc: getFilteringDoc(state)
-  }
-}
+const Reimbursements = props => {
+  const { addFilterByPeriod } = useFilters()
+  const groupedExpenses = useSelector(getGroupedFilteredExpenses)
+  const currentPeriod = useSelector(getPeriod)
+  const filteringDoc = useSelector(getFilteringDoc)
+  const transactions = useQuery(transactionsConn.query, transactionsConn)
+  const { brands } = useBrands({ queryName: 'reimbursementsPageTriggers' }) // We need to have a different query name otherwise we end with an infinite loading
 
-const Reimbursements = compose(
-  translate(),
-  queryConnect({
-    transactions: transactionsConn
-  }),
-  connect(mapStateToProps),
-  withFilters,
-  // We need to have a different query name otherwise we end with an infinite
-  // loading
-  withBrands({ queryName: 'reimbursementsPageTriggers' })
-)(DumbReimbursements)
+  return (
+    <DumbReimbursements
+      addFilterByPeriod={addFilterByPeriod}
+      groupedExpenses={groupedExpenses}
+      currentPeriod={currentPeriod}
+      filteringDoc={filteringDoc}
+      transactions={transactions}
+      brands={brands}
+      {...props}
+    />
+  )
+}
 
 export default Reimbursements
