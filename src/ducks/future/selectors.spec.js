a432084ea79aef8b2b656dc08cd8a6c1e4ac4da4
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2022-09-21 15:51:22 +0200

    feat: Redefine deprecated recurrence bundles to include finished bundles

--- src/ducks/future/selectors.spec.js
+++ src/ducks/future/selectors.spec.js
@@ -1,3 +1,4 @@
+import { STATUS_ONGOING, STATUS_FINISHED } from 'ducks/recurrence/api'
 import { isDeprecatedBundle } from './selectors.js'
 
 const now = Date.now()
@@ -7,16 +8,28 @@ const fiveMonthsAgo = new Date(now)
 fiveMonthsAgo.setMonth(fiveMonthsAgo.getMonth() - 5)
 
 describe('isDeprecatedBundle', () => {
-  it('should consider inactive recurrences as deprecated', () => {
+  it('should consider inactive or finished recurrences as deprecated', () => {
     const recurrences = [
       {
-        latestDate: threeMonthsAgo.toISOString()
+        latestDate: threeMonthsAgo.toISOString(),
+        status: STATUS_ONGOING
       },
       {
-        latestDate: fiveMonthsAgo.toISOString()
+        latestDate: fiveMonthsAgo.toISOString(),
+        status: STATUS_ONGOING
+      },
+      {
+        latestDate: threeMonthsAgo.toISOString(),
+        status: STATUS_FINISHED
+      },
+      {
+        latestDate: fiveMonthsAgo.toISOString(),
+        status: STATUS_FINISHED
       }
     ]
     expect(isDeprecatedBundle(recurrences[0])).toBe(false)
     expect(isDeprecatedBundle(recurrences[1])).toBe(true)
+    expect(isDeprecatedBundle(recurrences[2])).toBe(true)
+    expect(isDeprecatedBundle(recurrences[3])).toBe(true)
   })
 })
