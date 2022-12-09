682fcfcb8dc04d93a438aa1a5ba4aadb7c5db8ec
paultranvan <paul@cozycloud.cc>
2022-12-09 17:10:26 +0100

    wip: bench search

--- src/ducks/search/SearchPage.jsx
+++ src/ducks/search/SearchPage.jsx
@@ -10,7 +10,7 @@ import Button from 'cozy-ui/transpiled/react/Button'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import Empty from 'cozy-ui/transpiled/react/Empty'
 import NarrowContent from 'cozy-ui/transpiled/react/NarrowContent'
-import { useQuery, isQueryLoading } from 'cozy-client'
+import { useQuery, useQueryAll, isQueryLoading, Q } from 'cozy-client'
 
 import { useTrackPage } from 'ducks/tracking/browser'
 import {
@@ -35,9 +35,36 @@ import SearchHeader from 'ducks/search/SearchHeader'
 
 import searchIllu from 'assets/search-illu.svg'
 
+import { Index, Document } from 'flexsearch'
+
 const emptyResults = []
 const transactionListOptions = { mobileSectionDateFormat: 'ddd D MMMM YYYY' }
 const emptyButtonStyle = { maxWidth: '80%' }
+
+function roughSizeOfObject(object) {
+  var objectList = []
+  var stack = [object]
+  var bytes = 0
+
+  while (stack.length) {
+    var value = stack.pop()
+
+    if (typeof value === 'boolean') {
+      bytes += 4
+    } else if (typeof value === 'string') {
+      bytes += value.length * 2
+    } else if (typeof value === 'number') {
+      bytes += 8
+    } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
+      objectList.push(value)
+
+      for (var i in value) {
+        stack.push(value[i])
+      }
+    }
+  }
+  return bytes
+}
 
 const SearchPage = () => {
   const params = useParams()
@@ -51,37 +78,72 @@ const SearchPage = () => {
 
   useTrackPage('recherche')
 
-  const transactionCol = useQuery(searchConn.query, searchConn)
+  const filesQ = Q('io.cozy.files').limitBy(1000)
+  const transactionCol = useQueryAll(searchConn.query, searchConn)
+  const filesCol = useQueryAll(filesQ, { as: 'files-query' })
 
   const { data: transactions = emptyResults, lastUpdate } = transactionCol
+  const { data: files } = filesCol
+  // const files = []
+
+  if (transactions) {
+    console.log('transactions in search : ', transactions.length)
+  }
+  if (files) {
+    console.log('files in search : ', files.length)
+  }
+
+  let searchDocs = []
+  let allKeys = []
+  if (transactions?.length > 0 && files?.length > 0) {
+    searchDocs = searchDocs.concat(transactions, files)
+    allKeys = [...Object.keys(transactions[0]), ...Object.keys(files[0])]
+  }
 
   const earliestTransaction = useMemo(() => {
     return minBy(transactions, getTransactionDate)
   }, [transactions])
 
-  const fuse = useMemo(() => {
-    const fuse = new Fuse(transactions || [], {
-      keys: ['label'],
-      ignoreLocation: true,
-      includeScore: true,
-      includeMatches: true,
-      minMatchCharLength: 3
+  const index = useMemo(() => {
+    console.time('build index')
+    console.log('go index on ', searchDocs.length)
+    // const index = new Fuse(searchDocs || [], {
+    //   keys: allKeys || ['_id'],
+    //   ignoreLocation: true,
+    //   includeScore: true,
+    //   includeMatches: true,
+    //   minMatchCharLength: 3
+    // })
+    const index = new Document({
+      document: {
+        id: '_id',
+        index: ['label'],
+        store: true
+      }
     })
-    return fuse
+    for (const doc of searchDocs) {
+      index.add(doc)
+    }
+    console.timeEnd('build index')
+    return index
   }, [lastUpdate]) // eslint-disable-line react-hooks/exhaustive-deps
 
   const searchSufficient = isSearchSufficient(search)
 
   const performSearch = useMemo(() => {
     return debounce(searchValue => {
-      const results = fuse
-        .search(searchValue)
-        .filter(result => result.score < 0.3)
+      console.time('search')
+      const results = index.search(searchValue, { enrich: true })
+      
+      // const results = index.search(searchValue)
+      // .filter(result => result.score < 0.3)
+      console.timeEnd('search')
+      console.log('results : ', results)
       const orderedResults = orderSearchResults(results)
       const transactions = orderedResults.map(result => result.item)
       setResultIds(transactions.map(tr => tr._id))
     }, 200)
-  }, [fuse, setResultIds])
+  }, [index, setResultIds])
 
   const handleFetchMore = useCallback(() => {
     if (!isQueryLoading(transactionCol)) {
